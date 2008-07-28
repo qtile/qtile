f6c3eee314c068f6b796d0c32bba9b7cd4f8c5c6
Aldo Cortesi <aldo@nullcube.com>
2008-07-28 15:53:04 +1000

    Add LRUCache, a decorator that implements a per-object, self-expiring LRU cache.

--- libqtile/utils.py
+++ libqtile/utils.py
@@ -1,4 +1,4 @@
-import operator
+import operator, functools
 from Xlib import X
 import manager
 
@@ -39,3 +39,41 @@ def shuffleDown(lst):
         c = lst[0]
         lst.remove(c)
         lst.append(c)
+
+
+class LRUCache:
+    """
+        A decorator that implements a self-expiring LRU cache for class
+        methods (not functions!).
+
+        Cache data is tracked as attributes on the object itself. There is
+        therefore a separate cache for each object instance.
+    """
+    def __init__(self, size=100):
+        self.size = size
+
+    def __call__(self, f):
+        cacheName = "_cached_%s"%f.__name__
+        cacheListName = "_cachelist_%s"%f.__name__
+        size = self.size
+
+        @functools.wraps(f)
+        def wrap(self, *args):
+            if not hasattr(self, cacheName):
+                setattr(self, cacheName, {})
+                setattr(self, cacheListName, [])
+            cache = getattr(self, cacheName)
+            cacheList = getattr(self, cacheListName)
+            if cache.has_key(args):
+                cacheList.remove(args)
+                cacheList.insert(0, args)
+                return cache[args]
+            else:
+                ret = f(self, *args)
+                cacheList.insert(0, args)
+                cache[args] = ret
+                if len(cacheList) > size:
+                    d = cacheList.pop()
+                    cache.pop(d)
+                return ret
+        return wrap
--- test/test_utils.py
+++ test/test_utils.py
@@ -8,7 +8,39 @@ class utranslateMasks(libpry.AutoTree):
         assert utils.translateMasks([]) == 0
 
 
+class uLRUCache(libpry.AutoTree):
+    def test_one(self):
+        class Foo:
+            ran = False
+            @utils.LRUCache(2)
+            def one(self, x):
+                self.ran = True
+                return x
+
+        f = Foo()
+        assert f.one(1) == 1
+        assert f.ran
+        f.ran = False
+        assert f.one(1) == 1
+        assert not f.ran
+
+        f.ran = False
+        assert f.one(1) == 1
+        assert not f.ran
+        assert f.one(2) == 2
+        assert f.one(3) == 3
+        assert f.ran
+
+        f.ran = False
+        assert f.one(1) == 1
+        assert f.ran
+
+        assert len(f._cached_one) == 2
+        assert len(f._cachelist_one) == 2
+
+
 tests = [
-    utranslateMasks()
+    utranslateMasks(),
+    uLRUCache()
 ]
 
