0afb19a5b9c554590f108122ca8abb8a0fa32915
Aldo Cortesi <aldo@nullcube.com>
2008-08-31 17:09:16 +1000

    Add Qtile version number to documentation.

--- doc-src/index.py
+++ doc-src/index.py
@@ -52,14 +52,14 @@ ns.commands_textbox = bar._TextBoxCommands()
 ns.commands_measurebox = bar._MeasureBoxCommands()
 ns.examples = Examples("..")
 
-ns.docTitle = "Qtile"
+ns.docTitle = "Qtile 0.1"
 ns.docMaintainer = "Aldo Cortesi"
 ns.docMaintainerEmail = "aldo@nullcube.com"
 ns.foot = "Copyright Aldo Cortesi 2008"
 ns.head = readFrom("_header.html")
 ns.sidebar = countershape.widgets.SiblingPageIndex('/index.html')
 this.layout = countershape.layout.TwoPane("yui-t2", "doc3")
-this.titlePrefix = "Qtile Manual - "
+this.titlePrefix = "Qtile 0.1 - "
 
 ns.qtgrok = countershape.grok.grok("../libqtile")
 
