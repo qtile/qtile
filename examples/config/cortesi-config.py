3f211ab1e412de99505f0567b1433ae9fc3c33a0
Aldo Cortesi <aldo@nullcube.com>
2008-10-20 10:44:09 +1100

    Update example configuration file.

--- examples/config/cortesi-config.py
+++ examples/config/cortesi-config.py
@@ -1,66 +1,66 @@
-import libqtile
+from libqtile.conf import *
 
-# First we specify a set of basic keybindings. The bindings below are with a
-# Kinesis keyboard, and may not make sense for standard keyboards.
+# The bindings below are for use with a Kinesis keyboard, and may not make
+# sense for standard keyboards.
 keys = [
     # First, a set of bindings to control the layouts
-    libqtile.Key(
+    Key(
         ["mod1"], "k",
-        libqtile.command.Call("stack_down"),
+        lazy.layout.down()
     ),
-    libqtile.Key(
+    Key(
         ["mod1"], "j",
-        libqtile.command.Call("stack_up"),
+        lazy.layout.up()
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "control"], "k",
-        libqtile.command.Call("stack_shuffle_down"),
+        lazy.layout.shuffle_down()
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "control"], "j",
-        libqtile.command.Call("stack_shuffle_up"),
+        lazy.layout.shuffle_up()
     ),
-    libqtile.Key(
+    Key(
         ["mod1"], "space",
-        libqtile.command.Call("stack_next")
+        lazy.layout.next()
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "space",
-        libqtile.command.Call("stack_rotate")
+        lazy.layout.rotate()
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "Return",
-        libqtile.command.Call("stack_toggle_split")
+        lazy.layout.toggle_split()
     ),
 
-    libqtile.Key(["mod1"], "n",      libqtile.command.Call("spawn", "firefox")),
-    libqtile.Key(["mod1"], "h",      libqtile.command.Call("to_screen", 0)),
-    libqtile.Key(["mod1"], "l",      libqtile.command.Call("to_screen", 1)),
+    Key(["mod1"], "n",      lazy.spawn("firefox")),
+    Key(["mod1"], "h",      lazy.to_screen(0)),
+    Key(["mod1"], "l",      lazy.to_screen(1)),
     # ~/bin/x starts a terminal program
-    libqtile.Key(["mod1"], "Return", libqtile.command.Call("spawn", "~/bin/x")),
-    libqtile.Key(["mod1"], "Tab",    libqtile.command.Call("nextlayout")),
-    libqtile.Key(["mod1"], "w",      libqtile.command.Call("kill")),
+    Key(["mod1"], "Return", lazy.spawn("~/bin/x")),
+    Key(["mod1"], "Tab",    lazy.nextlayout()),
+    Key(["mod1"], "w",      lazy.window.kill()),
 
     # The bindings below control Amarok, and my sound volume.
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "k",
-        libqtile.command.Call("spawn", "amixer -qc 0 set PCM 2dB+")
+        lazy.spawn("amixer -qc 0 set PCM 2dB+")
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "j",
-        libqtile.command.Call("spawn", "amixer -qc 0 set PCM 2dB-")
+        lazy.spawn("amixer -qc 0 set PCM 2dB-")
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "n",
-        libqtile.command.Call("spawn", "amarok -t")
+        lazy.spawn("amarok -t")
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "l",
-        libqtile.command.Call("spawn", "amarok -f")
+        lazy.spawn("amarok -f")
     ),
-    libqtile.Key(
+    Key(
         ["mod1", "shift"], "h",
-        libqtile.command.Call("spawn", "amarok -r")
+        lazy.spawn("amarok -r")
     ),
 ]
 
@@ -69,36 +69,36 @@ keys = [
 groups = ["a", "s", "d", "f", "u", "i", "o", "p"]
 for i in groups:
     keys.append(
-        libqtile.Key(["mod1"], i, libqtile.command.Call("pullgroup", i))
+        Key(["mod1"], i, lazy.group[i].toscreen())
     )
 
 
 # Two simple layout instances:
 layouts = [
     # A layout instance with a single stack (filling the screen), and no border.
-    libqtile.layout.Stack(stacks=1, borderWidth=0),
+    layout.Stack(stacks=1, borderWidth=0),
     # A 2-stack layout instance
-    libqtile.layout.Stack(stacks=2, borderWidth=2)
+    layout.Stack(stacks=2, borderWidth=2)
 ]
 
 
 # I have two screens, each of which has a Bar at the bottom. Each Bar has two
 # simple widgets - a GroupBox, and a WindowName.
 screens = [
-    libqtile.Screen(
-        bottom = libqtile.bar.Bar(
+    Screen(
+        bottom = bar.Bar(
                     [
-                        libqtile.bar.GroupBox(),
-                        libqtile.bar.WindowName()
+                        bar.GroupBox(),
+                        bar.WindowName()
                     ],
                     30,
                 ),
     ),
-    libqtile.Screen(
-        bottom = libqtile.bar.Bar(
+    Screen(
+        bottom = bar.Bar(
                     [
-                        libqtile.bar.GroupBox(),
-                        libqtile.bar.WindowName()
+                        bar.GroupBox(),
+                        bar.WindowName()
                     ],
                     30,
                 ),
