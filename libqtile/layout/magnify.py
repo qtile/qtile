910c65c22e9e232aecdbe4d60df8ed4cba997bb2
Ben Duffield <jebavarde@gmail.com>
2009-01-16 20:43:15 +0000

    magnify layout: Now handle other windows. Stack the other windows behind the first one.

--- libqtile/layout/magnify.py
+++ libqtile/layout/magnify.py
@@ -7,6 +7,18 @@ class Magnify(Layout):
         Layout.__init__(self)
         self.clients = []
 
+    def up(self):
+        self.shuffle(utils.shuffleUp)
+
+    def down(self):
+        self.shuffle(utils.shuffleDown)
+
+    def shuffle(self, function):
+        if self.clients:
+            function(self.clients)
+            self.group.layoutAll()
+            self.group.focus(self.clients[0], False)
+
     def close(self, group):
         c = Layout.clone(self, group)
         c.clients = []
@@ -26,32 +38,48 @@ class Magnify(Layout):
         """
             places the window appropriately
             place(self, x, y, width, height, border, borderColor)
+            First window is centre of screen (index 0)
+            Others stack behind it (indices 1 +)
+            
         """
-        print "Trying to configure a client"
-        print c.info()
-        print "info printed"
-        width = self.group.screen.dwidth
-        height = self.group.screen.dheight
+        screenWidth = self.group.screen.dwidth
+        screenHeight = self.group.screen.dheight
         gap = 50
-        print "before if"
-        if self.clients and c is self.clients[0]:
-            print "yeah, it's the one"
-            x = self.group.screen.dx + gap
-            y = self.group.screen.dy + gap
-            wi = width - 2*gap
-            he = height - 2*gap
-            print ("items", x, y, wi, he)
+        x = y = w = h = 0
+        if self.clients and c in self.clients:
+            if c is self.clients[0]:
+                x = self.group.screen.dx + gap
+                y = self.group.screen.dy + gap
+                w = screenWidth - 2*gap
+                h = screenHeight - 2*gap
+            else:
+                clis = self.clients[1:]
+                position = clis.index(c)
+                w = screenWidth
+                h = screenHeight/len(clis) #TODO: CAST TO INT?
+                x = self.group.screen.dx
+                y = self.group.screen.dy + position*h
             c.place(
-                self.group.screen.dx + gap,
-                self.group.screen.dy + gap,
-                width - 2*gap,
-                height - 2*gap,
+                x,
+                y,
+                w,
+                h,
                 0,
                 None
                 )
             c.unhide()
         else:
-            print "else"
-            print self.clients
             c.hide()
+
+    def cmd_down(self):
+        """
+           Switch down in the window list
+        """
+        self.down()
+
+    def cmd_up(self):
+        """
+           Switch up...
+        """
+        self.up()
                 
