814369cc17f1cae94a9a04902bcf2c70d46b7826
Ben Duffield <jebavarde@gmail.com>
2009-01-17 21:51:46 +0000

    Tile layout: Keep track of focused client

--- libqtile/layout/tile.py
+++ libqtile/layout/tile.py
@@ -10,6 +10,7 @@ class Tile(Layout):
         self.theme = theme
         self.ratio = ratio
         self.master = masterWindows
+        self.focused = None
 
     def up(self):
         self.shuffle(utils.shuffleUp)
@@ -27,18 +28,18 @@ class Tile(Layout):
         c.clients = []
         return c
 
+    def focus(self, c):
+        self.focused = c
+
     def add(self, c):
         self.clients.insert(0, c) #TODO: maybe make this configurable
                                   # Should new clients go to top?
         
     def remove(self, c):
         self.clients.remove(c)
-        if self.clients:
-            return self.clients[0] #TODO: figure this out
-                                   #should the top get focus?
-        else:
-            return None
-        #A better way. implement focus(), record which has focus, return this
+        if self.clients and c is self.focused:
+            self.focused = self.clients[0]
+        return self.focused
 
     def configure(self, c):
         screenWidth = self.group.screen.dwidth
@@ -57,7 +58,10 @@ class Tile(Layout):
                 h = screenHeight/(len(self.clients[self.master:]))
                 x = self.group.screen.dx + int(screenWidth*self.ratio)
                 y = self.group.screen.dy + self.clients[self.master:].index(c)*h
-            borderColor = self.theme["tile_border_normal"]
+            if c is self.focused:
+                borderColor = self.theme["tile_border_focused"]
+            else:
+                borderColor = self.theme["tile_border_normal"]
             colormap = self.group.qtile.display.screen().default_colormap
             bc = colormap.alloc_named_color(borderColor).pixel
             c.place(
