3f081795d4e430d875bf28f8eb2d8f132054b2c2
Ben Duffield <jebavarde@gmail.com>
2009-01-22 20:10:41 +0000

    tiletwo sublayout: added expand option - when this is true the master windows grow to fill all vertical space

--- libqtile/layout/tiletwo.py
+++ libqtile/layout/tiletwo.py
@@ -4,10 +4,11 @@ from sublayouts import VerticalStack
 
 
 class TileTwo(SubLayout):
-    def __init__(self, clientStack, theme, master_windows=1, ratio=0.618):
+    def __init__(self, clientStack, theme, master_windows=1, ratio=0.618, expand=True):
         SubLayout.__init__(self, clientStack, theme)
         self.master_windows = master_windows
         self.ratio = ratio
+        self.expand = expand
         self.sublayouts = []
 
         class MasterWindows(VerticalStack):
@@ -26,9 +27,16 @@ class TileTwo(SubLayout):
         return True #TAKE THEM ALL
 
     def layout(self, r, windows):
-        master = Rect(r.x, r.y, int(r.w * self.ratio), r.h)
+        master_windows = [w for w in windows if self.sublayouts[0].filter(w)]
+        slave_windows = [w for w in windows if w not in master_windows]
+        # rectangles 
+        master = Rect(r.x, 
+                      r.y, 
+                      (int(r.w * self.ratio) if len(slave_windows) or not self.expand else r.w), 
+                      r.h)
         slave = Rect(r.x+int(r.w*self.ratio), r.y, int(r.w * (1 - self.ratio)), r.h)
-        self.layout_sublayouts([(self.sublayouts[0], master), (self.sublayouts[1], slave)], windows)
+        self.sublayouts[0].layout(master, master_windows)
+        self.sublayouts[1].layout(slave, slave_windows)
             
         
         
