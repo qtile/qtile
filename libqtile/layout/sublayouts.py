c82b857372a7f9d41d1d73c5c19677ce8d820404
Ben Duffield <jebavarde@gmail.com>
2009-01-29 21:59:20 +0000

    Sublayouts: remove initialisation of colours in VerticalStack so that window borders work properl

--- libqtile/layout/sublayouts.py
+++ libqtile/layout/sublayouts.py
@@ -22,13 +22,6 @@ class TopLevelSubLayout(SubLayout):
 
 class VerticalStack(SubLayout):
     def layout(self, rectangle, windows):
-        def color(color):
-                colormap = self.clientStack.group.qtile.display.screen().default_colormap
-                return colormap.alloc_named_color(color).pixel
-        self.active_border = color(self.theme["verticalstack_border_active"])
-        self.focused_border = color(self.theme["verticalstack_border_active"])
-        self.normal_border = color(self.theme["verticalstack_border_normal"])
-        self.border_width = self.theme["verticalstack_border_width"]
         SubLayout.layout(self, rectangle, windows)
 
     def configure(self, r, client):
