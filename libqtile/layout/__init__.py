4de80188b359cc03e4a6bca7618d8ba4cdb91d76
Ben Duffield <jebavarde@gmail.com>
2009-02-01 00:30:44 +0000

    Add Rect to layout module __init__

--- libqtile/layout/__init__.py
+++ libqtile/layout/__init__.py
@@ -4,6 +4,8 @@ from magnify import Magnify
 from tile import Tile
 from clientstack import ClientStack
 
+from base import Rect
+
 # sublayouts
 from subtile import SubTile
 from submax import SubMax
