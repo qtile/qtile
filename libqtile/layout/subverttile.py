98bdb678a9092c5c19bc2b379cca74a2ed33c457
Ben Duffield <jebavarde@gmail.com>
2009-02-04 21:19:37 +0000

    Refactor SubVertTile - make sublayout use the parents' variables

--- libqtile/layout/subverttile.py
+++ libqtile/layout/subverttile.py
@@ -6,28 +6,23 @@ class SubVertTile(SubTile):
     arrangements = ["top", "bottom"]
     
     def _init_sublayouts(self):
-        ratio = self.ratio
-        expand = self.expand
-        master_windows = self.master_windows
-        arrangement = self.arrangement
-
         class MasterWindows(HorizontalStack):
             def filter(self, client):
-                return self.index_of(client) < master_windows
+                return self.index_of(client) < self.parent.master_windows
             def request_rectangle(self, r, windows):
                 return (r, Rect())
 
         class SlaveWindows(HorizontalStack):
             def filter(self, client):
-                return self.index_of(client) >= master_windows
+                return self.index_of(client) >= self.parent.master_windows
             def request_rectangle(self, r, windows):
                 if self.autohide and not windows:
                     return (Rect(), r)
                 else:
-                    if arrangement == "top":
-                        rmaster, rslave = r.split_horizontal(ratio=ratio)
+                    if self.parent.arrangement == "top":
+                        rmaster, rslave = r.split_horizontal(ratio=self.parent.ratio)
                     else:
-                        rslave, rmaster = r.split_horizontal(ratio=(1-ratio))
+                        rslave, rmaster = r.split_horizontal(ratio=(1-self.parent.ratio))
                     return (rslave, rmaster)
         
         self.sublayouts.append(SlaveWindows(self.clientStack,
