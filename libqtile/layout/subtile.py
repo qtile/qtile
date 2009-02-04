c76b24c321aa4db50b6652deb4c076915252e191
Ben Duffield <jebavarde@gmail.com>
2009-02-04 21:37:08 +0000

    Added incnmaster command to SubTile - increase (or decrease) the number of master windows

--- libqtile/layout/subtile.py
+++ libqtile/layout/subtile.py
@@ -59,11 +59,17 @@ class SubTile(SubLayout):
             ratio = self.command_get_arg(args, kwargs, 'ratio', self.ratio)
             self.ratio = ratio
             self.clientStack.group.layoutAll()
-        if command == 'incratio':
+        elif command == 'incratio':
             incr = self.command_get_arg(args, kwargs, 'incr', 0.1)
             self.ratio += incr
             if self.ratio < 0: self.ratio = 0
             if self.ratio > 1: self.ratio = 1.0
+            self.clientStack.group.layoutAll()
+        elif command == 'incnmaster':
+            incr = self.command_get_arg(args, kwargs, 'incr', 1)
+            self.master_windows += incr
+            if self.master_windows < 1:
+                self.master_windows = 1 #don't let it drop below one - not set up to cope with this yet
             self.clientStack.group.layoutAll()
         SubLayout.command(self, mask, command, *args, **kwargs)
         
