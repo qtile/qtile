138db7c32f92a3f72567db761c0f5c0e614ee809
Aldo Cortesi <aldo@nullcube.com>
2009-12-11 19:43:55 +1300

    Slow progress to xpyb. This is turning out to be more of a pain in the ass than I expected.

--- libqtile/xcbq.py
+++ libqtile/xcbq.py
@@ -1,5 +1,10 @@
-
+"""
+    A minimal OO layer over xpyb. This is NOT intended to be complete - it only
+    implements the subset of functionalty needed by qtile.
+"""
+import sys
 import xcb.xproto, xcb.xinerama
+from xcb.xproto import CW, WindowClass, EventMask
 import xcb
 
 def toStr(s):
@@ -48,6 +53,66 @@ class Xinerama:
         return r.screen_info
 
 
+class Window:
+    def __init__(self, conn, wid):
+        self.conn, self.wid = conn, wid
+
+    def _get_property(self, xa, type):
+        q = self.conn.core.GetProperty(
+                False,
+                self.wid,
+                xa,
+                xcb.xproto.GetPropertyType.Any,
+                0, (2**32)-1
+            )
+        return q.reply()
+            
+    def get_name(self):
+        r = self._get_property(
+                xcb.xcb.XA_WM_NAME
+                xcb.xproto.GetPropertyType.Any,
+            )
+        if r.value_len == 0:
+            return None
+        else:
+            # FIXME
+            return toString(r.value[0])
+
+    def get_hints(self):
+        r = self._get_property(xcb.xcb.XA_WM_HINTS)
+        print r.type
+        print dir(r)
+
+    def get_geometry(self):
+        q = self.conn.core.GetGeometry(self.wid)
+        return q.reply()
+
+    def _change_attributes(self, mask, lst):
+        self.conn.core.ChangeWindowAttributesChecked(
+            self.wid,
+            mask,
+            lst
+        )
+
+    def set_event_mask(self, x):
+        return self._change_attributes(CW.EventMask, x)
+
+    def set_back_pixmap(self, x): raise NotImplementedError
+    def set_back_pixel(self, x): raise NotImplementedError
+    def set_border_pixel(self, x): raise NotImplementedError
+    def set_border_pixmap(self, x): raise NotImplementedError
+    def set_bit_gravity(self, x): raise NotImplementedError
+    def set_win_gravity(self, x): raise NotImplementedError
+    def set_backing_storej(self, x): raise NotImplementedError
+    def set_backing_planes(self, x): raise NotImplementedError
+    def set_backing_pixel(self, x): raise NotImplementedError
+    def set_override_redirect(self, x): raise NotImplementedError
+    def set_save_under(self, x): raise NotImplementedError
+    def set_dont_propagate(self, x): raise NotImplementedError
+    def set_colormap(self, x): raise NotImplementedError
+    def set_cursor(self, x): raise NotImplementedError
+
+
 class Connection:
     _extmap = {
         "xinerama": Xinerama,
@@ -61,6 +126,23 @@ class Connection:
                 setattr(self, i, self._extmap[i](self))
         self.screens = [Screen(self, i) for i in self.setup.roots]
         self.default_screen = self.screens[self.conn.pref_screen]
+
+    def create_window(self, x, y, width, height):
+        wid = self.conn.generate_id()
+        q = self.conn.core.CreateWindowChecked(
+                self.default_screen.root_depth,
+                wid,
+                self.default_screen.root,
+                x, y, width, height, 0,
+                WindowClass.InputOutput,
+                self.default_screen.root_visual,
+                CW.BackPixel|CW.EventMask,
+                [
+                    self.default_screen.black_pixel,
+                    EventMask.StructureNotify|EventMask.Exposure
+                ]
+        )
+        return Window(self.conn, wid)
 
     def flush(self):
         return self.conn.flush()
