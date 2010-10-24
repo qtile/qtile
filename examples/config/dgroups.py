9157dc8410ca7da486b6f6879967f73f1cb49a40
roger <roger@rogerpc.com.ar>
2010-10-24 18:12:03 -0300

    added Dynamic Groups, example config in examples/config/roger-config.py

---
+++ examples/config/dgroups.py
@@ -0,0 +1,84 @@
+import libqtile.hook
+
+
+class Match(object):
+    ''' Match for dynamic groups
+        it can match by title, class or role '''
+    def __init__(self, title=[], wm_class=[], role=[]):
+        self._rules = [('title', t) for t in title]
+        self._rules += [('wm_class', w) for w in wm_class]
+        self._rules += [('role', r) for r in  role]
+
+    def compare(self, client):
+        for _type, rule in self._rules:
+            match_func = getattr(rule, 'match', None) or\
+                         getattr(rule, 'count')
+
+            if _type == 'title':
+                value = client.name
+            elif _type == 'wm_class':
+                value = client.window.get_wm_class()[1]
+            else:
+                value = client.window.get_wm_window_role()
+
+            print _type, value
+
+            if match_func(value):
+                return True
+        return False
+
+
+class DGroups(object):
+    ''' Dynamic Groups '''
+    def __init__(self, qtile, groups, apps, config=None):
+        self.qtile = qtile
+
+        self.groups = groups
+        self.apps = apps
+
+        self.config = config
+
+        self._setup_hooks()
+        self._setup_groups()
+
+    def _setup_groups(self):
+        for name, tag in self.groups.iteritems():
+            if tag.get('init') == True:
+                self.qtile.addGroup(name)
+
+            spawn_cmd = tag.get('spawn')
+            if spawn_cmd:
+                self.qtile.cmd_spawn(spawn_cmd)
+
+    def _setup_hooks(self):
+        libqtile.hook.subscribe.client_new(self._add)
+        libqtile.hook.subscribe.client_killed(self._del)
+
+    def _add(self, client):
+        for app in self.apps:
+            # Matching Rules
+            if app['match'].compare(client):
+                group = app['group']
+                self.qtile.addGroup(group)
+                client.togroup(group)
+                return
+
+        # Unmatched
+        current_group = self.qtile.currentGroup.name
+        if current_group in self.groups and\
+                self.groups.get(current_group, 'exclusive'):
+
+            wm_class = client.window.get_wm_class()
+
+            self.qtile.addGroup(wm_class[1])
+            client.togroup(wm_class[1])
+
+    def _del(self, client):
+        name = client.name
+        group = client.group
+
+        # Delete group if empty and no persist
+        if not (group.name in self.groups and\
+           self.groups[group.name].get('persist')) and\
+                               len(group.windows) == 1:
+            self.qtile.delGroup(group.name)
