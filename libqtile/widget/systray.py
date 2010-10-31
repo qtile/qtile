19ebc3d01faed9ee8f96b34b6fdd18f7644f012b
roger <roger@rogerpc.com.ar>
2010-10-31 03:15:20 -0300

    Working implementation of systemtray with dynamic width

--- libqtile/widget/systray.py
+++ libqtile/widget/systray.py
@@ -1,4 +1,4 @@
-from .. import bar, hook, utils, manager
+from .. import bar, hook, utils, manager, xcbq
 import cairo
 import base
 
@@ -10,31 +10,79 @@ class Systray(base._Widget):
     """
         A widget that manage system tray
     """
-    defaults = manager.Defaults()
+    defaults = manager.Defaults(
+                ('icon_size', 20, 'Icon width'),
+            )
     def __init__(self, **config):
         base._Widget.__init__(self, bar.CALCULATED, **config)
+        self.icons = {}
 
     def click(self, x, y):
         pass
 
     def calculate_width(self):
-        return 50
+        width = len(self.icons) * self.icon_size
+        return width
+
+    def handle_client_message(self, event):
+        atoms = self.qtile.conn.atoms
+
+        opcode = xcb.xproto.ClientMessageData(event, 0, 20).data32[2]
+        data = xcb.xproto.ClientMessageData(event, 12, 20)
+        task = data.data32[2]
+
+        conn = self.qtile.conn.conn
+        parent = self.bar.window.window
+
+        if opcode == atoms['_NET_SYSTEM_TRAY_OPCODE']:
+            w = xcbq.Window(self.qtile.conn, task)
+            self.icons[task] = w
+
+            self.qtile.windowMap[task] = w
+
+            w.handle_ConfigureNotify = self.handle_configure_notify
+            w.handle_DestroyNotify = self.handle_destroy_notify
+            w.handle_UnmapNotify = self.handle_destroy_notify
+
+            conn.core.ReparentWindow(task, parent.wid, 0, 0)
+            conn.core.ChangeWindowAttributes(
+                    task, xcb.xproto.CW.EventMask,
+                    [xcb.xproto.EventMask.Exposure|\
+                     xcb.xproto.EventMask.StructureNotify])
+
+            conn.flush()
+            w.map()
+        return False
+
+    def handle_configure_notify(self, event):
+        self.draw()
+        return False
+
+    def handle_destroy_notify(self, event):
+        wid = event.window
+        del(self.qtile.windowMap[wid])
+        del(self.icons[wid])
+        self.draw()
+        return False
+
+    def _configure_window(self, window, pos):
+        conn = self.qtile.conn.conn
+        window.configure(x=self.offset+(self.icon_size*pos),
+                    y=0, width=self.icon_size,
+                    height=self.icon_size)
 
     def _configure(self, qtile, bar):
         base._Widget._configure(self, qtile, bar)
+        self.qtile = qtile
+        self.bar = bar
 
         atoms = qtile.conn.atoms
 
         self.window = qtile.conn.create_window(-1, -1, 1, 1)
         qtile.windowMap[self.window.wid] = self.window
 
-        def handle(event):
-            print 'My Event', event
-            return True
 
-        self.window.handle_ClientMessage = handle
-        self.window.handle_ConfigureNotify = handle
-        self.window.handle_DestroyNotify = handle
+        self.window.handle_ClientMessage = self.handle_client_message
 
         qtile.conn.conn.core.SetSelectionOwner(self.window.wid,
                   atoms['_NET_SYSTEM_TRAY_S0'], xcb.CurrentTime)
@@ -47,4 +95,5 @@ class Systray(base._Widget):
         self.window.send_event(event)
 
     def draw(self):
-        pass
+        for pos, window in enumerate(self.icons.values()):
+            self._configure_window(window, pos)
