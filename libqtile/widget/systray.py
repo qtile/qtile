f84f27b9e9d426a496f73119339b09fa7a2272fe
Aldo Cortesi <aldo@nullcube.com>
2010-11-01 00:13:29 +1300

    Factor out systray Icon and Tray windows to use internal classes.
    This fixes a crashing bug when closing a window with a tray icon.

--- libqtile/widget/systray.py
+++ libqtile/widget/systray.py
@@ -1,9 +1,64 @@
-from .. import bar, hook, utils, manager, xcbq
+from .. import bar, hook, utils, manager, xcbq, window
 import cairo
 import base
 
 import xcb
+from xcb.xproto import EventMask
 import struct
+
+
+class Icon(window._Window):
+    _windowMask = EventMask.StructureNotify |\
+                  EventMask.Exposure
+    def __init__(self, win, qtile, systray):
+        window._Window.__init__(self, win, qtile)
+        self.systray = systray
+
+    def _configure_icon(self, pos):
+        window.configure(x=self.offset+(self.icon_size*pos),
+                    y=0, width=self.icon_size,
+                    height=self.icon_size)
+
+    def handle_ConfigureNotify(self, event):
+        self.systray.draw()
+        return False
+
+    def handle_DestroyNotify(self, event):
+        wid = event.window
+        del(self.qtile.windowMap[wid])
+        del(self.systray.icons[wid])
+        self.systray.draw()
+        return False
+
+    handle_UnmapNotify = handle_DestroyNotify
+
+
+class TrayWindow(window._Window):
+    _windowMask = EventMask.StructureNotify |\
+                  EventMask.Exposure
+    def __init__(self, win, qtile, systray):
+        window._Window.__init__(self, win, qtile)
+        self.systray = systray
+
+    def handle_ClientMessage(self, event):
+        atoms = self.qtile.conn.atoms
+
+        opcode = xcb.xproto.ClientMessageData(event, 0, 20).data32[2]
+        data = xcb.xproto.ClientMessageData(event, 12, 20)
+        task = data.data32[2]
+
+        conn = self.qtile.conn.conn
+        parent = self.systray.bar.window.window
+
+        if opcode == atoms['_NET_SYSTEM_TRAY_OPCODE']:
+            w = xcbq.Window(self.qtile.conn, task)
+            icon = Icon(w, self.qtile, self.systray)
+            self.systray.icons[task] = icon
+            self.qtile.windowMap[task] = icon
+            conn.core.ReparentWindow(task, parent.wid, 0, 0)
+            conn.flush()
+            w.map()
+        return False
 
 
 class Systray(base._Widget):
@@ -24,76 +79,32 @@ class Systray(base._Widget):
         width = len(self.icons) * self.icon_size
         return width
 
-    def handle_client_message(self, event):
-        atoms = self.qtile.conn.atoms
-
-        opcode = xcb.xproto.ClientMessageData(event, 0, 20).data32[2]
-        data = xcb.xproto.ClientMessageData(event, 12, 20)
-        task = data.data32[2]
-
-        conn = self.qtile.conn.conn
-        parent = self.bar.window.window
-
-        if opcode == atoms['_NET_SYSTEM_TRAY_OPCODE']:
-            w = xcbq.Window(self.qtile.conn, task)
-            self.icons[task] = w
-
-            self.qtile.windowMap[task] = w
-
-            w.handle_ConfigureNotify = self.handle_configure_notify
-            w.handle_DestroyNotify = self.handle_destroy_notify
-            w.handle_UnmapNotify = self.handle_destroy_notify
-
-            conn.core.ReparentWindow(task, parent.wid, 0, 0)
-            conn.core.ChangeWindowAttributes(
-                    task, xcb.xproto.CW.EventMask,
-                    [xcb.xproto.EventMask.Exposure|\
-                     xcb.xproto.EventMask.StructureNotify])
-
-            conn.flush()
-            w.map()
-        return False
-
-    def handle_configure_notify(self, event):
-        self.draw()
-        return False
-
-    def handle_destroy_notify(self, event):
-        wid = event.window
-        del(self.qtile.windowMap[wid])
-        del(self.icons[wid])
-        self.draw()
-        return False
-
-    def _configure_window(self, window, pos):
-        conn = self.qtile.conn.conn
-        window.configure(x=self.offset+(self.icon_size*pos),
-                    y=0, width=self.icon_size,
-                    height=self.icon_size)
-
     def _configure(self, qtile, bar):
         base._Widget._configure(self, qtile, bar)
         self.qtile = qtile
         self.bar = bar
-
         atoms = qtile.conn.atoms
-
-        self.window = qtile.conn.create_window(-1, -1, 1, 1)
-        qtile.windowMap[self.window.wid] = self.window
-
-
-        self.window.handle_ClientMessage = self.handle_client_message
-
-        qtile.conn.conn.core.SetSelectionOwner(self.window.wid,
-                  atoms['_NET_SYSTEM_TRAY_S0'], xcb.CurrentTime)
-
+        win = qtile.conn.create_window(-1, -1, 1, 1)
+        intwin = TrayWindow(win, self.qtile, self)
+        qtile.windowMap[win.wid] = intwin
+        qtile.conn.conn.core.SetSelectionOwner(
+            win.wid,
+            atoms['_NET_SYSTEM_TRAY_S0'],
+            xcb.CurrentTime
+        )
         event = struct.pack('BBHII5I', 33, 32, 0, qtile.root.wid,
                             atoms['MANAGER'], 
                             xcb.CurrentTime, atoms['_NET_SYSTEM_TRAY_S0'],
-                            self.window.wid, 0, 0)
+                            win.wid, 0, 0)
 
-        self.window.send_event(event)
+        win.send_event(event)
 
     def draw(self):
-        for pos, window in enumerate(self.icons.values()):
-            self._configure_window(window, pos)
+        for pos, icon in enumerate(self.icons.values()):
+            icon.place(
+                    self.offset+(self.icon_size*pos), 0, 
+                    self.icon_size, self.icon_size,
+                    0,
+                    None
+            )
+            
