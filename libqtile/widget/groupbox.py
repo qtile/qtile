0a94dd77a72b33c9a921e7f47b22deaf96ef1835
aldo <aldo@basho.(none)>
2010-11-04 11:55:09 +1300

    First pass of Pango GroupBox widget.

--- libqtile/widget/groupbox.py
+++ libqtile/widget/groupbox.py
@@ -1,99 +1,104 @@
 from .. import bar, hook, utils, manager
 import base
 
-class GroupBox(base._Widget):
-    """
-        A widget that graphically displays the group list, indicating which
-        groups have focus, and which groups contain clients.
-    """
-    defaults = manager.Defaults(
-        ("padding_y", 2, "Y padding outside the box"),
-        ("padding_x", 2, "X padding outside the box"),
-        ("borderwidth", 3, "Current group border width."),
-        ("font", "Monospace", "Font face"),
-        ("active", "FFFFFF", "Active group font colour"),
-        ("inactive", "404040", "Inactive group font colour"),
-        ("background", "000000", "Widget background"),
-        ("this_screen_border", "215578", "Border colour for group on this screen."),
-        ("other_screen_border", "404040", "Border colour for group on other screen."),
-        ("min_margin_x", 5, "Minimum X margin (inside the box)."),
-        ("urgent_border", "FF0000", "Urgent border color")
-    )
+class _GroupBase(base._Widget):
     def __init__(self, **config):
         base._Widget.__init__(self, bar.CALCULATED, **config)
 
-    def click(self, x, y):
-        groupOffset = int(x/self.boxwidth)
-        if len(self.qtile.groups) - 1 >= groupOffset:
-            self.bar.screen.setGroup(self.qtile.groups[groupOffset])
+    @property
+    def fontsize(self):
+        if self._fontsize is None:
+            return self.bar.height - self.margin_y*2 - self.borderwidth*2 - self.padding*2
+        else:
+            return self._fontsize
 
-    def calculate_width(self):
-        self.drawer.set_font(self.font, self.bar.height)
+    @fontsize.setter
+    def fontsize(self, value):
+        self._fontsize = value
 
-        # Leave a 10% margin top and bottom
-        self.margin_y = int((self.bar.height - (self.padding_y + self.borderwidth)*2)*0.2)
-        self.maxwidth, self.maxheight = self.drawer.fit_text(
+    def box_width(self):
+        width, height = self.drawer.max_layout_size(    
             [i.name for i in self.qtile.groups],
-            self.bar.height - (self.padding_y + self.margin_y + self.borderwidth)*2
+            self.font,
+            self.fontsize
         )
-        self.margin_x = max(self.min_margin_x, int(self.maxwidth * 0.2))
-        self.boxwidth = self.maxwidth + self.padding_x*2 + self.borderwidth*2 + self.margin_x*2
-        return int(self.boxwidth * len(self.qtile.groups))
+        return width + self.padding*2 + self.margin_x*2 + self.borderwidth*2
 
     def _configure(self, qtile, bar):
         base._Widget._configure(self, qtile, bar)
         self.setup_hooks()
 
-    def group_has_urgent(self, group):
-        return len([w for w in group.windows if w.urgent]) > 0
+    def setup_hooks(self):
+        def hook_response(*args, **kwargs):
+            self.bar.draw()
+        hook.subscribe.client_managed(hook_response)
+        hook.subscribe.client_urgent_hint_changed(hook_response)
+        hook.subscribe.client_killed(hook_response)
+        hook.subscribe.setgroup(hook_response)
+        hook.subscribe.group_window_add(hook_response)
+
+    def drawbox(self, offset, text, width=None):
+        layout = self.drawer.textlayout(text, self.foreground, self.font, self.fontsize)
+        if width is not None:
+            layout.width = width
+        framed = layout.framed(self.borderwidth, self.border, self.padding, self.padding)
+        framed.draw(offset, self.margin_y)
+
+
+class AGroupBox(_GroupBase):
+    """
+        A widget that graphically displays the current group.
+    """
+    defaults = manager.Defaults(
+        ("margin_y", 3, "Y margin outside the box"),
+        ("margin_x", 3, "X margin outside the box"),
+        ("borderwidth", 3, "Current group border width"),
+        ("font", "Monospace", "Font face"),
+        ("fontsize", None, "Font pixel size - calculated if None"),
+        ("foreground", "aaaaaa", "Active group font colour"),
+        ("background", "000000", "Widget background"),
+        ("border", "215578", "Border colour"),
+        ("padding", 5, "Padding inside the box")
+    )
+    def click(self, x, y):
+        self.bar.screen.group.cmd_nextgroup()
+
+    def calculate_width(self):
+        return self.box_width()
 
     def draw(self):
-        self.calculate_width()
         self.drawer.clear(self.background)
-        for i, e in enumerate(self.qtile.groups):
-            border = False
-            if e.screen:
-                if self.bar.screen.group.name == e.name:
-                    border = self.this_screen_border
-                else:
-                    border = self.other_screen_border
-            elif self.group_has_urgent(e):
-                border = self.urgent_border
-
-            if border:
-                self.drawer.ctx.set_source_rgb(*utils.rgb(border))
-                self.drawer.rounded_rectangle(
-                    (self.boxwidth * i) + self.padding_x, self.padding_y,
-                    self.boxwidth - 2*self.padding_x,
-                    self.bar.height - 2*self.padding_y,
-                    self.borderwidth
-                )
-                self.drawer.ctx.stroke()
-
-            # We could cache these...
-            if e.windows:
-                self.drawer.ctx.set_source_rgb(*utils.rgb(self.active))
-            else:
-                self.drawer.ctx.set_source_rgb(*utils.rgb(self.inactive))
-            # We use the x_advance value rather than the width.
-            _, _, _, y, x, _ = self.drawer.text_extents(e.name)
-            self.drawer.ctx.move_to(
-                (self.boxwidth*i) + self.boxwidth/2 - x/2,
-                self.bar.height/2.0 + self.maxheight/2
-            )
-            self.drawer.ctx.show_text(
-                e.name
-            )
-            self.drawer.ctx.stroke()
+        e = (i for i in self.qtile.groups if i.name == self.bar.screen.group.name ).next()
+        self.drawbox(self.margin_x, e.name)
         self.drawer.draw()
 
-    def setup_hooks(self):
-        def bardraw(*args, **kwargs):
-            self.bar.draw()
-        hook.subscribe.client_managed(bardraw)
-        hook.subscribe.client_urgent_hint_changed(bardraw)
-        hook.subscribe.client_killed(bardraw)
-        hook.subscribe.setgroup(bardraw)
-        hook.subscribe.delgroup(bardraw)
-        hook.subscribe.group_window_add(bardraw)
+
+class GroupBox(_GroupBase):
+    """
+        A widget that graphically displays the current group.
+    """
+    defaults = manager.Defaults(
+        ("margin_y", 3, "Y margin outside the box"),
+        ("margin_x", 3, "X margin outside the box"),
+        ("borderwidth", 3, "Current group border width"),
+        ("font", "Monospace", "Font face"),
+        ("fontsize", None, "Font pixel size - calculated if None"),
+        ("foreground", "aaaaaa", "Active group font colour"),
+        ("background", "000000", "Widget background"),
+        ("border", "215578", "Border colour"),
+        ("padding", 5, "Padding inside the box")
+    )
+    def __init__(self, **config):
+        base._Widget.__init__(self, bar.CALCULATED, **config)
+
+    def calculate_width(self):
+        return self.box_width() * len(self.qtile.groups)
+
+    def draw(self):
+        bw = self.box_width()
+        self.drawer.clear(self.background)
+        for i, g in enumerate(self.qtile.groups):
+            self.drawbox(self.margin_x + (bw*i), g.name, bw - self.margin_x*2 - self.padding*2)
+        self.drawer.draw()
+
 
