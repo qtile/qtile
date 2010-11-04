29a395f84a9941e72765551458556e635ae30669
Aldo Cortesi <aldo@nullcube.com>
2010-11-04 16:23:04 +1300

    More groupbox: font colours, mouse click group change.

--- libqtile/widget/groupbox.py
+++ libqtile/widget/groupbox.py
@@ -8,7 +8,8 @@ class _GroupBase(base._Widget):
     @property
     def fontsize(self):
         if self._fontsize is None:
-            return self.bar.height - self.margin_y*2 - self.borderwidth*2 - self.padding*2
+            calc = self.bar.height - self.margin_y*2 - self.borderwidth*2 - self.padding*2
+            return max(calc, 1)
         else:
             return self._fontsize
 
@@ -37,11 +38,11 @@ class _GroupBase(base._Widget):
         hook.subscribe.setgroup(hook_response)
         hook.subscribe.group_window_add(hook_response)
 
-    def drawbox(self, offset, text, color, width=None):
-        layout = self.drawer.textlayout(text, self.foreground, self.font, self.fontsize)
+    def drawbox(self, offset, text, bordercolor, textcolor, width=None):
+        layout = self.drawer.textlayout(text, textcolor, self.font, self.fontsize)
         if width is not None:
             layout.width = width
-        framed = layout.framed(self.borderwidth, color, self.padding, self.padding)
+        framed = layout.framed(self.borderwidth, bordercolor, self.padding, self.padding)
         framed.draw(offset, self.margin_y)
 
 
@@ -55,7 +56,7 @@ class AGroupBox(_GroupBase):
         ("borderwidth", 3, "Current group border width"),
         ("font", "Monospace", "Font face"),
         ("fontsize", None, "Font pixel size - calculated if None"),
-        ("foreground", "aaaaaa", "Active group font colour"),
+        ("foreground", "aaaaaa", "Font colour"),
         ("background", "000000", "Widget background"),
         ("border", "215578", "Border colour"),
         ("padding", 5, "Padding inside the box")
@@ -69,7 +70,7 @@ class AGroupBox(_GroupBase):
     def draw(self):
         self.drawer.clear(self.background)
         e = (i for i in self.qtile.groups if i.name == self.bar.screen.group.name ).next()
-        self.drawbox(self.margin_x, e.name, self.border)
+        self.drawbox(self.margin_x, e.name, self.border, self.foreground)
         self.drawer.draw()
 
 
@@ -78,12 +79,13 @@ class GroupBox(_GroupBase):
         A widget that graphically displays the current group.
     """
     defaults = manager.Defaults(
+        ("active", "FFFFFF", "Active group font colour"),
+        ("inactive", "404040", "Inactive group font colour"),
         ("margin_y", 3, "Y margin outside the box"),
         ("margin_x", 3, "X margin outside the box"),
         ("borderwidth", 3, "Current group border width"),
         ("font", "Monospace", "Font face"),
         ("fontsize", None, "Font pixel size - calculated if None"),
-        ("foreground", "aaaaaa", "Active group font colour"),
         ("background", "000000", "Widget background"),
         ("this_screen_border", "215578", "Border colour for group on this screen."),
         ("other_screen_border", "404040", "Border colour for group on other screen."),
@@ -91,6 +93,11 @@ class GroupBox(_GroupBase):
     )
     def __init__(self, **config):
         base._Widget.__init__(self, bar.CALCULATED, **config)
+
+    def click(self, x, y):
+        groupOffset = int((x-self.margin_x)/self.box_width())
+        if len(self.qtile.groups) - 1 >= groupOffset:
+            self.bar.screen.setGroup(self.qtile.groups[groupOffset])
 
     def calculate_width(self):
         return self.box_width() * len(self.qtile.groups)
@@ -112,10 +119,16 @@ class GroupBox(_GroupBase):
             else:
                 border = self.background
 
+            if g.windows:
+                text = self.active
+            else:
+                text = self.inactive
+
             self.drawbox(
                 self.margin_x + (bw*i),
                 g.name,
                 border,
+                text,
                 bw - self.margin_x*2 - self.padding*2
             )
         self.drawer.draw()
