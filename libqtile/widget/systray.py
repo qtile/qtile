006f6533459fbf013f1891640b54342ecf2a9aa3
dequis <dx@dxzone.com.ar>
2010-11-21 01:07:56 -0300

    Fix crashes on apps that use tray icons by adding them to the save-set
    Quoting awesome git commit 775ea16e by Uli Schlachter:
    
     When an X11 client disconnects from the X server, all of its windows are
     destroyed. This also means that all the sub-windows of those windows are
     destroyed which is not necessary what one wants to happen.
    
     To work around this, X11 has a "save-set". All the windows that are in the
     save-set are reparented to their closest ancestor when they would normally be
     destroyed. This is just what this code needs. :)

--- libqtile/widget/systray.py
+++ libqtile/widget/systray.py
@@ -3,7 +3,7 @@ import cairo
 import base
 
 import xcb
-from xcb.xproto import EventMask
+from xcb.xproto import EventMask, SetMode
 import atexit, struct
 
 
@@ -55,6 +55,11 @@ class TrayWindow(window._Window):
             icon = Icon(w, self.qtile, self.systray)
             self.systray.icons[task] = icon
             self.qtile.windowMap[task] = icon
+
+            # add icon window to the save-set, so it gets reparented
+            # to the root window when qtile dies
+            conn.core.ChangeSaveSet(SetMode.Insert, task)
+
             conn.core.ReparentWindow(task, parent.wid, 0, 0)
             conn.flush()
             w.map()
