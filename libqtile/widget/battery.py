5fa36a993dc9a041d19023c66d8c9e3c3bbb0f41
Chad Brubaker <pencilo.mmotl@gmail.com>
2011-04-14 13:56:15 -0600

    Add config options for files names to battery widget
    Some systems use different names in /sys/class/power_supply/<battery>/
    for the charge info so move the file names from hard coded to config options

--- libqtile/widget/battery.py
+++ libqtile/widget/battery.py
@@ -20,6 +20,11 @@ class Battery(base._TextBox):
         ("foreground", "ffffff", "Foreground colour"),
         ("format", "{char} {percent:2.0%} {hour:d}:{min:02d}", "Display format"),
         ("battery_name", "BAT0", "ACPI name of a battery, usually BAT0"),
+        ("status_file", "status", "Name of status file in /sys/class/power_supply/battery_name"),
+        ("energy_now_file", "energy_now", "Name of file with the current energy in /sys/class/power_supply/battery_name"),
+        ("energy_full_file", "energy_full", "Name of file with the maximum energy in /sys/class/power_supply/battery_name"),
+        ("power_now_file", "power_now", "Name of file with the current power draw in /sys/class/power_supply/battery_name"),
+        
     )
     def __init__(self, width=bar.CALCULATED, **config):
         base._TextBox.__init__(self, "BAT", **config)
@@ -29,10 +34,10 @@ class Battery(base._TextBox):
         self.timeout_add(1, self.update)
 
     def _get_info(self):
-        stat = self._get_param('status')
-        now = float(self._get_param('energy_now'))
-        full = float(self._get_param('energy_full'))
-        power = float(self._get_param('power_now'))
+        stat = self._get_param(self.status_file)
+        now = float(self._get_param(self.energy_now_file))
+        full = float(self._get_param(self.energy_full_file))
+        power = float(self._get_param(self.power_now_file))
 
         if stat == DISCHARGING:
             char = 'V'
