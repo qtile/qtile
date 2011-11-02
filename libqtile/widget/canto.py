831884caed194b3b866af6dce50e0f8f5ef63239
Kenji_Takahashi <wozniakk@gmail.com>
2011-11-02 23:45:22 +0100

    displaying separate feeds should work

--- libqtile/widget/canto.py
+++ libqtile/widget/canto.py
@@ -2,7 +2,7 @@
 
 from .. import bar, manager
 import base
-from subprocess import check_output
+from subprocess import check_output, call
 
 class Canto(base._TextBox):
     defaults = manager.Defaults(
@@ -13,21 +13,31 @@ class Canto(base._TextBox):
         ("foreground", "ffffff", "Foreground colour"),
         ("fetch", False, "Whether to fetch new items on update"),
         ("feeds", [], "List of feeds to display, empty for all"),
-        ("format", "{number}", "Display format"),
+        ("one_format", "{name}: {number}", "One feed display format"),
+        ("all_format", "{number}", "All feeds display format"),
         ("update_delay", 600, "The delay in seconds between updates"),
     )
+
     def __init__(self, width = bar.CALCULATED, **config):
-        base._TextBox.__init__(self, "0", **config)
+        base._TextBox.__init__(self, "0", width, **config)
 
     def _configure(self, qtile, bar):
         base._TextBox._configure(self, qtile, bar)
         self.timeout_add(self.update_delay, self.update)
 
     def _get_info(self):
-        arg = "-a"
-        if self.fetch:
-            arg += "u"
-        return self.format.format(number = check_output(["canto", arg]))
+        if not self.feeds:
+            arg = "-a"
+            if self.fetch:
+                arg += "u"
+            return self.all_format.format(number = check_output(["canto", arg])[:-1])
+        else:
+            if self.fGetch:
+                call(["canto", "-u"])
+            return "".join([self.one_format(
+                name = feed,
+                number = check_output(["canto", "-n '{1}'".format(feed)])[:-1]
+            ) for feed in self.feeds])
 
     def update(self):
         ntext = self._get_info()
