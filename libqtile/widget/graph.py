e65e11c953b2825d730c926ef2226bb0074fdfea
Mounier Florian <paradoxxx.zero@gmail.com>
2011-11-19 10:30:33 +0100

    Fix graph shift + pep8 ffs

--- libqtile/widget/graph.py
+++ libqtile/widget/graph.py
@@ -10,6 +10,7 @@ __all__ = [
     'NetGraph'
 ]
 
+
 class _Graph(base._Widget):
     fixed_upper_bound = False
     defaults = manager.Defaults(
@@ -27,49 +28,48 @@ class _Graph(base._Widget):
         ("start_pos", "bottom", "Drawer starting position ('bottom'/'top')")
     )
 
-    def __init__(self, width = 100, **config):
+    def __init__(self, width=100, **config):
         base._Widget.__init__(self, width, **config)
-        self.values = [0]*self.samples
+        self.values = [0] * self.samples
         self.maxvalue = 0
 
     @property
     def graphwidth(self):
-        return self.width - self.border_width*2 - self.margin_x*2
+        return self.width - self.border_width * 2 - self.margin_x * 2
 
     @property
     def graphheight(self):
-        return self.bar.height - self.margin_y*2 - self.border_width*2
+        return self.bar.height - self.margin_y * 2 - self.border_width * 2
 
     def draw_box(self, x, y, values):
-        step = self.graphwidth/float(self.samples)
+        step = self.graphwidth / float(self.samples)
         self.drawer.set_source_rgb(self.graph_color)
         for val in values:
             val = self.val(val)
-            self.drawer.fillrect(x, y-val, step, val)
-            x += step 
+            self.drawer.fillrect(x, y - val, step, val)
+            x += step
 
     def draw_line(self, x, y, values):
-        step = self.graphwidth/float(self.samples-1)
+        step = self.graphwidth / float(self.samples - 1)
         self.drawer.ctx.set_line_join(cairo.LINE_JOIN_ROUND)
         self.drawer.set_source_rgb(self.graph_color)
         self.drawer.ctx.set_line_width(self.line_width)
         for val in values:
-            self.drawer.ctx.line_to(x, y-self.val(val))
-            x += step 
+            self.drawer.ctx.line_to(x, y - self.val(val))
+            x += step
         self.drawer.ctx.stroke()
 
     def draw_linefill(self, x, y, values):
-        step = self.graphwidth/float(self.samples-1)
+        step = self.graphwidth / float(self.samples - 2)
         self.drawer.ctx.set_line_join(cairo.LINE_JOIN_ROUND)
         self.drawer.set_source_rgb(self.graph_color)
         self.drawer.ctx.set_line_width(self.line_width)
-        current = x
-        for val in values:
-            self.drawer.ctx.line_to(current, y-self.val(val))
-            current += step 
+        for index, val in enumerate(values):
+            self.drawer.ctx.line_to(x + index * step, y - self.val(val))
         self.drawer.ctx.stroke_preserve()
-        self.drawer.ctx.line_to(current, y - 1 + self.line_width/2.0)
-        self.drawer.ctx.line_to(x, y - 1 + self.line_width/2.0)
+        self.drawer.ctx.line_to(x + (len(values) - 1) * step,
+                                y - 1 + self.line_width / 2.0)
+        self.drawer.ctx.line_to(x, y - 1 + self.line_width / 2.0)
         self.drawer.set_source_rgb(self.fill_color)
         self.drawer.ctx.fill()
 
@@ -79,7 +79,7 @@ class _Graph(base._Widget):
         elif self.start_pos == 'top':
             return -val
         else:
-            raise ValueError("Unknown starting position: %s."%self.start_pos)
+            raise ValueError("Unknown starting position: %s." % self.start_pos)
 
     def draw(self):
         self.drawer.clear(self.background)
@@ -87,9 +87,10 @@ class _Graph(base._Widget):
             self.drawer.set_source_rgb(self.border_color)
             self.drawer.ctx.set_line_width(self.border_width)
             self.drawer.ctx.rectangle(
-                self.margin_x + self.border_width/2.0, self.margin_y + self.border_width/2.0,
+                self.margin_x + self.border_width / 2.0,
+                self.margin_y + self.border_width / 2.0,
                 self.graphwidth + self.border_width,
-                self.bar.height - self.margin_y*2 - self.border_width,
+                self.bar.height - self.margin_y * 2 - self.border_width,
             )
             self.drawer.ctx.stroke()
         x = self.margin_x + self.border_width
@@ -97,8 +98,8 @@ class _Graph(base._Widget):
         if self.start_pos == 'bottom':
             y += self.graphheight
         elif not self.start_pos == 'top':
-            raise ValueError("Unknown starting position: %s."%self.start_pos)
-        k = 1.0/(self.maxvalue or 1)
+            raise ValueError("Unknown starting position: %s." % self.start_pos)
+        k = 1.0 / (self.maxvalue or 1)
         scaled = [self.graphheight * val * k for val in reversed(self.values)]
 
         if self.type == "box":
@@ -108,7 +109,7 @@ class _Graph(base._Widget):
         elif self.type == "linefill":
             self.draw_linefill(x, y, scaled)
         else:
-            raise ValueError("Unknown graph type: %s."%self.type)
+            raise ValueError("Unknown graph type: %s." % self.type)
 
         self.drawer.draw(self.offset, self.width)
 
@@ -148,13 +149,13 @@ class CPUGraph(_Graph):
     def update_graph(self):
         nval = self._getvalues()
         oval = self.oldvalues
-        busy = (nval[0]+nval[1]+nval[2] - oval[0]-oval[1]-oval[2])
-        total = busy+nval[3]-oval[3]
+        busy = (nval[0] + nval[1] + nval[2] - oval[0] - oval[1] - oval[2])
+        total = busy + nval[3] - oval[3]
         if total:
             # sometimes this value is zero for unknown reason (time shift?)
             # we just skip the value, because it gives us no info about
             # cpu load, if it's zero
-            self.push(busy*100.0/total)
+            self.push(busy * 100.0 / total)
         self.oldvalues = nval
 
 
@@ -170,6 +171,7 @@ def get_meminfo():
 
 class MemoryGraph(_Graph):
     fixed_upper_bound = True
+
     def __init__(self, **config):
         _Graph.__init__(self, **config)
         val = self._getvalues()
@@ -188,6 +190,7 @@ class MemoryGraph(_Graph):
 
 class SwapGraph(_Graph):
     fixed_upper_bound = True
+
     def __init__(self, **config):
         _Graph.__init__(self, **config)
         val = self._getvalues()
@@ -208,6 +211,7 @@ class SwapGraph(_Graph):
             self.maxvalue = val['SwapTotal']
             self.fullfill(swap)
         self.push(swap)
+
 
 class NetGraph(_Graph):
     defaults = manager.Defaults(
@@ -230,8 +234,8 @@ class NetGraph(_Graph):
     def __init__(self, **config):
         _Graph.__init__(self, **config)
         self.filename = '/sys/class/net/{interface}/statistics/{type}'.format(
-            interface = self.interface,
-            type = self.bandwidth_type == 'down' and 'rx_bytes' or 'tx_bytes'
+            interface=self.interface,
+            type=self.bandwidth_type == 'down' and 'rx_bytes' or 'tx_bytes'
         )
         self.bytes = 0
         self.bytes = self._getValues()
