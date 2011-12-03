979274410d1445a0969043be1ed39dcab7fa0042
Anshuman Bhaduri <anshuman.bhaduri0@gmail.com>
2011-12-03 19:11:24 +1300

    Converted to using nose and code cleanup/refactoring when required:
      test_config.py
      test_hook.py
    
    --HG--
    branch : experimental

--- test/test_config.py
+++ test/test_config.py
@@ -1,16 +1,12 @@
-import libpry
 from libqtile import confreader, manager
+from nose.tools import raises
 
 
-class uConfig(libpry.AutoTree):
-    def test_syntaxerr(self):
-        libpry.raises("invalid syntax", confreader.File, "configs/syntaxerr.py")
+@raises(confreader.ConfigError)
+def test_syntaxerr():
+    confreader.File('configs/syntaxerr.py')
 
-    def test_basic(self):
-        f = confreader.File("configs/basic.py")
-        assert f.keys
-        
 
-tests = [
-    uConfig()
-]
+def test_basic():
+    f = confreader.File("configs/basic.py")
+    assert f.keys
--- test/test_hook.py
+++ test/test_hook.py
@@ -1,53 +1,66 @@
-import time
+from nose.tools import with_setup, raises
 import cStringIO
-import libpry
-import libqtile.manager, libqtile.hook
-import utils
+import libqtile.hook
+import libqtile.manager
 
-class uHook(libpry.AutoTree):
-    def tearDown(self):
-        libqtile.hook.clear()
-        
-    def setUpAll(self):
-        class Dummy: pass
-        dummy = Dummy()
-        io = cStringIO.StringIO()
-        dummy.log = libqtile.manager.Log(5, io)
-        libqtile.hook.init(dummy)
-        
-    def test_basic(self):
-        self.testVal = None
-        def test(x):
-            self.testVal = x
-
-        libpry.raises("unknown event", libqtile.hook.fire, "unkown")
-        libqtile.manager.hook.subscribe.group_window_add(test)
-        libqtile.manager.hook.fire("group_window_add", 1)
-        assert self.testVal == 1
-
-        assert libqtile.manager.hook.subscriptions
-        libqtile.manager.hook.clear()
-        assert not libqtile.manager.hook.subscriptions
-
-    def test_unsubscribe(self):
-        self.testVal = None
-        def test(x):
-            self.testVal = x
-
-        libqtile.manager.hook.subscribe.group_window_add(test)
-        libqtile.manager.hook.fire("group_window_add", 3)
-        assert self.testVal == 3
-
-        libqtile.manager.hook.unsubscribe.group_window_add(test)
-        libqtile.manager.hook.fire("group_window_add", 4)
-        assert self.testVal == 3
-
-        libqtile.manager.hook.clear()
-        assert not libqtile.manager.hook.subscriptions
+# TODO: more tests required.
+# 1. Check all hooks that can be fired
 
 
+def setup_module():
+    class Dummy:
+        pass
 
-tests = [
-    uHook(),
-]
+    dummy = Dummy()
+    io = cStringIO.StringIO()
+    dummy.log = libqtile.manager.Log(5, io)
+    libqtile.hook.init(dummy)
 
+
+def teardown_func():
+    libqtile.hook.clear()
+
+
+@raises(libqtile.manager.QtileError)
+def test_firing_unknown_event():
+    libqtile.hook.fire("unknown")
+
+
+@with_setup(None, teardown_func)
+def test_subscriber_called():
+    test_list = []
+
+    def test(list):
+        list.append(1)
+
+    libqtile.manager.hook.subscribe.group_window_add(test)
+    libqtile.manager.hook.fire("group_window_add", test_list)
+    assert len(test_list) == 1
+    assert 1 in test_list
+
+
+@with_setup(None, teardown_func)
+def test_subscriber_addition_removal():
+
+    def test(list):
+        pass
+
+    libqtile.manager.hook.subscribe.group_window_add(test)
+    assert libqtile.manager.hook.subscriptions
+    libqtile.manager.hook.clear()
+    assert not libqtile.manager.hook.subscriptions
+
+
+@with_setup(None, teardown_func)
+def test_unsubscribe():
+    test_list = []
+
+    def test(list):
+        list.append(len(list) + 1)
+
+    libqtile.manager.hook.subscribe.group_window_add(test)
+    libqtile.manager.hook.fire("group_window_add", test_list)
+    libqtile.manager.hook.unsubscribe.group_window_add(test)
+    libqtile.manager.hook.fire("group_window_add", test_list)
+    assert len(test_list) == 1
+    assert 1 in test_list
