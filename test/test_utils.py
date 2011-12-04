04417f45c0eea945ca8ee16fb49e00d2aad0b229
Anshuman Bhaduri <anshuman.bhaduri0@gmail.com>
2011-12-04 19:20:47 +1300

    Converted test_utils to nose. Split up some tests.
    --HG--
    branch : experimental

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,55 +1,88 @@
-import libpry
 import libqtile.utils as utils
 
-
-class utranslateMasks(libpry.AutoTree):
-    def test_one(self):
-        assert utils.translateMasks(["shift", "control"])
-        assert utils.translateMasks([]) == 0
+# TODO: more tests are required here. Several of the utilities are untested.
 
 
-class uLRUCache(libpry.AutoTree):
-    def test_one(self):
-        class Foo:
-            ran = False
-            @utils.LRUCache(2)
-            def one(self, x):
-                self.ran = True
-                return x
+class Foo:
+    ran = False
 
-        f = Foo()
-        assert f.one(1) == 1
-        assert f.ran
-        f.ran = False
-        assert f.one(1) == 1
-        assert not f.ran
-
-        f.ran = False
-        assert f.one(1) == 1
-        assert not f.ran
-        assert f.one(2) == 2
-        assert f.one(3) == 3
-        assert f.ran
-
-        f.ran = False
-        assert f.one(1) == 1
-        assert f.ran
-
-        assert len(f._cached_one) == 2
-        assert len(f._cachelist_one) == 2
+    @utils.LRUCache(2)
+    def one(self, x):
+        self.ran = True
+        return x
 
 
-class uRGB(libpry.AutoTree):
-    def test_rgb(self):
-        assert utils.rgb([255, 255, 0, 0.5]) == (1, 1, 0, 0.5)
-        assert utils.rgb([255, 255, 0]) == (1, 1, 0, 1)
-        assert utils.rgb("ff0000") == (1, 0, 0, 1)
-        assert utils.rgb("ff0000.5") == (1, 0, 0, 0.5)
+def test_translate_masks_one():
+    assert utils.translateMasks(["shift", "control"])
+    assert utils.translateMasks([]) == 0
 
 
-tests = [
-    utranslateMasks(),
-    uLRUCache(),
-    uRGB(),
-]
+def test_lrucache_decorator():
+    f = Foo()
+    assert f.one(1) == 1
+    assert f.one('test') == 'test'
 
+
+def test_lrucache_caches():
+    f = Foo()
+    f.one(1)
+    f.one(2)
+    f.ran = False
+    f.one(1)
+    assert not f.ran
+    f.one(2)
+    assert not f.ran
+
+
+def test_lrucache_discard_lru():
+    f = Foo()
+    f.one(1)
+    assert f.ran
+    f.ran = False
+    f.one(1)
+    assert not f.ran
+    f.one(2)
+    f.one(3)
+    f.one(1)
+    assert f.ran
+
+
+def test_lrucache_size():
+    f = Foo()
+    f.one(1)
+    f.one(2)
+    f.one(3)
+    assert len(f._cached_one) == 2
+    assert len(f._cachelist_one) == 2
+
+
+def test_rgb_hex():
+    assert utils.rgb("ff00ff") == (1, 0, 1, 1)
+
+
+def test_rgb_hex_string():
+    assert utils.rgb("#00ff00") == (0, 1, 0, 1)
+
+
+def test_rgb_hex_alpha():
+    assert utils.rgb("ff0000.3") == (1, 0, 0, 0.3)
+
+
+def test_rgb_hex_string_alpha():
+    assert utils.rgb("#ff0000.5") == (1, 0, 0, 0.5)
+
+
+def test_rgb_tuple():
+    assert utils.rgb([255, 255, 0]) == (1, 1, 0, 1)
+
+
+def test_rgb_tuple_alpha():
+    assert utils.rgb([255, 255, 0, 0.5]) == (1, 1, 0, 0.5)
+
+# TODO: test scrub to utf8
+# TODO: test Data
+# TODO: test issequencelike
+# TODO: test isstringlike
+# TODO: test shuffleUp, shuffleDown
+# Probably do not require a whole lot of tests, but at least one for each
+# function so that we can refactor with confidence.
