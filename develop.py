dcf184d299bf95db227c9c814c6683b3af0d3ba8
Anshuman Bhaduri <anshuman.bhaduri0@gmail.com>
2011-12-26 14:22:52 +1300

    Added develop.py to assist in setting up a virtualenv to run qtile. The script removes the dependence on global site-packages by installing each one of qtile's requirements from source.
    --HG--
    branch : experimental

---
+++ develop.py
@@ -0,0 +1,258 @@
+import subprocess as sp
+import os
+import sys
+
+VENV = None
+DEST_DIR = None
+#SITE_PACKAGES = None
+
+
+def xorg_util_macros():
+    url = 'git://anongit.freedesktop.org/git/xorg/util/macros'
+    version = 'master'
+    
+    aclocal = os.environ['ACLOCAL']
+    export('ACLOCAL', '')
+    
+    get_git_src(url, 'xorg_util_macros', version)
+    autogen()
+    configure()
+    make_make_install()
+    
+    export('ACLOCAL', aclocal)
+
+
+def libxau():
+    url = 'git://anongit.freedesktop.org/git/xorg/lib/libXau'
+    version = 'master'
+    
+    get_git_src(url, 'libxau', version)
+    autogen()
+    configure()
+    make_make_install()
+
+
+def libxdmcp():
+    url = 'git://anongit.freedesktop.org/git/xorg/lib/libXdmcp'
+    version = 'master'
+    
+    get_git_src(url, 'libxdmcp', version)
+    autogen()
+    configure()
+    make_make_install()
+
+def pthread_stubs():
+    url = 'git://anongit.freedesktop.org/git/xcb/pthread-stubs'
+    version = 'master'
+    
+    get_git_src(url, 'pthread_stubs', version)
+    autogen()
+    configure()
+    make_make_install()
+
+def xcb_proto():
+    url = 'git://anongit.freedesktop.org/git/xcb/proto'
+    # http://groups.google.com/group/qtile-dev/browse_thread/thread/1a184316e991a119
+    version = '661fe8dd7727c27467e61a0d20dc91557ce3747f'
+    
+    get_git_src(url, 'xcb_proto', version)
+    autogen()
+    configure()
+    make_make_install()
+
+def libxcb():
+    url = 'git://anongit.freedesktop.org/git/xcb/libxcb'
+    #version = '8ecd754b168a0352783bf1ba0f0887f7ff479ee8'
+    version = '8c2707773b3621fb8bbda9021d23944f5be34aab'
+    # First commit that doesn't work: 80322d11636dd638902660d80481080d2fad40fe
+    
+    get_git_src(url, 'libxcb', version)
+    autogen()
+    configure('--enable-xinput')
+    make_make_install()
+
+def xcb_util():
+    # http://aur.archlinux.org/packages/xc/xcb-util-git/PKGBUILD
+    url = 'git://anongit.freedesktop.org/git/xcb/util'
+    version = 'master'
+    
+    get_git_src(url, 'xcb_util', version, cloneargs=['--recursive'])
+    autogen()
+    configure()
+    make_make_install()
+
+def pixman():
+    url = 'git://anongit.freedesktop.org/git/pixman.git'
+    version = 'master'
+    
+    get_git_src(url, 'pixman', version)
+    autogen()
+    configure()
+    make_make_install()
+    
+    
+def cairo_xcb():
+    # http://aur.archlinux.org/packages/ca/cairo-xcb/PKGBUILD
+    url = 'git://anongit.freedesktop.org/git/cairo'
+    version = 'master'
+
+    get_git_src(url, 'cairo_xcb', version)
+    autogen()
+    configure('--enable-xcb')
+    make_make_install()
+
+
+def xpyb_ng():
+    pip_install('-e git://github.com/dequis/xpyb-ng.git#egg=xpyb')
+
+
+def py2cairo_xcb():
+    # http://aur.archlinux.org/packages/py/pycairo-xcb-git/PKGBUILD
+    url = 'git://git.cairographics.org/git/py2cairo'
+    version = 'master'
+    
+    get_git_src(url, 'py2cairo_xcb', version)
+    autogen()
+    configure('--enable-xcb')
+    make_make_install()
+
+
+#def glib2():
+#    # http://aur.archlinux.org/packages/gl/glib2-git/PKGBUILD
+#    url = 'git://git.gnome.org/glib'
+#    version = 'master'
+#
+#    get_git_src(url, 'glib2', version)
+#    autogen()
+#    configure()
+#    make_make_install()
+
+
+#def gobject_introspection():
+#    # https://aur.archlinux.org/packages/go/gobject-introspection-git/PKGBUILD
+#    url = 'git://git.gnome.org/gobject-introspection'
+#    version = 'GOBJECT_INTROSPECTION_1_30_0'
+#    
+#    get_git_src(url, 'gobject_introspection', version)
+#    autogen()
+#    configure()
+#    make_make_install()
+
+
+def pygobject():
+    url = 'git://git.gnome.org/pygobject'
+    version = 'PYGOBJECT_2_28_6'
+    
+    get_git_src(url, 'pygobject', version)
+    autogen()
+    configure('--disable-introspection')
+    make_make_install()
+
+
+def pygtk():
+    url = 'git://git.gnome.org/pygtk'
+    version = 'master'
+
+    get_git_src(url, 'pygtk', version)
+    autogen()
+    configure()
+    make_make_install()
+
+def python_xlib():
+    pip_install('http://downloads.sourceforge.net/python-xlib/python-xlib-0.15rc1.tar.gz')
+    pass
+
+
+def pip_requirements_file():
+    os.chdir(os.path.abspath(__file__))
+    pip_install('-r requirements.txt')
+
+
+def pip_install(*args):
+    call('pip install {0}'.format(' '.join(args)))
+
+
+def export(key, value):
+    print 'export {0}={1}'.format(key, value)
+    os.environ[key] = value
+
+
+def get_git_src(url, destination, revision, cloneargs=[], checkoutargs=[]):
+    print '***********', destination
+    # If directory exists checkout, else clone and checkout.
+    d = dest(destination)
+    if not os.path.exists(d):
+        git_clone(url, d, *cloneargs)
+
+    os.chdir(d)
+    git_checkout(revision, *checkoutargs)
+    
+
+def git_clone(url, dest, *args):
+    call('git clone {0} {1} {2}'.format(url, dest, ' '.join(args)))
+
+
+def git_checkout(revision, *args):
+    call('git checkout {0} {1}'.format(revision, ' '.join(args)))
+
+
+def dest(d):
+    return os.path.join(DEST_DIR, d)
+
+
+def autogen(*args):
+    call('./autogen.sh {0}'.format(' '.join(args)))
+
+
+def configure(*args):
+    call('./configure --prefix={0} {1}'.format(VENV, ' '.join(args)))
+
+
+def make_make_install():
+    call('make')
+    call('make install')
+
+def call(cmd):
+    print cmd
+    #out = 0
+    out = sp.call(cmd, shell=True)
+    if out != 0:
+        raise Exception('Ah! $@#%^&!! Error!!')
+    return out
+
+def main():
+    global VENV
+    global DEST_DIR
+    #global SITE_PACKAGES
+
+    # Assumes the virtualenv has been activated.
+    VENV = os.environ['VIRTUAL_ENV']
+    DEST_DIR = os.path.join(VENV, 'src')
+    #SITE_PACKAGES = os.path.join(VENV,
+    #    'lib',
+    #    'python{0}.{1}'.format(sys.version_info.major, sys.version_info.minor),
+    #    'site-packages')
+    bin = os.path.join(VENV, 'bin')
+    python = os.path.join(bin, 'python')
+    lib = os.path.join(VENV, 'lib')
+    pkgconfig = os.path.join(lib, 'pkgconfig')
+    aclocal = os.path.join(VENV, 'share', 'aclocal')
+
+    # All of Qtile's requirements, including those in the pip requirements
+    # file.
+    reqs = [xorg_util_macros, libxau, libxdmcp, pthread_stubs, xcb_proto,
+            libxcb, xcb_util, xpyb_ng, pixman, cairo_xcb, py2cairo_xcb,
+            pygobject, pygtk, python_xlib, pip_requirements_file]
+    
+    export('ACLOCAL', 'aclocal -I {0}'.format(aclocal))
+    export('PKG_CONFIG_PATH', pkgconfig)
+    export('LD_LIBRARY_PATH', lib)
+    export('PYTHON', python)
+    
+    for r in reqs:
+        r()
+
+
+if __name__ == '__main__':
+    main()
+
