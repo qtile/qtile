250049d807c5c47e97209900512423e5daec6d8e
Tycho Andersen <tycho@tycho.ws>
2011-12-28 09:33:10 -0600

    Support changing text color on low battery
    By default, the battery widget now turns red when the battery has less than 10%
    of its life remaining.

--- libqtile/widget/battery.py
+++ libqtile/widget/battery.py
@@ -18,6 +18,7 @@ class Battery(base._TextBox):
         ("padding", None, "Battery widget padding. Calculated if None."),
         ("background", "000000", "Background colour"),
         ("foreground", "ffffff", "Foreground colour"),
+        ("low_foreground", "FF0000", "font when battery is low"),
         ("format", "{char} {percent:2.0%} {hour:d}:{min:02d}", "Display format"),
         ("battery_name", "BAT0", "ACPI name of a battery, usually BAT0"),
         ("status_file", "status", "Name of status file in /sys/class/power_supply/battery_name"),
@@ -29,8 +30,9 @@ class Battery(base._TextBox):
         ("discharge_char","V","Character to indicate the battery is discharging"),
         
     )
-    def __init__(self, width=bar.CALCULATED, **config):
+    def __init__(self, low_percentage=0.10, width=bar.CALCULATED, **config):
         base._TextBox.__init__(self, "BAT", **config)
+        self.low_percentage = low_percentage
 
     def _configure(self, qtile, bar):
         base._TextBox._configure(self, qtile, bar)
@@ -53,8 +55,13 @@ class Battery(base._TextBox):
 
         hour = int(time)
         min = int(time*60) % 60
+        percent = now / full
+        if stat == DISCHARGING and percent < self.low_percentage:
+            self.layout.colour = self.low_foreground
+        else:
+            self.layout.colour = self.foreground
         return self.format.format(char=char,
-                           percent=now/full,
+                           percent=percent,
                            hour=hour, min=min)
 
     def update(self):
