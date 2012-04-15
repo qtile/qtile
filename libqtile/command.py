059f5f0f9a62182dd483a0f82886f27ec4e6967e
Tycho Andersen <tycho@tycho.ws>
2012-04-15 11:04:15 -0500

    Create the cache directory if necessary
    Fixes #117

--- libqtile/command.py
+++ libqtile/command.py
@@ -210,6 +210,8 @@ def find_sockfile(display=None):
     cache_directory = os.path.expandvars('$XDG_CACHE_HOME')
     if cache_directory == '$XDG_CACHE_HOME': #if variable wasn't set
         cache_directory = os.path.expanduser("~/.cache")
+    if not os.path.exists(cache_directory):
+        os.makedirs(cache_directory)
     return os.path.join(cache_directory, SOCKBASE%display)
 
 
