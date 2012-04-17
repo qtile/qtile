d1be6dcdd9d6c44f0d590067b79e753c912adb20
uberj <uberj@onid.orst.edu>
2012-04-17 16:40:21 -0700

    when switching the screen, fire the layout_change hook.

--- libqtile/manager.py
+++ libqtile/manager.py
@@ -1346,6 +1346,8 @@ class Qtile(command.CommandObject):
             self.currentWindow,
             True
         )
+        currentLayout = self.currentGroup.layouts[self.currentGroup.currentLayout]
+        hook.fire("layout_change", currentLayout)
 
     def moveToGroup(self, group):
         """
