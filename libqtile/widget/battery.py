fe091a31cb79320912201af83e0e61bbc3591eea
Tycho Andersen <tycho@tycho.ws>
2012-05-05 13:26:50 -0500

    Better error message for battery widget
    Battery widget now displays "Battery files not found" when it can't read the
    battery files on the machine, rather than causing it to crash and be stuck on
    "BAT" forever.

--- libqtile/widget/battery.py
+++ libqtile/widget/battery.py
@@ -39,10 +39,13 @@ class Battery(base._TextBox):
         self.timeout_add(self.update_delay, self.update)
 
     def _get_info(self):
-        stat = self._get_param(self.status_file)
-        now = float(self._get_param(self.energy_now_file))
-        full = float(self._get_param(self.energy_full_file))
-        power = float(self._get_param(self.power_now_file))
+        try:
+            stat = self._get_param(self.status_file)
+            now = float(self._get_param(self.energy_now_file))
+            full = float(self._get_param(self.energy_full_file))
+            power = float(self._get_param(self.power_now_file))
+        except IOError:
+            return "Battery files not found"
 
         try:
             if stat == DISCHARGING:
