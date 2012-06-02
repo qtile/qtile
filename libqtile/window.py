3041b7c9cb68aebd4a8f77d70fcbd37536592989
Ilya Voronin <ivoronin@gmail.com>
2012-06-02 15:49:29 +0400

    added support for WM_TAKE_FOCUS protocol
    @tych0: Closes #132

--- libqtile/window.py
+++ libqtile/window.py
@@ -430,8 +430,24 @@ class _Window(command.CommandObject):
             )
 
     def focus(self, warp):
-        if not self.hidden and self.hints['input']:
-            self.window.set_input_focus()
+        if not self.hidden:
+            if "WM_TAKE_FOCUS" in self.window.get_wm_protocols():
+                vals = [
+                    33,
+                    32,
+                    0,
+                    self.window.wid,
+                    self.qtile.conn.atoms["WM_PROTOCOLS"],
+                    self.qtile.conn.atoms["WM_TAKE_FOCUS"],
+                    xcb.xproto.Time.CurrentTime,
+                    0,
+                    0,
+                    0,
+                ]
+                e = struct.pack('BBHII5I', *vals)
+                self.window.send_event(e)
+            if self.hints['input']:
+                    self.window.set_input_focus()
             try:
                 if warp and self.qtile.config.cursor_warp:
                     self.window.warp_pointer(self.width // 2, self.height // 2)
