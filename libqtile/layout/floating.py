3125bc2b3406bb9fcc4a4736ba32e77f0f3faf9c
Craig Barnes <cjbarnes18@gmail.com>
2012-11-03 13:23:50 +0000

    Fix typo.

--- libqtile/layout/floating.py
+++ libqtile/layout/floating.py
@@ -87,7 +87,7 @@ class Floating(Layout):
             while new_x > right_edge:
                 new_x = (new_x - new_screen.x) / 2
             while new_y > bottom_edge:
-                new_y = (new_y - new_y.y) / 2
+                new_y = (new_y - new_screen.y) / 2
             win.x = new_x
             win.y = new_y
             win.group = new_screen.group
