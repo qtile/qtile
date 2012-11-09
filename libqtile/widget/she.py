7d1085a1c70963e8d965ea57199265f2835e5932
Craig Barnes <cjbarnes18@gmail.com>
2012-11-09 20:46:35 +0000

    Add Super Hybrid Engine status widget.
    Closes #191

---
+++ libqtile/widget/she.py
@@ -0,0 +1,53 @@
+from libqtile.widget import base
+from libqtile import manager, bar, hook
+
+__all__ = ['She']
+
+
+class She(base._TextBox):
+    ''' Widget to display the Super Hybrid Engine status.
+    can display either the mode or CPU speed on eeepc computers.'''
+
+    defaults = manager.Defaults(
+        ('font', 'Arial', 'Text Font'),
+        ('fontsize', None, 'Calculated if None.'),
+        ('padding', None, 'Padding Left and Right. Calculated in None.'),
+        ('background', '000000', 'Background Colour'),
+        ('foreground', 'ffffff', 'Foreground Colour'),
+        ('device', '/sys/devices/platform/eeepc/cpufv', 'sys path to cpufv'),
+        ('format', 'speed', 'Type of info to display "speed" or "name"'),
+        ('update_delay', 0.5, 'Update Time in seconds.'),
+    )
+
+    def __init__(self, width=bar.CALCULATED, **config):
+        base._TextBox.__init__(self, 'CPU', **config)
+        self.modes = {
+            '0x300': {'name': 'Performance', 'speed': '1.6GHz'},
+            '0x301': {'name': 'Normal', 'speed': '1.2GHz'},
+            '0x302': {'name': 'PoswerSave', 'speed': '800MHz'}
+        }
+        self.modes_index = self.modes.keys().sort()
+        self.mode = None
+
+    def _configure(self, qtile, bar):
+        base._TextBox._configure(self, qtile, bar)
+        self.timeout_add(self.update_delay, self.update)
+
+    def _get_mode(self):
+        with open(self.device) as f:
+            mode = f.read().strip()
+        return mode
+
+    def update(self):
+        mode = self._get_mode()
+        if mode != self.mode:
+            self.mode = mode
+            self.draw()
+        return True
+
+    def draw(self):
+        if self.mode in self.modes.keys():
+            self.text = self.modes[self.mode][self.format]
+        else:
+            self.text = self.mode
+        base._TextBox.draw(self)
