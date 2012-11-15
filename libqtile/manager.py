3c90d4d006ac4c48c626ad0e1c39214dd058cfe1
Tycho Andersen <tycho@tycho.ws>
2012-11-15 14:31:46 -0600

    restart when number of screens changes
    Closes #32

--- libqtile/manager.py
+++ libqtile/manager.py
@@ -1375,6 +1375,9 @@ class Qtile(command.CommandObject):
         if e.event != self.root.wid:
             self.unmanage(e.window)
 
+    def handle_ScreenChangeNotify(self, e):
+        self.cmd_restart()
+
     def toScreen(self, n):
         """
         Have Qtile move to screen and put focus there
