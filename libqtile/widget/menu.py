d4f705a0753d8e73b0e9f797cdb8b416f799fff3
Calvin Kosmatka <kiwi940@gmail.com>
2013-01-28 10:21:49 -0600

    Revert "Stupid stuff... spacing, marked misnomer of _MenuMarkup.entries"
    This reverts commit 1067e9dc2c8c8616623ebadc7c8742622fc5d111.

--- libqtile/widget/menu.py
+++ libqtile/widget/menu.py
@@ -8,17 +8,16 @@ class _MenuMarkup():
 
 	For example _MenuMarkup(["File",["New",[[".py",newpyfunc],[".c",newcfunc]]],["Quit",quitfunc]],
 		["Edit",["Copy",copyfunc],["Paste",pastefunc]])
-	Represents the menu with File and Edit as top level entries where file has New and Quit as
-	submenu entries, and Edit has Copy and Paste as submenu entries.
-
-	In addition, New has .py and .c as submenu entries.
+	Represents the menu with File and Edit as top level entries where file has New and Quit as submenu entries,
+	and Edit has Copy and Paste as submenu entries. In addition, New has .py and .c as submenu entries.
 
 	All of the submenu entries, when clicked, call their corresponding functions.
 	"""
 	def __init__(self, *entries):
 		self.tree = entries
 		self.names = [i[0] for i in entries]
-		self.entries = [i[1:] for i in entries] #misnomer
+		self.entries = [i[1:] for i in entries]
+		print self.entries
 
 class _Menu(base._Widget):
 	defaults = manager.Defaults()
@@ -184,7 +183,7 @@ class SampleMenu(_Menu):
 	"""
 	def __init__(self):
 		_Menu.__init__(self, _MenuMarkup(["ab",["a"],["b"],["c"]],["cats",["doop"], ["loop"], ["hoop"]]))
-	
+
 
 ##Make a debian/gnome menu class which opens programs like the gnome2 menu##
 
