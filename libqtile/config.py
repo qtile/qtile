5ad9d8577721a14b952d361653f69425ece7749f
Tao Sauvage <sauvage.tao@gmail.com>
2013-06-25 00:31:02 +0200

    still pep8 clean

--- libqtile/config.py
+++ libqtile/config.py
@@ -16,7 +16,9 @@ class Key:
             command.lazy helper. If multiple Call objects are specified, they
             are run in sequence.
         """
-        self.modifiers, self.key, self.commands = modifiers, key, commands
+        self.modifiers = modifiers
+        self.key = key
+        self.commands = commands
         if key not in xcbq.keysyms:
             raise utils.QtileError("Unknown key: %s" % key)
         self.keysym = xcbq.keysyms[key]
@@ -80,22 +82,32 @@ class ScreenRect(object):
         self.height = height
 
     def __repr__(self):
-        return '<%s %d,%d %d,%d>' % (self.__class__.__name__,
-            self.x, self.y, self.width, self.height)
+        return '<%s %d,%d %d,%d>' % (
+                self.__class__.__name__,
+                self.x, self.y, self.width, self.height
+        )
 
     def hsplit(self, columnwidth):
         assert columnwidth > 0
         assert columnwidth < self.width
-        return (self.__class__(self.x, self.y, columnwidth, self.height),
-                self.__class__(self.x + columnwidth, self.y,
-                               self.width - columnwidth, self.height))
+        return (
+                self.__class__(self.x, self.y, columnwidth, self.height),
+                self.__class__(
+                    self.x + columnwidth, self.y,
+                    self.width - columnwidth, self.height
+                )
+        )
 
     def vsplit(self, rowheight):
         assert rowheight > 0
         assert rowheight < self.height
-        return (self.__class__(self.x, self.y, self.width, rowheight),
-                self.__class__(self.x, self.y + rowheight,
-                               self.width, self.height - rowheight))
+        return (
+                self.__class__(self.x, self.y, self.width, rowheight),
+                self.__class__(
+                    self.x, self.y + rowheight,
+                    self.width, self.height - rowheight
+                )
+        )
 
 
 class Screen(command.CommandObject):
@@ -116,8 +128,10 @@ class Screen(command.CommandObject):
             x,y,width and height aren't specified usually unless you are
             using 'fake screens'.
         """
-        self.top, self.bottom = top, bottom
-        self.left, self.right = left, right
+        self.top = top
+        self.bottom = bottom
+        self.left = left
+        self.right = right
         self.qtile = None
         self.index = None
         self.x = x  # x position of upper left corner can be > 0
@@ -128,8 +142,11 @@ class Screen(command.CommandObject):
 
     def _configure(self, qtile, index, x, y, width, height, group):
         self.qtile = qtile
-        self.index, self.x, self.y = index, x, y,
-        self.width, self.height = width, height
+        self.index = index
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
         self.setGroup(group)
         for i in self.gaps:
             i._configure(qtile, self)
@@ -364,14 +381,14 @@ class Match(object):
 
     def compare(self, client):
         for _type, rule in self._rules:
-            match_func = getattr(rule, 'match', None) or\
+            match_func = getattr(rule, 'match', None) or \
                          getattr(rule, 'count')
 
             if _type == 'title':
                 value = client.name
             elif _type == 'wm_class':
                 value = client.window.get_wm_class()
-                if value and len(value)>1:
+                if value and len(value) > 1:
                     value = value[1]
                 elif value:
                     value = value[0]
