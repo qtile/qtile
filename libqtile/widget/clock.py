28c382f125c3ad522db9d0d55d01da3db5154721
jrazik <joseph.razik@univ-tln.fr>
2013-09-10 14:49:13 +0200

    Add to the widget the ability to display the current month calendar in a popup window when the mouse is over the widget. In this case, add also two possible actions: - left click : show previous month and so on - right click : show the next month and so on

--- libqtile/widget/clock.py
+++ libqtile/widget/clock.py
@@ -1,14 +1,22 @@
 from time import time
-from datetime import datetime
+from datetime import datetime, timedelta
+import calendar
 
-from .. import bar
+from libqtile import bar
 import base
 
 import gobject
 
+# for the popup window
+from libqtile import notify_window
+from textbox import TextBox
+import pango
+
+
 class Clock(base._TextBox):
     """
-        A simple but flexible text-based clock.
+        A simple but flexible text-based clock, with ability to display 
+        the current month calendar when the mouse is over the widget.
     """
     def __init__(self, fmt="%H:%M", width=bar.CALCULATED, **config):
         """
@@ -20,6 +28,8 @@ class Clock(base._TextBox):
         base._TextBox.__init__(self, " ", width, **config)
         self.fmt = fmt
         self.configured = False
+        self.popup = None
+
 
     def _configure(self, qtile, bar):
         if not self.configured:
@@ -27,8 +37,9 @@ class Clock(base._TextBox):
             gobject.idle_add(self.update)
         base._TextBox._configure(self, qtile, bar)
 
-    def update(self):
 
+    def update(self):
+        """ action automatically called to update the time displayed """
         ts = time()
 
         self.timeout_add(1. - ts % 1., self.update)
@@ -46,3 +57,65 @@ class Clock(base._TextBox):
             self.draw()
 
         return False
+
+
+    def pointer_over(self, x, y, detail):
+        """ action to do if the mouse if over the widget: display a calendar """
+        if not self.popup:
+            # create the calendar popup window
+            self.today = datetime.today()
+            w = TextBox(calendar.month(self.today.year, self.today.month), fontsize=self.fontsize, font="monospace")
+            self.popup = notify_window.NotifyWindow(self, [w])
+
+            # save the popup window in the bar in case of the mouse
+            # leave the widget to another widget in the bar
+            self.bar.popup_window[self] = self.popup
+            
+            self.popup._configure(self.qtile, self.bar.screen)
+            w.layout.layout.set_alignment(pango.ALIGN_LEFT)
+
+
+    def leave_window(self, x, y, detail):
+        """ action to proceed if the mouse leave the widget: kill the popup """
+        if self.popup:
+            self.popup.window.hide()
+            self.popup.window.kill()
+            self.popup.window = None
+            self.popup = None
+            self.bar.popup_window.pop(self)
+
+
+    def newMonth(self, date, way):
+        """
+        return a date in the previous or next month
+        @date: current date to move from
+        @way: direction (-1/+1) to move
+        """
+        if date.day < 15:
+            if way > 0:
+                date = date + timedelta(33)
+            else:
+                date = date - timedelta(18)
+        else:
+            if way > 0:
+                date = date + timedelta(18)
+            else:
+                date = date - timedelta(33)
+        return date
+
+
+
+    def button_press(self, x, y, button):
+        """ 
+        Two actions of modifying the calendar date are defined in case
+        of button press on the widget
+        """
+        if self.today:
+            if button == 1:
+                # leftmost button: prev month
+                self.today = self.newMonth(self.today, -1)
+            elif button == 3:
+                # rightmost button: next month
+                self.today = self.newMonth(self.today, 1)
+            self.popup.widgets[0].text = calendar.month(self.today.year, self.today.month)
+            self.popup.draw()
