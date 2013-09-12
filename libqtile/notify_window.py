348ca5a92f1aea8551fdd2b811c9594c2c31099f
jrazik <joseph.razik@univ-tln.fr>
2013-09-12 22:03:31 +0200

    Simplification by inherit of the new global Pane class.

--- libqtile/notify_window.py
+++ libqtile/notify_window.py
@@ -6,10 +6,15 @@ Used to make kind of popup window (see the clock widget)
 
 
 import drawer
-import hook
 import configurable
-import window
 import libqtile.widget
+
+from libqtile import pane
+from pane import Obj
+
+STRETCH = pane.STRETCH
+CALCULATED = pane.CALCULATED
+STATIC = pane.STATIC
 
 class Obj:
     def __init__(self, name):
@@ -22,16 +27,9 @@ class Obj:
         return self.name
 
 STRETCH = Obj("STRETCH")
-CALCULATED = Obj("CALCULATED")
-STATIC = Obj("STATIC")
 
 
-class NotifyWindow(configurable.Configurable):
-    defaults = [
-        ("background", "#000000", "Background colour."),
-        ("opacity",  1, "Bar window opacity.")
-    ]
-    
+class NotifyWindow(pane.Pane, configurable.Configurable):
 
     def __init__(self, src_widget, widgets, size=None, **config):
         """
@@ -40,15 +38,8 @@ class NotifyWindow(configurable.Configurable):
         @src_widget: the widget that call for the creation of this window
         """
 
-        self.size = size
-        self.qtile, self.screen = None, None
+        pane.Pane.__init__(self, widgets, (0,0), size, hidden=True,  **config)
         self.src_widget = src_widget
-
-        configurable.Configurable.__init__(self, **config)
-        self.add_defaults(NotifyWindow.defaults)
-
-        self.widgets = widgets
-        self.saved_focus = None
 
 
     def _configure(self, qtile, screen):
@@ -56,61 +47,41 @@ class NotifyWindow(configurable.Configurable):
         if any([w.width_type == STRETCH for w in self.widgets]):
             raise confreader.ConfigError("No STRETCH widget allowed!")
 
-        self.qtile = qtile
-        self.screen = screen
-        
         # initial width is set to the screen size
         # if width is adapted to the content then this attribute 
         # is set to True
         self.size_adapted = False
+        
+        # the initial window is just used to compute content width
+        # and height, no need to render or draw.
+        # So, first save the drawer and handle methods
+        drawer_backup = pane.Pane._configure_drawer
+        window_handle_backup = pane.Pane._configure_window_handle
 
-        # create an initial 'large' window that will not be shown
-        self.window = window.Internal.create(
-                        self.qtile,
-                        self.x, self.y, self.width, self.height,
-                        self.opacity
-                     )
+        pane.Pane._configure_drawer =  self._configure_no_drawer
+        pane.Pane._configure_window_handle = self._configure_no_window_handle
+        pane.Pane._configure(self, qtile, screen)
 
-        for i in self.widgets:
-            i._configure(qtile, self)
-            
         # recompute the width of the window according to the content
         self.compute_size()
         self.size_adapted = True
 
         self.window.kill()
         # now, the real window
-        self.window = window.Internal.create(
-                        self.qtile,
-                        self.x, self.y, self.width, self.height,
-                        self.opacity
-                     )
 
-        for i in self.widgets:
-            i._configure(qtile, self)
+        # restore the initial drawer and handle methods
+        pane.Pane._configure_drawer =  drawer_backup
+        pane.Pane._configure_window_handle = window_handle_backup
+        self.hidden = False
+        pane.Pane._configure(self, qtile, screen)
 
-        self.drawer = drawer.Drawer(
-                            self.qtile,
-                            self.window.window.wid,
-                            self.width,
-                            self.height
-                      )
-        self.drawer.clear(self.background)
 
-        self.window.handle_Expose = self.handle_Expose
-        self.window.handle_EnterNotify = self.handle_EnterWindow
-        self.window.handle_ButtonPress = self.handle_ButtonPress
-        self.window.handle_ButtonRelease = self.handle_ButtonRelease
-        self.window.handle_MotionNotify = self.handle_PointerMotion
+    def _configure_no_drawer(self):
+        pass
 
-        qtile.windowMap[self.window.window.wid] = self.window
-        self.window.unhide()
+    def _configure_no_window_handle(self):
+        pass
 
-        # FIXME: These should be targeted better.
-        hook.subscribe.setgroup(self.draw)
-        hook.subscribe.changegroup(self.draw)
-
-        
     def compute_size(self):
         # it is supposed that for now all widget are aline like in 'bar'
         width = 0
@@ -127,54 +98,8 @@ class NotifyWindow(configurable.Configurable):
             width = min(width, size[0])
             height = min(height, size[1])
         self.size = (min(width, self.screen.width), min(height, self.screen.height))
-
-
-    def _resize(self, width, widgets):
-        offset = 0
-        for i in widgets:
-            i.offset = offset
-            offset += i.width
-
-    def handle_Expose(self, e):
-        self.draw()
-
-    def get_widget_in_position(self, e):
-        for i in self.widgets:
-            if e.event_x < i.offset + i.width:
-                return i
-
-    def handle_PointerMotion(self, e):
-        pass
-
-    def handle_EnterWindow(self, e):
-        pass
-             
-    def handle_LeaveWindow(self, e):
-        pass
-
-    def handle_ButtonPress(self, e):
-        pass
-
-    def handle_ButtonRelease(self, e):
-        pass
-
-
-    def widget_grab_keyboard(self, widget):
-        pass
-
-    def widget_ungrab_keyboard(self):
-        pass
-
-
-    def draw(self):
-        self._resize(self.width, self.widgets)
-        for i in self.widgets:
-            i.draw()
-        if self.widgets:
-            end = i.offset + i.width
-            if end < self.width:
-                self.drawer.draw(end, self.width - end)
-
+        self._width = self.size[0]
+        self._height = self.size[1]
 
     @property
     def x(self):
@@ -219,9 +144,9 @@ class NotifyWindow(configurable.Configurable):
     @property
     def width(self):
         if self.size_adapted:
-            return self.size[0]
+            return self._width
         elif self.size:
-            return min(self.size[0], self.screen.width)
+            return min(self._width, self.screen.width)
         else:
             return self.screen.width
 
@@ -229,23 +154,12 @@ class NotifyWindow(configurable.Configurable):
     @property
     def height(self):
         if self.size_adapted:
-            return self.size[1]
+            return self._height
         elif self.size:
-            return min(self.size[1], self.screen.height)
+            return min(self._height, self.screen.height)
         else:
             return self.screen.dheight
 
-
-    def geometry(self):
-        return self.x, self.y, self.width, self.height
-
-    def _items(self, name):
-        if name == "screen":
-            return True, None
-
-    def _select(self, name, sel):
-        if name == "screen":
-            return self.screen
 
     def info(self):
         return dict(
@@ -255,10 +169,6 @@ class NotifyWindow(configurable.Configurable):
             height = self.height,
             widget = self.src.widget,
             widgets = [i.info() for i in self.widgets],
+            window = self.window.window.wid,
         )
 
-    def cmd_info(self):
-        """
-            Info for this object.
-        """
-        return self.info()
