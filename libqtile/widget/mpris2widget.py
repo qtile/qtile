6da561caea68f0c756f955ef57577f643504b6a8
Sebastian Kricner <sebastian.kricner@tuxwave.net>
2014-05-24 23:46:02 +0200

    mpriswidget.py renamed to mpris2widget.py. And class name change to Mpris2. mpriswidget.py is now from cbe029cb2c9cdc6e627b458f5675d076d651df53 again.

---
+++ libqtile/widget/mpris2widget.py
@@ -0,0 +1,114 @@
+import dbus
+from dbus.mainloop.glib import DBusGMainLoop
+import gobject
+
+import base
+from .. import bar
+
+class Mpris2(base._TextBox):
+    ''''
+    A widget which displays the current track/artist of your favorite MPRIS
+    player. It should work with all MPRIS 2 compatible players
+    which implement a reasonably correct version of MPRIS,
+    though I have only tested it with audacious.
+    '''
+    defaults = [
+                ('name', 'audacious', 'Name of the MPRIS widget.'),
+
+                ('objname', 'org.mpris.MediaPlayer2.audacious',
+                'DBUS MPRIS 2 compatible player identifier'
+                '- Find it out with dbus-monitor - '
+                'Also see: http://specifications.freedesktop.org/'
+                'mpris-spec/latest/#Bus-Name-Policy'),
+
+                ('display_metadata', ['xesam:title', 'xesam:album', 'xesam:artist'],
+                 'Which metadata identifiers to display.'),
+
+                ('scroll_chars', 30, 'How many chars at once to display.'),
+                ('scroll_interval', 1, 'Scroll delay interval.'),
+               ]
+
+    def __init__(self, **config):
+        super(self.__class__, self).__init__(self, **config)
+        base._TextBox.__init__(self, '', **config)
+        self.add_defaults(self.__class__.defaults)
+
+        dbus_loop = DBusGMainLoop()
+        bus = dbus.SessionBus(mainloop=dbus_loop)
+        bus.add_signal_receiver(self.update, 'PropertiesChanged',
+        'org.freedesktop.DBus.Properties', self.objname,
+        '/org/mpris/MediaPlayer2')
+
+        self.scrolltext = None
+        self.displaytext = ''
+        self.is_playing = False
+
+    def update(self, interface_name, changed_properties, invalidated_properties):
+        '''http://specifications.freedesktop.org/
+        mpris-spec/latest/Track_List_Interface.html#Mapping:Metadata_Map'''
+        if not self.configured:
+            return True
+        olddisplaytext = self.displaytext
+        self.displaytext = ''
+
+        metadata = None
+        playbackstatus = None
+        metadata = changed_properties.get('Metadata', None)
+        playbackstatus = changed_properties.get('PlaybackStatus', None)
+        if metadata:
+            self.is_playing = True
+            self.displaytext = ' - '.join([metadata.get(x)
+                          if isinstance(metadata.get(x), dbus.String)
+                          else ' + '.join([y for y in metadata.get(x)
+                          if isinstance(y, dbus.String)])
+                          for x in self.display_metadata if metadata.get(x)])
+            self.displaytext.replace('\n', '')
+        if playbackstatus:
+            if playbackstatus == 'Paused' and  olddisplaytext:
+                self.is_playing = False
+                self.displaytext = 'Paused: {}'.format(olddisplaytext)
+            elif playbackstatus == 'Paused':
+                self.is_playing = False
+                self.displaytext = 'Paused'
+            elif playbackstatus == 'Playing' and not self.displaytext and olddisplaytext:
+                self.is_playing = True
+                self.displaytext = olddisplaytext.replace('Paused: ', '')
+            elif playbackstatus == 'Playing' and not self.displaytext and not olddisplaytext:
+                self.is_playing = True
+                self.displaytext = 'No metadata for current track'
+            elif playbackstatus == 'Playing' and self.displaytext:
+                self.playbackstatus = True
+            else:
+                self.is_playing = False
+                self.displaytext = ''
+
+        if not self.scroll_chars or not self.scroll_interval:
+            if self.text != self.displaytext:
+                self.text = self.displaytext
+                self.bar.draw()
+        else:
+            if self.displaytext != '' or self.displaytext is None:
+                self.scrolltext = '{}{}{}'.format(' ' *\
+                        self.scroll_chars, self.displaytext,
+                        ' ' * self.scroll_chars)
+            else:
+                self.scrolltext = ''
+            self.timeout_add(self.scroll_interval, self.scroll_text)
+
+    def scroll_text(self):
+        if self.scrolltext:
+            self.text = self.scrolltext[:self.scroll_chars]
+            self.scrolltext = self.scrolltext[1:]
+            self.bar.draw()
+            return True
+        self.text = ''
+        self.bar.draw()
+        return False
+
+    def cmd_info(self):
+        '''What's the current state of the widget?'''
+        return dict(
+            displaytext=self.displaytext,
+            isplaying=self.is_playing,
+        )
+# vim: tabstop=4 shiftwidth=4 expandtab
--- libqtile/widget/mpriswidget.py
+++ libqtile/widget/mpriswidget.py
@@ -1,114 +1,141 @@
 import dbus
+
 from dbus.mainloop.glib import DBusGMainLoop
-import gobject
 
 import base
 from .. import bar
 
+
 class Mpris(base._TextBox):
-    ''''
+    """
     A widget which displays the current track/artist of your favorite MPRIS
-    player. It should work with all MPRIS 2 compatible players
-    which implement a reasonably correct version of MPRIS,
-    though I have only tested it with audacious.
-    '''
-    defaults = [
-                ('name', 'audacious', 'Name of the MPRIS widget.'),
+    player. It should work with all players which implement a reasonably
+    correct version of MPRIS, though I have only tested it with clementine.
+    """
 
-                ('objname', 'org.mpris.MediaPlayer2.audacious',
-                'DBUS MPRIS 2 compatible player identifier'
-                '- Find it out with dbus-monitor - '
-                'Also see: http://specifications.freedesktop.org/'
-                'mpris-spec/latest/#Bus-Name-Policy'),
+    def __init__(self, name="clementine", width=bar.CALCULATED,
+                 objname='org.mpris.clementine', **config):
+        base._TextBox.__init__(self, " ", width, **config)
 
-                ('display_metadata', ['xesam:title', 'xesam:album', 'xesam:artist'],
-                 'Which metadata identifiers to display.'),
+        # we need a main loop to get event signals
+        # we just piggyback on qtile's main loop
+        self.dbus_loop = DBusGMainLoop()
+        self.bus = dbus.SessionBus(mainloop=self.dbus_loop)
 
-                ('scroll_chars', 30, 'How many chars at once to display.'),
-                ('scroll_interval', 1, 'Scroll delay interval.'),
-               ]
+        # watch for our player to start up
+        deebus = self.bus.get_object(
+            'org.freedesktop.DBus',
+            '/org/freedesktop/DBus'
+        )
+        deebus.connect_to_signal(
+            "NameOwnerChanged",
+            self.handle_name_owner_change
+        )
 
-    def __init__(self, **config):
-        super(self.__class__, self).__init__(self, **config)
-        base._TextBox.__init__(self, '', **config)
-        self.add_defaults(self.__class__.defaults)
+        self.objname = objname
+        self.connected = False
+        self.name = name
 
-        dbus_loop = DBusGMainLoop()
-        bus = dbus.SessionBus(mainloop=dbus_loop)
-        bus.add_signal_receiver(self.update, 'PropertiesChanged',
-        'org.freedesktop.DBus.Properties', self.objname,
-        '/org/mpris/MediaPlayer2')
+        # try to connect for grins
+        self._connect()
+        self.timeout_add(1, self.update)
 
-        self.scrolltext = None
-        self.displaytext = ''
-        self.is_playing = False
+    def _connect(self):
+        """ Try to connect to the player if it exists. """
+        try:
+            self.player = self.bus.get_object(self.objname, '/Player')
+            self.iface = dbus.Interface(
+                self.player,
+                dbus_interface='org.freedesktop.MediaPlayer'
+            )
+            # See: http://xmms2.org/wiki/MPRIS for info on signals
+            # and what they mean.
+            self.iface.connect_to_signal(
+                "TrackChange",
+                self.handle_track_change
+            )
+            self.iface.connect_to_signal(
+                "StatusChange",
+                self.handle_status_change
+            )
+            self.connected = True
+        except dbus.exceptions.DBusException:
+            self.connected = False
 
-    def update(self, interface_name, changed_properties, invalidated_properties):
-        '''http://specifications.freedesktop.org/
-        mpris-spec/latest/Track_List_Interface.html#Mapping:Metadata_Map'''
+    def handle_track_change(self, metadata):
+        self.update()
+
+    def handle_status_change(self, *args):
+        self.update()
+
+    def handle_name_owner_change(self, name, old_owner, new_owner):
+        if name == self.objname:
+            if old_owner == '':
+                # Our player started, so connect to it
+                self._connect()
+            elif new_owner == '':
+                # It disconnected :-(
+                self.connected = False
+            self.update()
+
+    def ensure_connected(f):
+        """
+        Tries to connect to the player. It *should* be succesful if the player
+        is alive. """
+        def wrapper(*args, **kwargs):
+            self = args[0]
+            try:
+                self.iface.GetMetadata()
+            except (dbus.exceptions.DBusException, AttributeError):
+                # except AttributeError because
+                # self.iface won't exist if we haven't
+                # _connect()ed yet
+                self._connect()
+            return f(*args, **kwargs)
+        return wrapper
+
+    @ensure_connected
+    def update(self):
         if not self.configured:
             return True
-        olddisplaytext = self.displaytext
-        self.displaytext = ''
-
-        metadata = None
-        playbackstatus = None
-        metadata = changed_properties.get('Metadata', None)
-        playbackstatus = changed_properties.get('PlaybackStatus', None)
-        if metadata:
-            self.is_playing = True
-            self.displaytext = ' - '.join([metadata.get(x)
-                          if isinstance(metadata.get(x), dbus.String)
-                          else ' + '.join([y for y in metadata.get(x)
-                          if isinstance(y, dbus.String)])
-                          for x in self.display_metadata if metadata.get(x)])
-            self.displaytext.replace('\n', '')
-        if playbackstatus:
-            if playbackstatus == 'Paused' and  olddisplaytext:
-                self.is_playing = False
-                self.displaytext = 'Paused: {}'.format(olddisplaytext)
-            elif playbackstatus == 'Paused':
-                self.is_playing = False
-                self.displaytext = 'Paused'
-            elif playbackstatus == 'Playing' and not self.displaytext and olddisplaytext:
-                self.is_playing = True
-                self.displaytext = olddisplaytext.replace('Paused: ', '')
-            elif playbackstatus == 'Playing' and not self.displaytext and not olddisplaytext:
-                self.is_playing = True
-                self.displaytext = 'No metadata for current track'
-            elif playbackstatus == 'Playing' and self.displaytext:
-                self.playbackstatus = True
-            else:
-                self.is_playing = False
-                self.displaytext = ''
-
-        if not self.scroll_chars or not self.scroll_interval:
-            if self.text != self.displaytext:
-                self.text = self.displaytext
-                self.bar.draw()
+        if not self.connected:
+            playing = ''
+        elif not self.is_playing():
+            playing = 'Stopped'
         else:
-            if self.displaytext != '' or self.displaytext is None:
-                self.scrolltext = '{}{}{}'.format(' ' *\
-                        self.scroll_chars, self.displaytext,
-                        ' ' * self.scroll_chars)
-            else:
-                self.scrolltext = ''
-            self.timeout_add(self.scroll_interval, self.scroll_text)
+            try:
+                metadata = self.iface.GetMetadata()
 
-    def scroll_text(self):
-        if self.scrolltext:
-            self.text = self.scrolltext[:self.scroll_chars]
-            self.scrolltext = self.scrolltext[1:]
+                # TODO: Make this configurable?
+                playing = metadata["title"] + ' - ' + metadata["artist"]
+            except dbus.exceptions.DBusException:
+                self.connected = False
+                playing = ''
+
+        if playing != self.text:
+            self.text = playing
             self.bar.draw()
-            return True
-        self.text = ''
-        self.bar.draw()
-        return False
+
+    @ensure_connected
+    def is_playing(self):
+        """ Returns true if we are connected to the player and it is playing
+        something, false otherwise. """
+        if self.connected:
+            (playing, random, repeat, stop_after_last) = self.iface.GetStatus()
+            return playing == 0
+        else:
+            return False
 
     def cmd_info(self):
-        '''What's the current state of the widget?'''
+        """ What's the current state of the widget? """
         return dict(
-            displaytext=self.displaytext,
-            isplaying=self.is_playing,
+            connected=self.connected,
+            nowplaying=self.text,
+            isplaying=self.is_playing(),
         )
+
+    def cmd_update(self):
+        """ Force the widget to update. Mostly used for testing. """
+        self.update()
+
 # vim: tabstop=4 shiftwidth=4 expandtab
