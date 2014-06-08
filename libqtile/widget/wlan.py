1f85f4e243ac7d5ac1bce2fafb36a2414cdf8a60
Sebastian Kricner <sebastian.kricner@tuxwave.net>
2014-06-08 15:24:05 -0500

    widget/wlan.py:
    Fix for IOError when wlan device is deactivated (Softswitch)
    
    Closes #438

--- libqtile/widget/wlan.py
+++ libqtile/widget/wlan.py
@@ -1,4 +1,5 @@
 import base
+import logging
 from pythonwifi.iwlibs import Wireless, Iwstats
 
 
@@ -16,7 +17,12 @@ class Wlan(base.InLoopPollText):
 
     def poll(self):
         interface = Wireless(self.interface)
-        stats = Iwstats(self.interface)
-        quality = stats.qual.quality
-        essid = interface.getEssid()
-        return "{} {}/70".format(essid, quality)
+        try:
+            stats = Iwstats(self.interface)
+            quality = stats.qual.quality
+            essid = interface.getEssid()
+            return "{} {}/70".format(essid, quality)
+        except IOError as e:
+            logging.getLogger('qtile').error('%s: Probably your wlan device '
+                    'is switched off or otherwise not present in your system.',
+                    self.__class__.__name__)
