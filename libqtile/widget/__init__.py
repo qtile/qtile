9492e2726b8113d39937b0d34c8f04ff36cfcafd
roger <rogerduran@gmail.com>
2014-07-18 10:40:23 -0300

    add countdown widget

--- libqtile/widget/__init__.py
+++ libqtile/widget/__init__.py
@@ -22,6 +22,7 @@ from df import DF
 from image import Image
 from gmail_checker import GmailChecker
 from clipboard import Clipboard
+from countdown import Countdown
 
 try:
     from launchbar import LaunchBar
