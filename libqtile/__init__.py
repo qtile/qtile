8e5eecc5f385b2abfeef1eb42bce54aa154d4cd3
Sean Vig <sean.v.775@gmail.com>
2014-08-14 18:53:38 -0500

    Add note on asyncio/trollius importing

--- libqtile/__init__.py
+++ libqtile/__init__.py
@@ -9,6 +9,11 @@ moves = [
 for m in moves:
     six.add_move(m)
 
+# Here, we can't use six.Moved* methods because being able to import asyncio vs
+# trollius is not strictly Py 2 vs Py 3, but rather asyncio for >=3.4, and
+# possibly 3.3 with Tulip, and trollius for 2 and <=3.2, and 3.3 without Tulip.
+# Despite this, six.moves.asyncio makes a convenient place to store this so we
+# don't need to keep try/except importing asyncio.
 try:
     import asyncio
 except ImportError:
