e5e9f7ed5757e1a4462d8b3fd707b8bb2177ffe7
Tao Sauvage <sauvage.tao@gmail.com>
2014-09-06 13:47:00 +0200

    [log_utils] Fix log name (related to #494).

--- libqtile/log_utils.py
+++ libqtile/log_utils.py
@@ -45,6 +45,10 @@ def init_log(log_level=logging.ERROR, logger='qtile', log_path='~/.%s.log'):
     log.setLevel(log_level)
 
     if log_path:
+        try:
+            log_path = log_path % logger
+        except TypeError:  # Happens if log_path doesn't contain formatters.
+            pass
         log_path = os.path.expanduser(log_path)
         handler = logging.FileHandler(log_path)
         handler.setFormatter(
