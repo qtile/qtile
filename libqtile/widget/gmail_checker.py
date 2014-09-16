3fd724bb98305e354b4955691ee27b72736efc87
Tycho Andersen <tycho@tycho.ws>
2014-09-16 18:59:59 -0500

    Fix gmail_checker the Right Way
    See discussion on #504

--- libqtile/widget/gmail_checker.py
+++ libqtile/widget/gmail_checker.py
@@ -44,8 +44,9 @@ class GmailChecker(base.ThreadedPollText):
         answer, raw_data = self.gmail.status(self.email_path,
                                              '(MESSAGES UNSEEN)')
         if answer == "OK":
-            messages = int(re.search('MESSAGES\s+(\d+)', str(raw_data[0])).group(1))
-            unseen = int(re.search('UNSEEN\s+(\d+)', str(raw_data[0])).group(1))
+            enc = raw_data[0].encode()
+            messages = int(re.search('MESSAGES\s+(\d+)', enc).group(1))
+            unseen = int(re.search('UNSEEN\s+(\d+)', enc).group(1))
             if(self.status_only_unseen):
                 return self.fmt % unseen
             else:
