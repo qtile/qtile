cfd8dd01482d01a985a385be3b2c6dfac05c9900
Foster McLane <fkmclane@gmail.com>
2014-12-08 16:41:21 -0500

    Add PAVolume widget
    Add PAVolume class in volume.py that derives from Volume and also uses
    the amixer command, but uses the "pulse" device and does not use a
    cardid number.

--- libqtile/widget/volume.py
+++ libqtile/widget/volume.py
@@ -10,6 +10,7 @@ from six import u
 
 __all__ = [
     'Volume',
+    'PAVolume',
 ]
 
 re_vol = re.compile('\[(\d?\d?\d?)%\]')
@@ -194,3 +195,93 @@ class Volume(base._TextBox):
             self.drawer.draw(self.offset, self.width)
         else:
             base._TextBox.draw(self)
+
+class PAVolume(Volume):
+    ''' Pulse Audio version of Volume widget '''
+    defaults = [
+        ("channel", "Master", "Channel"),
+        ("padding", 3, "Padding left and right. Calculated if None."),
+        ("theme_path", None, "Path of the icons"),
+        ("update_interval", 0.2, "Update time in seconds."),
+        ("emoji", False, "Use emoji to display volume states, only if ``theme_path`` is not set."
+            "The specified font needs to contain the correct unicode characters."),
+        ("mute_command", None, "Mute command"),
+        ("volume_up_command", None, "Volume up command"),
+        ("volume_down_command", None, "Volume down command"),
+    ]
+
+    def __init__(self, **config):
+        base._TextBox.__init__(self, '0', width=bar.CALCULATED, **config)
+        self.add_defaults(PAVolume.defaults)
+        if self.theme_path:
+            self.width_type = bar.STATIC
+            self.width = 0
+        self.surfaces = {}
+        self.volume = None
+
+    def button_press(self, x, y, button):
+        if button == 5:
+            if self.volume_down_command is not None:
+                subprocess.call(self.volume_down_command)
+            else:
+                subprocess.call([
+                    'amixer',
+                    '-q',
+                    '-D',
+                    'pulse',
+                    'sset',
+                    self.channel,
+                    '4%-'
+                ])
+        elif button == 4:
+            if self.volume_up_command is not None:
+                subprocess.call(self.volume_up_command)
+            else:
+                subprocess.call([
+                    'amixer',
+                    '-q',
+                    '-D',
+                    'pulse',
+                    'sset',
+                    self.channel,
+                    '4%+'
+                ])
+        elif button == 1:
+            if self.mute_command is not None:
+                subprocess.call(self.mute_command)
+            else:
+                subprocess.call([
+                    'amixer',
+                    '-q',
+                    '-D',
+                    'pulse',
+                    'sset',
+                    self.channel,
+                    'toggle'
+                ])
+                self.draw()
+
+    def get_volume(self):
+        mixerprocess = subprocess.Popen(
+            [
+                'amixer',
+                '-D',
+                'pulse',
+                'sget',
+                self.channel
+            ],
+            stdout=subprocess.PIPE
+        )
+        mixer_out = mixerprocess.communicate()[0].decode()
+        if mixerprocess.returncode:
+            return -1
+
+        if '[off]' in mixer_out:
+            return -1
+
+        volgroups = re_vol.search(mixer_out)
+        if volgroups:
+            return int(volgroups.groups()[0])
+        else:
+            # this shouldn't happend
+            return -1
