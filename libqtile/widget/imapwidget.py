ceda14e7f8425257b1e2171cfdcca52a2acd3ecb
David R. Andersen <k0rx@RXcomm.net>
2015-01-11 19:46:27 -0600

    p3 keyring is not threadsafe - moved to __init__()

--- libqtile/widget/imapwidget.py
+++ libqtile/widget/imapwidget.py
@@ -5,33 +5,44 @@
 from . import base
 import imaplib
 import re
-import gnomekeyring as gk
+import keyring
 
 class ImapWidget(base.ThreadedPollText):
     """
     This widget will scan one of your imap email boxes and report
     the number of unseen messages present.  I've configured it to
     only work with imap with ssl. Your password is obtained from
-    the Gnome Keyring.  You can write your password to the keyring
-    using the add-keyring-passwords.py script from my rxcomm-scripts
-    repo at https://github.com/rxcomm/rxcomm-scripts.git
+    the Gnome Keyring.
 
-    Parameters for writing the password using the above script are:
-        application name = imapwidget
-        server = <fqdn of your imap server>
-        user = <your userid>
-        protocol = imapwidget
-        password = <your password>
+    Writing your password to the keyring initially is as simple as
+    (changing out <userid> and <password> for your userid and password):
+
+    1) create the file ~/.local/share/python_keyring/keyringrc.cfg
+       with the following contents:
+
+       [backend]
+       default-keyring=keyring.backends.Gnome.Keyring
+       keyring-path=/home/<userid>/.local/share/keyring/
+
+
+    2) Execute the following python shell script once:
+
+       #!/usr/bin/env python3
+       import keyring
+       user = <userid>
+       password = <password>
+       keyring.set_password('imapwidget', user, password)
 
     mbox names must include the path to the mbox (except for the
     default INBOX).  So, for example if your mailroot is ~/Maildir,
     and you want to look at the mailbox at HomeMail/fred, the mbox
-    setting would be: mbox='~/Maildir/HomeMail/fred'.  Labels
-    can be whatever you choose, of course.
+    setting would be: mbox='"~/Maildir/HomeMail/fred"'.  Note the
+    nested sets of quotes! Labels can be whatever you choose, of
+    course.
     """
 
     defaults = [
-        ('mbox', 'INBOX', 'mailbox to fetch'),
+        ('mbox', '"INBOX"', 'mailbox to fetch'),
         ('label', 'INBOX', 'label for display'),
         ('user', None, 'email username'),
         ('server', None, 'email server name'),
@@ -40,25 +51,21 @@ class ImapWidget(base.ThreadedPollText):
     def __init__(self, **config):
         base.ThreadedPollText.__init__(self, **config)
         self.add_defaults(ImapWidget.defaults)
+        password = keyring.get_password('imapwidget', self.user)
+        if password is not None:
+            self.password = password
+        else:
+            self.log.critical('Gnome Keyring Error')
+            return ('Gnome Keyring Error',)
 
     def poll(self):
         im = imaplib.IMAP4_SSL(self.server, 993)
-        creds = self.get_pass(user=self.user, server=self.server, protocol='imapwidget')
-        if creds[0] == 'Gnome Keyring Error':
+        if self.password == 'Gnome Keyring Error':
             self.text = 'Gnome Keyring Error'
         else:
-            im.login(self.user, creds)
-            self.text = str(im.status(self.mbox, '(UNSEEN)')[1])
+            im.login(self.user, self.password)
+            status, response = im.status(self.mbox, '(UNSEEN)')
+            self.text = response[0].decode()
             self.text = self.label + ': ' + re.sub(r'\).*$', '', re.sub(r'^.*N\s', '', self.text))
             im.logout()
         return self.text
-
-    def get_pass(self, **kwargs):
-
-        try:
-            keys = gk.find_network_password_sync(**kwargs)
-            password = keys[0]['password']
-            return password
-        except (gk.NoMatchError, gk.IOError):
-            self.log.critical('Gnome Keyring Error')
-            return ('Gnome Keyring Error',)
