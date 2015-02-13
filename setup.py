da482ad27a9d14ef881b246ca11e4bf1c67368b2
Tycho Andersen <tycho@tycho.ws>
2015-02-13 08:31:43 -0700

    Release 0.9.1

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ else:
 
 setup(
     name="qtile",
-    version="0.9.0",
+    version="0.9.1",
     description="A pure-Python tiling window manager.",
     long_description=long_description,
     classifiers=[
