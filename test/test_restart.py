be41c64b5cc3341c6de23074352d6992fe42988d
Aniruddh Kanojia <aniurddhkanojia94@gmail.com>
2015-07-14 07:52:58 +0530

    Add a comment

--- test/test_restart.py
+++ test/test_restart.py
@@ -113,7 +113,7 @@ def test_move(self):
     a = self.c.get_info()
     x = self.simulate_restart()
     b = self.c.get_info()
-    print(a),'\n\n'
+    print(a), '\n\n'
     print(b)
     assert a == b
 
@@ -128,3 +128,4 @@ def test_move(self):
     x = self.simulate_restart()
     b = self.c.get_info()
     assert a == b
+#   Comment
