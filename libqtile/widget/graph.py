279b942737c71b151aa64af42ea372e5c21cd037
Andreas Andersson <a.andersson.thn@gmail.com>
2015-11-03 19:58:30 +0100

    Fix erroneous calculation.
    Fixed erroneus calcualation in MemoryGraph.

--- libqtile/widget/graph.py
+++ libqtile/widget/graph.py
@@ -255,7 +255,7 @@ def get_meminfo():
             else:
                 key, tail = line.strip().split(':')
                 uv = tail.split()
-                val[key] = int(uv[0]) // 1000
+                val[key] = int(uv[0])
     val['MemUsed'] = val['MemTotal'] - val['MemFree']
     return val
 
