e271faaa02a66cde43bd06d768da1fed91328915
Sean Vig <sean.v.775@gmail.com>
2016-01-26 23:53:17 -0600

    Fix logic in toScreen
    An index operation, `list[n]`, will fail if `n >= len(list)` or `n + 1 >
    len(list)`, this previously let you go 2 over in your index before
    returning.
    
    This goes back all the way to 2008 O.O 2aa8e9f0

--- libqtile/manager.py
+++ libqtile/manager.py
@@ -1074,7 +1074,7 @@ class Qtile(command.CommandObject):
         """
         Have Qtile move to screen and put focus there
         """
-        if len(self.screens) < n - 1:
+        if n <= len(self.screens):
             return
         old = self.currentScreen
         self.currentScreen = self.screens[n]
