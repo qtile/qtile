3c0f1936271799d1f5c9b7e8288fc411870cef5a
Sean Vig <sean.v.775@gmail.com>
2016-03-04 14:48:51 -0600

    Show all "defaults" for widgets in docs
    Closes #803

--- docs/sphinx_qtile.py
+++ docs/sphinx_qtile.py
@@ -52,7 +52,7 @@ qtile_class_template = Template('''
         * - key
           - default
           - description
-        {% for key, default, description in obj.defaults %}
+        {% for key, default, description in defaults %}
         * - ``{{ key }}``
           - ``{{ default|pprint }}``
           - {{ description }}
@@ -107,11 +107,29 @@ class QtileClass(SimpleDirectiveMixin, Directive):
         is_configurable = ':no-config:' not in arguments
         is_commandable = ':no-commands:' not in arguments
         arguments = [i for i in arguments if i not in (':no-config:', ':no-commands:')]
+
+        # build up a dict of defaults using reverse MRO
+        defaults = {}
+        for klass in reversed(obj.mro()):
+            if not issubclass(klass, configurable.Configurable):
+                continue
+            if not hasattr(klass, "defaults"):
+                continue
+            klass_defaults = getattr(klass, "defaults")
+            defaults.update({
+                d[0]: d[1:] for d in klass_defaults
+            })
+        # turn the dict into a list of ("value", "default", "description") tuples
+        defaults = [
+            (k, v[0], v[1]) for k, v in sorted(defaults.items())
+        ]
+
         context = {
             'module': module,
             'class_name': class_name,
             'class_underline': "=" * len(class_name),
             'obj': obj,
+            'defaults': defaults,
             'configurable': is_configurable and issubclass(obj, configurable.Configurable),
             'commandable': is_commandable and issubclass(obj, command.CommandObject),
             'is_widget': issubclass(obj, widget.base._Widget),
