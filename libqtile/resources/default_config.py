8659d74559150d7149261f55ed88792bf8677456
Dario Giovannetti <dev@dariogiovannetti.net>
2017-03-14 20:01:58 +0800

    default_config.py: Fix white space

--- libqtile/resources/default_config.py
+++ libqtile/resources/default_config.py
@@ -102,9 +102,9 @@ screens = [
 # Drag floating layouts.
 mouse = [
     Drag([mod], "Button1", lazy.window.set_position_floating(),
-        start=lazy.window.get_position()),
+         start=lazy.window.get_position()),
     Drag([mod], "Button3", lazy.window.set_size_floating(),
-        start=lazy.window.get_size()),
+         start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front())
 ]
 
