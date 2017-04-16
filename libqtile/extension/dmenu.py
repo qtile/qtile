qtile/qtile

commit 98e4006c0df309ed9f4523fd1e17f4f32be75933

extension.WindowList: change item format and make configurable

libqtile/extension/window_list.py
@@ -27,7 +27,9 @@ class WindowList(Dmenu):
     Give vertical list of all open windows in dmenu. Switch to selected.
     """
 
-    defaults = []
+    defaults = [
+        ("item_format", "{group}.{id}: {window}", "the format for the menu items"),
+    ]
 
     def __init__(self, **config):
         Dmenu.__init__(self, **config)
@@ -42,7 +44,8 @@ class WindowList(Dmenu):
         self.id_map = {}
         for win in self.qtile.windowMap.values():
             if win.group:
-                self.wins.append("%i: %s (%s)" % (id, win.name, win.group.name))
+                self.wins.append(self.item_format.format(
+                    group=win.group.name, id=id, window=win.name))
                 self.id_map[id] = win
                 id += 1
 

commit 27bc20f71b0cf7d02d735aa11940ecbdf7b4dbb8

Fix and improve WindowList extension
Amend 98e4006c0df309ed9f4523fd1e17f4f32be75933 which broke it because the default items don't start with a digit anymore

libqtile/extension/dmenu.py
@@ -100,7 +100,7 @@ class Dmenu(base.RunCommand):
 
         if items:
             input_str = "\n".join([six.u(i) for i in items]) + "\n"
-            return proc.communicate(str.encode(input_str))[0]
+            return proc.communicate(str.encode(input_str))[0].decode('utf-8')
 
         return proc
 

libqtile/extension/window_list.py
@@ -40,23 +40,32 @@ class WindowList(Dmenu):
 
     def list_windows(self):
         id = 0
-        self.wins = []
-        self.id_map = {}
+        self.item_to_win = {}
         for win in self.qtile.windowMap.values():
             if win.group:
-                self.wins.append(self.item_format.format(
-                    group=win.group.name, id=id, window=win.name))
-                self.id_map[id] = win
+                item = self.item_format.format(
+                    group=win.group.name, id=id, window=win.name)
+                self.item_to_win[item] = win
                 id += 1
 
     def run(self):
         self.list_windows()
-        out = super(WindowList, self).run(items=self.wins)
-        if not out:
+        out = super(WindowList, self).run(items=self.item_to_win.keys())
+
+        try:
+            sout = out.rstrip('\n')
+        except AttributeError:
+            # out is not a string (for example it's a Popen object returned
+            # by super(WindowList, self).run() when there are no menu items to
+            # list
             return
 
-        id = int(re.match(b"^\d+", out).group())
-        win = self.id_map[id]
+        try:
+            win = self.item_to_win[sout]
+        except KeyError:
+            # The selected window got closed while the menu was open?
+            return
+
         screen = self.qtile.currentScreen
         screen.setGroup(win.group)
         win.group.focus(win)