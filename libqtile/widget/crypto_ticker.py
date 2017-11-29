78e332a026c28ca674010401245dcebb93373e51
granitosaurus <tinarg@protonmail.com>
2017-11-29 05:43:03 +0000

    add reverse compatibility and fix flae errors

--- libqtile/widget/crypto_ticker.py
+++ libqtile/widget/crypto_ticker.py
@@ -36,7 +36,7 @@ class CryptoTicker(GenPollUrl):
     ::
 
         # display the average price of bitcoin in euros in format like "BTC:9001"
-        widget.CryptoTicker(from_currency='bitcoin', to_currency='eur', format="{symbol}:{price}")
+        widget.CryptoTicker(from_currency='bitcoin', currency='eur', format="{symbol}:{price}")
 
         # display the average price of litecoin in local currency
         widget.CryptoTicker(from_currency='litecoin')
@@ -45,12 +45,12 @@ class CryptoTicker(GenPollUrl):
         widget.CryptoTicker(format="{symbol}:{price_btc}", from_currency='litecoin')
     """
 
-    QUERY_URL = "https://api.coinmarketcap.com/v1/ticker/{from_currency}/?convert={to_currency}"
+    QUERY_URL = "https://api.coinmarketcap.com/v1/ticker/{from_currency}/?convert={currency}"
 
     orientations = base.ORIENTATION_HORIZONTAL
 
     defaults = [
-        ('to_currency', locale.localeconv()['int_curr_symbol'].strip() or 'usd',
+        ('currency', locale.localeconv()['int_curr_symbol'].strip() or 'usd',
          'Result currency'),
         ('from_currency', 'bitcoin', 'The source currency to convert from'),
         ('format', '{symbol}:{to_price}',
@@ -60,12 +60,11 @@ class CryptoTicker(GenPollUrl):
     def __init__(self, **config):
         super().__init__(**config)
         self.add_defaults(self.defaults)
-        self.url = self.QUERY_URL.format(from_currency=self.from_currency.lower(), to_currency=self.to_currency.lower())
+        self.url = self.QUERY_URL.format(from_currency=self.from_currency.lower(), currency=self.currency.lower())
 
     def parse(self, body):
         body = body[0] if body else {}
-        body['to_currency'] = self.to_currency
+        body['currency'] = self.currency
         body['from_currency'] = self.from_currency
-        body['to_price'] = body.get('price_{}'.format(self.to_currency), 'unknown_output_currency')
+        body['to_price'] = body.get('price_{}'.format(self.currency), 'unknown_output_currency')
         return self.format.format(**body)
-
