49a90c95f0f12f2bcf171fb65d52fe63be103271
Emiel Kollof <emiel.kollof@gmail.com>
2018-12-20 20:05:52 +0100

    Placate travis

--- libqtile/widget/memory.py
+++ libqtile/widget/memory.py
@@ -1,4 +1,5 @@
-# -*- coding: utf-8 -*- # Copyright (c) 2015 Jörg Thalheim (Mic92)
+# -*- coding: utf-8 -*-
+# Copyright (c) 2015 Jörg Thalheim (Mic92)
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
