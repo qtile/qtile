c32f020052a2787cdbb4665a16e1fe7d9ca06abf
Guangwang Huang <whatacold@gmail.com>
2019-08-29 23:01:40 +0800

    Make it explicit to install cairocffi after xcffib. (#1406)
    * Make it explicit to install cairocffi after xcffib.
    
    * Per review comments.

--- dev.sh
+++ dev.sh
@@ -7,6 +7,12 @@ echo "Creating dev environment in ./venv..."
 python3.7 -m venv venv
 . venv/bin/activate
 pip3.7 install -U pip setuptools
+
+# https://github.com/qtile/qtile/issues/994#issuecomment-497984551
+echo "Installing xcffib then cairocffi..."
+pip3.7 install 'xcffib >= 0.5.0' && pip3.7 install --no-cache-dir 'cairocffi >= 0.9.0'
+
+echo "Installing other required packages..."
 pip3.7 install -r requirements.txt
 pip3.7 install -r requirements-dev.txt
 
