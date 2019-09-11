268133beb14443e4a34858b2424949894970bd81
Timothée Mazzucotelli <pawamoy@pm.me>
2019-09-11 03:37:07 +0200

    docs: Add script to run Xephyr and take screenshots with custom config

---
+++ scripts/take-screenshots.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+PROJECT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
+SCREEN_SIZE=${SCREEN_SIZE:-800x600}
+XDISPLAY=${XDISPLAY:-:1}
+if [[ -z $PYTHON ]]; then
+    PYTHON=python
+fi
+
+Xephyr +extension RANDR -screen ${SCREEN_SIZE} ${XDISPLAY} -ac &
+XEPHYR_PID=$!
+(
+  sleep 1
+  env DISPLAY=${XDISPLAY} ${PYTHON} "${PROJECT_DIR}/bin/qtile" -c "${PROJECT_DIR}/docs/screenshots/config.py" &
+  QTILE_PID=$!
+
+  sleep 1
+  env DISPLAY=${XDISPLAY} xterm -e "${PYTHON}" "${PROJECT_DIR}/docs/screenshots/take_all.py"
+
+  sleep 1
+  kill $QTILE_PID
+  kill $XEPHYR_PID
+)
