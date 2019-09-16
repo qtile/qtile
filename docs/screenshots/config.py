eaf695c8640e63dcf200d3fc236b328f2b72ecb8
Timothée Mazzucotelli <pawamoy@pm.me>
2019-09-16 15:27:31 +0200

    docs(screenshots): Remove copyright, refactor layouts style

--- docs/screenshots/config.py
+++ docs/screenshots/config.py
@@ -1,30 +1,3 @@
-# Copyright (c) 2010 Aldo Cortesi
-# Copyright (c) 2010, 2014 dequis
-# Copyright (c) 2012 Randall Ma
-# Copyright (c) 2012-2014 Tycho Andersen
-# Copyright (c) 2012 Craig Barnes
-# Copyright (c) 2013 horsik
-# Copyright (c) 2013 Tao Sauvage
-# Copyright (c) 2019 Timothée Mazzucotelli
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 from libqtile.config import Key
 from libqtile.lazy import lazy
 from libqtile import layout
@@ -45,24 +18,26 @@ keys = [
 border_focus = "#ff0000"
 border_normal = "#000000"
 border_width = 10
-border = dict(
+margin = 20
+style = dict(
     border_focus=border_focus,
     border_normal=border_normal,
-    border_width=border_width
+    border_width=border_width,
+    margin=margin,
 )
 
 layouts = [
     layout.Max(name="max"),
-    layout.Bsp(name="bsp", margin=20, **border),
-    layout.Columns(name="columns", **border),
-    layout.Matrix(name="matrix", **border),
-    layout.MonadTall(name="monadtall", **border),
-    layout.MonadWide(name="monadwide", **border),
-    layout.RatioTile(name="ratiotile", **border),
+    layout.Bsp(name="bsp", **style),
+    layout.Columns(name="columns", **style),
+    layout.Matrix(name="matrix", **style),
+    layout.MonadTall(name="monadtall", **style),
+    layout.MonadWide(name="monadwide", **style),
+    layout.RatioTile(name="ratiotile", **style),
     # layout.Slice(name="slice"),  # Makes the session freeze
-    layout.Stack(name="stack", **border),
-    layout.Tile(name="tile", **border),
+    layout.Stack(name="stack", **style),
+    layout.Tile(name="tile", **style),
     layout.TreeTab(name="treetab", border_width=border_width),
-    layout.VerticalTile(name="verticaltile", **border),
-    layout.Zoomy(name="zoomy"),
+    layout.VerticalTile(name="verticaltile", **style),
+    layout.Zoomy(name="zoomy", margin=margin),
 ]
