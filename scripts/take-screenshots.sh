0ab6fb501492dde778c369ec403b418d0a862a04
Timothée Mazzucotelli <pawamoy@pm.me>
2019-09-18 01:11:15 +0200

    docs(screenshots): Change shell script env defaults

--- scripts/take-screenshots.sh
+++ scripts/take-screenshots.sh
@@ -1,28 +1,36 @@
 #!/bin/sh
 
 PROJECT_DIR=$(dirname "$(dirname "$(readlink -f "$0")")")
-SCREEN_SIZE=${SCREEN_SIZE:-800x600}
+SCREEN_SIZE=${SCREEN_SIZE:-1920x1080}
 XDISPLAY=${XDISPLAY:-:1}
-if [[ -z $PYTHON ]]; then
+if [[ -z "${PYTHON}" ]]; then
+  if [[ -f "${PROJECT_DIR}/venv/bin/python" ]]; then
+    PYTHON="${PROJECT_DIR}/venv/bin/python"
+  else
     PYTHON=python
+  fi
 fi
+
+nested() {
+  env DISPLAY=${XDISPLAY} PYTHON="${PYTHON}" "$@"
+}
 
 Xephyr +extension RANDR -screen ${SCREEN_SIZE} ${XDISPLAY} -ac &
 XEPHYR_PID=$!
 (
   sleep 1
-  env DISPLAY=${XDISPLAY} ${PYTHON} "${PROJECT_DIR}/bin/qtile" -c "${PROJECT_DIR}/docs/screenshots/config.py" &
+  nested "${PYTHON}" "${PROJECT_DIR}/bin/qtile" -c "${PROJECT_DIR}/docs/screenshots/config.py" &
   QTILE_PID=$!
 
   sleep 1
   case $1 in
     -i|--interactive)
-      env DISPLAY=${XDISPLAY} xterm
+      nested xterm
     ;;
     *)
-      env DISPLAY=${XDISPLAY} xterm -e "${PYTHON}" "${PROJECT_DIR}/docs/screenshots/take_all.py" "$@"
+      nested xterm -e "${PYTHON}" "${PROJECT_DIR}/docs/screenshots/take_all.py" "$@"
       sleep 1
-      env DISPLAY=${XDISPLAY} xterm -e qtile-cmd -o cmd -f shutdown
+      nested xterm -e qtile-cmd -o cmd -f shutdown
     ;;
   esac
 
