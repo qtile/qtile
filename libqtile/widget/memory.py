fed9d6c37f70cf51966bf5dd4f3ebed6ca35db42
Niko Järvinen <nbjarvinen@gmail.com>
2019-10-20 05:10:06 +0300

    Fix for memory widget causing crash

--- libqtile/widget/memory.py
+++ libqtile/widget/memory.py
@@ -22,7 +22,7 @@ import psutil
 
 from libqtile.widget import base
 
-__all__ = ['Memory']
+__all__ = ["Memory"]
 
 
 class Memory(base.ThreadedPollText):
@@ -39,27 +39,33 @@ class Memory(base.ThreadedPollText):
     SwapFree: Returns amount of swap free
     SwapUsed: Returns amount of swap in use
 """
+
     orientations = base.ORIENTATION_HORIZONTAL
     defaults = [
-        ("fmt", "{MemUsed}M/{MemTotal}M", "Formatting for field names.")
+        ("format", "{MemUsed}M/{MemTotal}M", "Formatting for field names."),
+        ("update_interval", 1.0, "Update interval for the Memory"),
     ]
 
     def __init__(self, **config):
-        super().__init__(**config)
+        base.ThreadedPollText.__init__(self, **config)
         self.add_defaults(Memory.defaults)
+
+    def tick(self):
+        self.update(self.poll())
+        return self.update_interval
 
     def poll(self):
         mem = psutil.virtual_memory()
         swap = psutil.swap_memory()
         val = {}
-        val['MemUsed'] = mem.used // 1024 // 1024
-        val['MemTotal'] = mem.total // 1024 // 1024
-        val['MemFree'] = mem.free // 1024 // 1024
-        val['Buffers'] = mem.buffers // 1024 // 1024
-        val['Active'] = mem.active // 1024 // 1024
-        val['Inactive'] = mem.inactive // 1024 // 1024
-        val['Shmem'] = mem.shared // 1024 // 1024
-        val['SwapTotal'] = swap.total // 1024 // 1024
-        val['Swapfree'] = swap.free // 1024 // 1024
-        val['SwapUsed'] = swap.used // 1024 // 1024
-        return self.fmt.format(**val)
+        val["MemUsed"] = mem.used // 1024 // 1024
+        val["MemTotal"] = mem.total // 1024 // 1024
+        val["MemFree"] = mem.free // 1024 // 1024
+        val["Buffers"] = mem.buffers // 1024 // 1024
+        val["Active"] = mem.active // 1024 // 1024
+        val["Inactive"] = mem.inactive // 1024 // 1024
+        val["Shmem"] = mem.shared // 1024 // 1024
+        val["SwapTotal"] = swap.total // 1024 // 1024
+        val["Swapfree"] = swap.free // 1024 // 1024
+        val["SwapUsed"] = swap.used // 1024 // 1024
+        return self.format.format(**val)
