bbab63f877b09d90a5a9cbf061068eeb48f3f502
Arnas Udovičius <zordsdavini@gmail.com>
2020-04-22 23:33:43 +0300

    pep8 remove unused import

--- libqtile/widget/pomodoro.py
+++ libqtile/widget/pomodoro.py
@@ -18,7 +18,6 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import subprocess
 from datetime import datetime, timedelta
 from time import time
 
