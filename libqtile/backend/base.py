8627fafecc6f7d1049d4170ed8f096b0e426604d
Sean Vig <sean.v.775@gmail.com>
2020-06-27 21:04:36 -0400

    Move un/grab methods to core
    Setup the core with abstract (un)grab methods for the keys, buttons, and
    pointer.  Move the functionality which performed this grabbing from the
    xcbq window into the X core.  The use of a root window is somewhat
    X-specific, so exposing this instead in the core with the calls made
    using the X root window id is easier to extend to Wayland.

--- libqtile/backend/base.py
+++ libqtile/backend/base.py
@@ -1,6 +1,8 @@
 import typing
 from abc import ABCMeta, abstractmethod
 
+from libqtile import config
+
 
 class Core(metaclass=ABCMeta):
     @property
@@ -15,3 +17,31 @@ class Core(metaclass=ABCMeta):
     @abstractmethod
     def get_modifiers(self) -> typing.List[str]:
         pass
+
+    @abstractmethod
+    def grab_key(self, key: config.Key) -> typing.Tuple[int, int]:
+        """Configure the backend to grab the key event"""
+
+    @abstractmethod
+    def ungrab_key(self, key: config.Key) -> typing.Tuple[int, int]:
+        """Release the given key event"""
+
+    @abstractmethod
+    def ungrab_keys(self) -> None:
+        """Release the grabbed key events"""
+
+    @abstractmethod
+    def grab_button(self, mouse: config.Mouse) -> None:
+        """Configure the backend to grab the mouse event"""
+
+    @abstractmethod
+    def ungrab_buttons(self) -> None:
+        """Release the grabbed button events"""
+
+    @abstractmethod
+    def grab_pointer(self) -> None:
+        """Configure the backend to grab mouse events"""
+
+    @abstractmethod
+    def ungrab_pointer(self) -> None:
+        """Release grabbed pointer events"""
