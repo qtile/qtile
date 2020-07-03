2ea0a13bf03e89eb9bca470c9553c6a643a23cf9
fdev31 <fdev31@gmail.com>
2020-07-03 17:57:07 +0200

    fix import order

--- libqtile/resources/default_config.py
+++ libqtile/resources/default_config.py
@@ -27,7 +27,7 @@
 from typing import List  # noqa: F401
 
 from libqtile import bar, layout, widget
-from libqtile.config import Click, Drag, Group, Key, Screen, Match
+from libqtile.config import Click, Drag, Group, Key, Match, Screen
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 
