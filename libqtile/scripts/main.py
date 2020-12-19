908f5266b58cfc77bb8e545d483f7f0f6404b0d6
Drew Ditthardt <drew.ditthardt@gmail.com>
2020-12-19 14:49:54 -0500

    fix commands not in subcommands

--- libqtile/scripts/main.py
+++ libqtile/scripts/main.py
@@ -11,6 +11,14 @@ except (pkg_resources.DistributionNotFound, ImportError):
 
 
 def main():
+    # backward compat hack: `qtile` with no args (or non-subcommand args)
+    # should default to `qtile start`. it seems impolite for commands to do
+    # nothing when run with no args, so let's warn about this being deprecated.
+    if len(sys.argv) == 1:
+        print("please move to `qtile start` as your qtile invocation, "
+              "instead of just `qtile`; this default will be removed Soon(TM)")
+        sys.argv.insert(1, "start")
+
     parser = argparse.ArgumentParser(
         prog='qtile',
         description='A full-featured, pure-Python tiling window manager.',
@@ -33,14 +41,6 @@ def main():
         parser.print_help()
     help_ = subparsers.add_parser("help", help="Print help information and exit")
     help_.set_defaults(func=print_help)
-
-    # backward compat hack: `qtile` with no args (or non-subcommand args)
-    # should default to `qtile start`. it seems impolite for commands to do
-    # nothing when run with no args, so let's warn about this being deprecated.
-    if len(sys.argv) == 1 or sys.argv[1] not in subparsers.choices.keys():
-        print("please move to `qtile start` as your qtile invocation, "
-              "instead of just `qtile`; this default will be removed Soon(TM)")
-        sys.argv.insert(1, "start")
 
     options = parser.parse_args()
     options.func(options)
