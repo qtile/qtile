0842789d7d0aeff396f98e83967c9aaabf2f6a4e
Arkadiusz Czader <czader@student.agh.edu.pl>
2020-12-27 16:26:20 +0100

    Fix fixture in test_margin()

--- test/test_window.py
+++ test/test_window.py
@@ -2,30 +2,30 @@ import pytest
 
 from test.conftest import BareConfig
 
-bare_config = pytest.mark.parametrize("qtile", [BareConfig], indirect=True)
+bare_config = pytest.mark.parametrize("manager", [BareConfig], indirect=True)
 
 
 @bare_config
-def test_margin(qtile):
-    qtile.test_window('one')
+def test_margin(manager):
+    manager.test_window('one')
 
     # No margin
-    qtile.c.window.place(10, 20, 50, 60, 0, '000000')
-    assert qtile.c.window.info()['x'] == 10
-    assert qtile.c.window.info()['y'] == 20
-    assert qtile.c.window.info()['width'] == 50
-    assert qtile.c.window.info()['height'] == 60
+    manager.c.window.place(10, 20, 50, 60, 0, '000000')
+    assert manager.c.window.info()['x'] == 10
+    assert manager.c.window.info()['y'] == 20
+    assert manager.c.window.info()['width'] == 50
+    assert manager.c.window.info()['height'] == 60
 
     # Margin as int
-    qtile.c.window.place(10, 20, 50, 60, 0, '000000', margin=8)
-    assert qtile.c.window.info()['x'] == 18
-    assert qtile.c.window.info()['y'] == 28
-    assert qtile.c.window.info()['width'] == 34
-    assert qtile.c.window.info()['height'] == 44
+    manager.c.window.place(10, 20, 50, 60, 0, '000000', margin=8)
+    assert manager.c.window.info()['x'] == 18
+    assert manager.c.window.info()['y'] == 28
+    assert manager.c.window.info()['width'] == 34
+    assert manager.c.window.info()['height'] == 44
 
     # Margin as list
-    qtile.c.window.place(10, 20, 50, 60, 0, '000000', margin=[2, 4, 8, 10])
-    assert qtile.c.window.info()['x'] == 20
-    assert qtile.c.window.info()['y'] == 22
-    assert qtile.c.window.info()['width'] == 36
-    assert qtile.c.window.info()['height'] == 50
+    manager.c.window.place(10, 20, 50, 60, 0, '000000', margin=[2, 4, 8, 10])
+    assert manager.c.window.info()['x'] == 20
+    assert manager.c.window.info()['y'] == 22
+    assert manager.c.window.info()['width'] == 36
+    assert manager.c.window.info()['height'] == 50
