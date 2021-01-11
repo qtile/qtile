b2fc5c151fc1c7876493b64de239f235524ae7c2
Drew Ditthardt <drew.ditthardt@gmail.com>
2021-01-11 09:39:07 -0500

    Deprecation warnings for command_*.py

---
+++ libqtile/command_graph.py
@@ -0,0 +1,7 @@
+from libqtile.command.graph import *  # noqa
+from libqtile.log_utils import logger
+
+logger.warning(
+    'libqtile.command_graph is deprecated. '
+    'It has been moved to libqtile.command.graph'
+)
---
+++ libqtile/command_interface.py
@@ -0,0 +1,7 @@
+from libqtile.command.interface import *  # noqa
+from libqtile.log_utils import logger
+
+logger.warning(
+    'libqtile.command_interface is deprecated. '
+    'It has been moved to libqtile.command.interface'
+)
