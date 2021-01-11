8a6aa85639fa158423a2a1a7150ca85c8c7a55e1
Drew Ditthardt <drew.ditthardt@gmail.com>
2021-01-11 09:39:07 -0500

    Deprecation warnings for command_*.py

---
+++ libqtile/command_client.py
@@ -0,0 +1,7 @@
+from libqtile.command.client import *  # noqa
+from libqtile.log_utils import logger
+
+logger.warning(
+    'libqtile.command_client is deprecated. '
+    'It has been moved to libqtile.command.client'
+)
---
+++ libqtile/command_object.py
@@ -0,0 +1,7 @@
+from libqtile.command.base import *  # noqa
+from libqtile.log_utils import logger
+
+logger.warning(
+    'libqtile.command_object is deprecated. '
+    'It has been moved to libqtile.command.base.'
+)
