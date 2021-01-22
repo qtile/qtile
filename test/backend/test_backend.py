f987e1a082a2c183569d5a84248f8db9573234bd
Drew Ditthardt <drew.ditthardt@gmail.com>
2021-01-22 12:44:49 -0500

    Added display fixture
    This cleans up unnecessary instantiation of manager

--- test/backend/test_backend.py
+++ test/backend/test_backend.py
@@ -4,10 +4,10 @@ from libqtile.backend import get_core
 from libqtile.utils import QtileError
 
 
-def test_get_core_x11(manager_nospawn):
-    get_core('x11', manager_nospawn.display).finalize()
+def test_get_core_x11(display):
+    get_core('x11', display).finalize()
 
 
-def test_get_core_bad(manager_nospawn):
+def test_get_core_bad():
     with pytest.raises(QtileError):
         get_core("NonBackend").finalize()
