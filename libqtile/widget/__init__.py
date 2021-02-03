2c9542ce9ba467b7b1eeb05661300ce8d00b6181
Tycho Andersen <tycho@tycho.pizza>
2021-02-02 20:44:26 -0700

    widget: drop YahooWeather
    RIP Yahoo weather API
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- libqtile/widget/__init__.py
+++ libqtile/widget/__init__.py
@@ -88,7 +88,6 @@ widgets = {
     "WindowName": "windowname",
     "WindowTabs": "windowtabs",
     "Wlan": "wlan",
-    "YahooWeather": "yahoo_weather"
 }
 
 __all__, __dir__, __getattr__ = lazify_imports(widgets, __package__,
