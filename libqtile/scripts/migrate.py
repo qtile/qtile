b0bd16b2d2a1aae57b51943f2d1d027c933d272c
Tycho Andersen <tycho@tycho.pizza>
2021-02-17 13:22:21 -0700

    qtile: add migrate command
    Here's an implementation of the proposed `qtile migrate` command.
    
    For now, I've used pybowler. While they say in their docs that their API
    may not be stable, it seems mostly unchanged since 2016 when it was
    introduced. I played around with other refactoring packages like rope, and
    pybowler ends up being much simpler for the most part.
    
    I tried to write a variety of migrations from our last release that people
    complained about. One I looked at briefly but couldn't figure out was how
    to automatically remove the qtile arg mouse_callbacks in widget
    constructors. I think it's probably reasonably easy if anyone is
    interested.
    
    One thing I didn't do was write any sort of policy about what kinds of
    requirements we want to place on API breaking changes, or what we want to
    state as our official API. For now, I'd like to require any API breaking
    changes at least do a good faith effort at a migration, and let's see how
    much of a burden it places. But others' thoughts are welcome on this policy
    :)
    
    I'm going to say this:
    
    Closes #1773
    
    although the conversation about API stability continues...
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

---
+++ libqtile/scripts/migrate.py
@@ -0,0 +1,116 @@
+# Copyright (c) 2021, Tycho Andersen. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+import os
+import os.path
+import shutil
+import sys
+
+BACKUP_SUFFIX = ".migrate.bak"
+
+try:
+    import bowler
+except ImportError:
+    pass
+
+
+def rename_hook(config, fro, to):
+    # could match on dotted_name< 'hook' '.' 'subscribe' '.' '{name}' >
+    # but the replacement gets more complicated...
+    selector = "'{name}'".format(name=fro)
+    q = bowler.Query(config).select_pattern(selector)
+    q.current.kwargs["name"] = fro
+    return q.rename(to)
+
+
+def client_name_updated(config):
+    """ Rename window_name_change -> client_name_updated"""
+    return rename_hook(config, "window_name_change", "client_name_updated")
+
+
+def tile_master_windows_rename(config):
+    return (
+        bowler.Query(config)
+        .select_function("Tile")
+        .modify_argument("masterWindows", "master_length")
+    )
+
+
+def threaded_poll_text_rename(config):
+    return (
+        bowler.Query(config)
+        .select_class("ThreadedPollText")
+        .rename("ThreadPoolText")
+    )
+
+
+MIGRATIONS = [
+    client_name_updated,
+    tile_master_windows_rename,
+    threaded_poll_text_rename,
+]
+
+
+MODULE_RENAMES = [
+    ("libqtile.command_graph", "libqtile.command.graph"),
+    ("libqtile.command_client", "libqtile.command.client"),
+    ("libqtile.command_interface", "libqtile.command.interface"),
+    ("libqtile.command_object", "libqtile.command.object"),
+]
+
+for (fro, to) in MODULE_RENAMES:
+    def f(config, fro=fro, to=to):
+        return (
+            bowler.Query(config)
+            .select_module(fro)
+            .rename(to)
+        )
+    MIGRATIONS.append(f)
+
+
+def do_migrate(args):
+    if "bowler" not in sys.modules:
+        print("bowler can't be found, not migrating config file")
+        print("install it and try again")
+        sys.exit(1)
+
+    shutil.copyfile(args.config, args.config+BACKUP_SUFFIX)
+
+    for m in MIGRATIONS:
+        m(args.config).execute(interactive=args.interactive, write=True)
+
+
+def add_subcommand(subparsers):
+    parser = subparsers.add_parser(
+        "migrate", help="Migrate a configuration file to the current API"
+    )
+    parser.add_argument(
+        "-c",
+        "--config",
+        action="store",
+        default=os.path.expanduser(
+            os.path.join(os.getenv("XDG_CONFIG_HOME", "~/.config"), "qtile", "config.py")
+        ),
+        help="Use the specified configuration file",
+    )
+    parser.add_argument(
+        "--interactive",
+        action="store_true",
+        help="Interactively apply diff (similar to git add -p)",
+    )
+    parser.set_defaults(func=do_migrate)
