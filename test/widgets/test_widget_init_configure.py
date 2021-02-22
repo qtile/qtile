f3e7aafc39f3d44ae7a5b9468ff5b23514bb4188
Tycho Andersen <tycho@tycho.pizza>
2021-02-22 10:56:21 -0700

    tests: fix a couple of config instantiations
    622b69428c3b ("Manage Qtile subprocess better") rearranged the way configs
    get instantiated, and run_qtile() now automatically instantiates them if
    the class object is passed.
    
    However, these two tests were making modifications to the class object,
    which were then getting discarded by the instantiation in run_qtile().
    
    We can't just drop this instantiation, because Config.load expects a self
    argument:
    
    Traceback (most recent call last):
      File "/home/tycho/packages/qtile/libqtile/core/manager.py", line 104, in load_config
        self.config.load()
    TypeError: load() missing 1 required positional argument: 'self'
    
    which means if we're going to make local modifications to config objects, we
    need to instantiate them before we do so.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- test/widgets/test_widget_init_configure.py
+++ test/widgets/test_widget_init_configure.py
@@ -82,7 +82,7 @@ def test_widget_init_config(manager_nospawn, widget_class, kwargs):
         assert getattr(widget, k) == v
 
     # Test configuration
-    config = MinimalConf
+    config = MinimalConf()
     config.screens = [
         libqtile.config.Screen(
             top=libqtile.bar.Bar([widget], 10)
