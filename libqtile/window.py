094041e22ac5826392fe021379e51de40caf20db
ep12 <epsilonproducts12@gmail.com>
2021-02-26 15:36:20 +0100

    Implement the proposed fix for #2242

--- libqtile/window.py
+++ libqtile/window.py
@@ -1164,10 +1164,13 @@ class Window(_Window):
                     logger.info("Focusing window")
                     self.qtile.current_screen.set_group(self.group)
                     self.group.focus(self)
-                elif focus_behavior == "smart" and self.group.screen and self.group.screen == self.qtile.current_screen:
-                    logger.info("Focusing window")
-                    self.qtile.current_screen.set_group(self.group)
-                    self.group.focus(self)
+                elif focus_behavior == "smart" and self.group.screen:
+                    if self.group.screen == self.qtile.current_screen:
+                        logger.info("Focusing window")
+                        self.qtile.current_screen.set_group(self.group)
+                        self.group.focus(self)
+                    else:
+                        logger.info("Ignoring focus request")
                 elif focus_behavior == "urgent" or (focus_behavior == "smart" and not self.group.screen):
                     logger.info("Setting urgent flag for window")
                     self.urgent = True
