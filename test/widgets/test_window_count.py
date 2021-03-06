0727a0ea18387f6582bbae7f785b549135f43f70
Tycho Andersen <tycho@tycho.pizza>
2021-03-06 09:15:55 -0700

    window_count test: get rid of a bunch of custom infra
    We can implement this test without all the custom mocking infrastructure,
    and actually run more of the code in the process. Let's do that.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- test/widgets/test_window_count.py
+++ test/widgets/test_window_count.py
@@ -1,70 +1,38 @@
-from libqtile import hook
-from libqtile.bar import Bar
+import libqtile
 from libqtile.widget import WindowCount
 
 
-class FakeWindowList:
-    def __init__(self):
-        self.windows = []
+def test_window_count(manager_nospawn):
+    class WindowCountConf(libqtile.confreader.Config):
+        groups = [libqtile.config.Group("a"), libqtile.config.Group("b")]
+        screens = [
+            libqtile.config.Screen(
+                top=libqtile.bar.Bar([WindowCount()], 10)
+            )
+        ]
 
-
-class FakeQtile:
-    def __init__(self):
-        self.groups = {1: FakeWindowList(), 0: FakeWindowList()}
-        self._current_group = 1
-
-    def add_window(self):
-        self.groups[self._current_group].windows.append("window")
-        hook.fire("client_managed", "window")
-
-    def close_window(self):
-        try:
-            self.groups[self._current_group].windows.pop()
-            hook.fire("client_killed", "window")
-        except IndexError:
-            pass
-
-    def switch_group(self):
-        self._current_group = (self._current_group + 1) % 2
-        hook.fire("setgroup")
-
-    def draw(self):
-        pass
-
-    @property
-    def current_group(self):
-        return self.groups[self._current_group]
-
-
-def test_window_count():
-    wc = WindowCount()
-    fakebar = Bar([wc], 24)
-    wc.bar = fakebar
-    qtile = FakeQtile()
-    wc.qtile = qtile
-    wc._setup_hooks()
+    manager_nospawn.start(WindowCountConf)
 
     # No windows opened
-    assert wc._count == 0
+    assert int(manager_nospawn.c.widget["windowcount"].get()) == 0
 
     # Add a window and check count
-    qtile.add_window()
-    assert wc._count == 1
+    one = manager_nospawn.test_window("one")
+    assert int(manager_nospawn.c.widget["windowcount"].get()) == 1
 
     # Add a window and check text
-    qtile.add_window()
-    assert wc.text == "2"
+    two = manager_nospawn.test_window("two")
+    assert manager_nospawn.c.widget["windowcount"].get() == "2"
 
     # Change to empty group
-    qtile.switch_group()
-    assert wc._count == 0
+    manager_nospawn.c.group["b"].toscreen()
+    assert int(manager_nospawn.c.widget["windowcount"].get()) == 0
 
     # Change back to group
-    qtile.switch_group()
-    assert wc._count == 2
+    manager_nospawn.c.group["a"].toscreen()
+    assert int(manager_nospawn.c.widget["windowcount"].get()) == 2
 
     # Close all windows and check count is 0 and widget not displayed
-    qtile.close_window()
-    qtile.close_window()
-    assert wc._count == 0
-    assert wc.calculate_length() == 0
+    manager_nospawn.kill_window(one)
+    manager_nospawn.kill_window(two)
+    assert int(manager_nospawn.c.widget["windowcount"].get()) == 0
