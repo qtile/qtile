fc43548ccd9e177401d4438fb4b612130ab7d74f
mcol <mcol@posteo.net>
2021-04-04 19:20:29 +0100

    Remove leftover references to deprecated `main` config variable

--- libqtile/resources/default_config.py
+++ libqtile/resources/default_config.py
@@ -158,7 +158,6 @@ mouse = [
 
 dgroups_key_binder = None
 dgroups_app_rules = []  # type: List
-main = None  # WARNING: this is deprecated and will be removed soon
 follow_mouse_focus = True
 bring_front_click = False
 cursor_warp = False
