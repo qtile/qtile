2ffc8e139b0fa45859c1813bfa85f4a96952124d
elParaguayo <elparaguayocode@gmail.com>
2021-04-15 12:21:15 +0100

    Fix copyrights

--- test/widgets/test_spacer.py
+++ test/widgets/test_spacer.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015 Tycho Andersen
+# Copyright (c) 2021 elParaguayo
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
