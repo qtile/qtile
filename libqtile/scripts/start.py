4d64313b63187ced4cc371ef0e81e68790baa82d
mcol <mcol@posteo.net>
2021-04-16 19:01:17 +0100

    Expose Wayland as available backend for development only
    This makes it much easier to work on the Wayland backend.  Also adds a
    message to the argument hints for `qtile start` to clarify that the
    Wayland backend is for development only.

--- libqtile/scripts/start.py
+++ libqtile/scripts/start.py
@@ -132,6 +132,6 @@ def add_subcommand(subparsers, parents):
         default='x11',
         dest='backend',
         choices=libqtile.backend.CORES,
-        help='Use specified backend. Currently only x11 is implemented.',
+        help='Use specified backend. Wayland backend is currently for development only.',
     )
     parser.set_defaults(func=start)
