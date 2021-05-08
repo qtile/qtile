qtile/qtile

commit 373ad5531e7b48ed28c650e7a895f661e98ce9a5

Move button mask handling to backend
This moves the setting up of config.Click and Drag and their masks etc
into the backend, as this concerns backend-specific logic. This is
similar to what is already done for keyboard keys. This also makes
xcbq.XCBQError subclass from QtileError so that backend errors can be
caught with the more generic class name by the Qtile class.

As a side-effect, resolution of modifiers from config (list of strings)
to mask is done once at startup rather than upon button press.

Additionally, cmd_simulate_keypress is delegated to the backend for
similar reasons as above.

libqtile/core/manager.py
@@ -74,7 +74,7 @@ class Qtile(CommandObject):
         self.socket_path = socket_path
 
         self._drag: Optional[Tuple] = None
-        self.mouse_map: Dict[int, List[Union[Click, Drag]]] = {}
+        self.mouse_map: Dict[Tuple[int, int], List[Union[Click, Drag]]] = {}
         self.mouse_position = (0, 0)
 
         self.windows_map: Dict[int, window._Window] = {}
@@ -85,7 +85,6 @@ class Qtile(CommandObject):
 
         self.keys_map: Dict[Tuple[int, int], Union[Key, KeyChord]] = {}
         self.chord_stack: List[KeyChord] = []
-        self.numlock_mask, self.valid_mask = self.core.masks
 
         self.current_screen: Optional[Screen] = None
         self.screens: List[Screen] = []
@@ -162,12 +161,8 @@ class Qtile(CommandObject):
         for key in self.config.keys:
             self.grab_key(key)
 
-        for i in self.config.mouse:
-            if self.mouse_map.get(i.button_code) is None:
-                self.mouse_map[i.button_code] = []
-            self.mouse_map[i.button_code].append(i)
-
-        self.grab_mouse()
+        for button in self.config.mouse:
+            self.grab_button(button)
 
         # no_spawn is set when we are restarting; we only want to run the
         # startup hook once.
@@ -430,10 +425,17 @@ class Qtile(CommandObject):
         for key in self.config.keys:
             self.grab_key(key)
 
-    def grab_mouse(self) -> None:
-        self.core.ungrab_buttons()
-        for mouse in self.config.mouse:
-            self.core.grab_button(mouse)
+    def grab_button(self, button: Union[Click, Drag]) -> None:
+        """Grab the given mouse button event"""
+        try:
+            modmask = self.core.grab_button(button)
+        except utils.QtileError:
+            logger.warning(f"Unknown modifier(s): {button.modifiers}")
+            return
+        key = (button.button_code, modmask)
+        if key not in self.mouse_map:
+            self.mouse_map[key] = []
+        self.mouse_map[key].append(button)
 
     def update_desktops(self) -> None:
         try:
@@ -771,16 +773,9 @@ class Qtile(CommandObject):
         self.conn.conn.core.AllowEvents(xcffib.xproto.Allow.ReplayPointer, e.time)
         self.conn.conn.flush()
 
-    def process_button_click(self, button_code, state, x, y, event) -> None:
+    def process_button_click(self, button_code, modmask, x, y, event) -> None:
         self.mouse_position = (x, y)
-        for m in self.mouse_map.get(button_code, []):
-            try:
-                modmask = xcbq.translate_masks(m.modifiers)
-            except xcbq.XCBQError as e:
-                raise utils.QtileError(e)
-            if not m or modmask & self.valid_mask != state & self.valid_mask:
-                logger.debug("Ignoring unknown button: %s" % button_code)
-                continue
+        for m in self.mouse_map.get((button_code, modmask), []):
             if isinstance(m, Click):
                 for i in m.commands:
                     if i.check(self):
@@ -813,8 +808,8 @@ class Qtile(CommandObject):
                 self._drag = (x, y, val[0], val[1], m.commands)
                 self.core.grab_pointer()
 
-    def process_button_release(self, button_code):
-        k = self.mouse_map.get(button_code)
+    def process_button_release(self, button_code, modmask):
+        k = self.mouse_map.get((button_code, modmask))
         for m in k:
             if not m:
                 logger.info(
@@ -1149,20 +1144,13 @@ class Qtile(CommandObject):
         ========
             simulate_keypress(["control", "mod2"], "k")
         """
-        # FIXME: This needs to be done with sendevent, once we have that fixed.
-        try:
-            modmasks = xcbq.translate_masks(modifiers)
-            keysym = xcbq.keysyms.get(key)
-        except xcbq.XCBQError as e:
-            raise CommandError(str(e))
-
-        class DummyEv:
-            pass
-
-        d = DummyEv()
-        d.detail = self.conn.keysym_to_keycode(keysym)[0]
-        d.state = modmasks
-        self.core.handle_KeyPress(d)
+        if hasattr(self.core, 'simulate_keypress'):
+            try:
+                self.core.simulate_keypress(modifiers, key)
+            except utils.QtileError as e:
+                raise CommandError(str(e))
+        else:
+            raise CommandError("Backend does not support simulating keypresses")
 
     def cmd_validate_config(self):
         try:

libqtile/backend/base.py
@@ -49,7 +49,7 @@ class Core(metaclass=ABCMeta):
         """Release the grabbed key events"""
 
     @abstractmethod
-    def grab_button(self, mouse: config.Mouse) -> None:
+    def grab_button(self, mouse: config.Mouse) -> int:
         """Configure the backend to grab the mouse event"""
 
     @abstractmethod

libqtile/backend/wayland/core.py
@@ -310,12 +310,13 @@ class Core(base.Core):
         """Release the grabbed key events"""
         self.grabbed_keys.clear()
 
-    def grab_button(self, mouse: config.Mouse) -> None:
+    def grab_button(self, mouse: config.Mouse) -> int:
         """Configure the backend to grab the mouse event"""
-        keysym = wlrq.buttons.get(mouse.button)
+        keysym = wlrq.buttons.get(mouse.button.lower())
         assert keysym is not None
         mask_key = wlrq.translate_masks(mouse.modifiers)
         self.grabbed_buttons.append((keysym, mask_key))
+        return mask_key
 
     def ungrab_buttons(self) -> None:
         """Release the grabbed button events"""

libqtile/backend/x11/core.py
@@ -189,10 +189,6 @@ class Core(base.Core):
         self._wmname = wmname
         self._supporting_wm_check_window.set_property("_NET_WM_NAME", wmname)
 
-    @property
-    def masks(self) -> Tuple[int, int]:
-        return self._numlock_mask, self._valid_mask
-
     def setup_listener(
         self, qtile: "Qtile"
     ) -> None:
@@ -465,12 +461,9 @@ class Core(base.Core):
         """Ungrab the focus for pointer events"""
         self.conn.conn.core.UngrabPointer(xcffib.xproto.Atom._None)
 
-    def grab_button(self, mouse: config.Mouse) -> None:
+    def grab_button(self, mouse: config.Mouse) -> int:
         """Grab the given mouse button for events"""
-        try:
-            modmask = xcbq.translate_masks(mouse.modifiers)
-        except xcbq.XCBQError as err:
-            raise utils.QtileError(err)
+        modmask = xcbq.translate_masks(mouse.modifiers)
 
         if isinstance(mouse, config.Click) and mouse.focus:
             # Make a freezing grab on mouse button to gain focus
@@ -495,6 +488,8 @@ class Core(base.Core):
                 mouse.button_code,
                 modmask | amask,
             )
+
+        return modmask & self._valid_mask
 
     def ungrab_buttons(self) -> None:
         """Un-grab all mouse events"""
@@ -572,7 +567,9 @@ class Core(base.Core):
         assert self.qtile is not None
 
         button_code = event.detail
-        self.qtile.process_button_release(button_code)
+        state = event.state | self._numlock_mask
+        state &= self._valid_mask & ~xcbq.AllButtonsMask
+        self.qtile.process_button_release(button_code, state)
 
     def handle_MotionNotify(self, event) -> None:  # noqa: N802
         assert self.qtile is not None
@@ -628,3 +625,17 @@ class Core(base.Core):
         if self._painter is None:
             self._painter = xcbq.Painter(self._display_name)
         return self._painter
+
+    def simulate_keypress(self, modifiers, key):
+        """Simulates a keypress on the focused window."""
+        # FIXME: This needs to be done with sendevent, once we have that fixed.
+        modmasks = xcbq.translate_masks(modifiers)
+        keysym = xcbq.keysyms.get(key)
+
+        class DummyEv:
+            pass
+
+        d = DummyEv()
+        d.detail = self.conn.keysym_to_keycode(keysym)[0]
+        d.state = modmasks
+        self.handle_KeyPress(d)

libqtile/backend/x11/xcbq.py
@@ -52,12 +52,12 @@ from xcffib.xproto import CW, EventMask, WindowClass
 from libqtile import xkeysyms
 from libqtile.backend.x11.xcursors import Cursors
 from libqtile.log_utils import logger
-from libqtile.utils import hex
+from libqtile.utils import QtileError, hex
 
 keysyms = xkeysyms.keysyms
 
 
-class XCBQError(Exception):
+class XCBQError(QtileError):
     pass
 
 

commit eebd13a045935dfa0b200281a8b4f7265e043b58

Stop dragging only when mouse button is released
This lets drags continue for as long as the mouse button is held down,
even if modifier keys are released. This is old behaviour that was
broken by 373ad55. Fixes #2440.

libqtile/config.py
@@ -110,6 +110,7 @@ class Mouse:
         self.button_code = int(self.button.replace('Button', ''))
         for k, v in kwargs.items():
             setattr(self, k, v)
+        self.modmask: int = 0
 
 
 class Drag(Mouse):

libqtile/core/manager.py
@@ -68,7 +68,7 @@ class Qtile(CommandObject):
         self.socket_path = socket_path
 
         self._drag: Optional[Tuple] = None
-        self.mouse_map: Dict[Tuple[int, int], List[Union[Click, Drag]]] = {}
+        self.mouse_map: Dict[int, List[Union[Click, Drag]]] = {}
         self.mouse_position = (0, 0)
 
         self.windows_map: Dict[int, base.WindowType] = {}
@@ -408,14 +408,13 @@ class Qtile(CommandObject):
     def grab_button(self, button: Union[Click, Drag]) -> None:
         """Grab the given mouse button event"""
         try:
-            modmask = self.core.grab_button(button)
+            button.modmask = self.core.grab_button(button)
         except utils.QtileError:
             logger.warning(f"Unknown modifier(s): {button.modifiers}")
             return
-        key = (button.button_code, modmask)
-        if key not in self.mouse_map:
-            self.mouse_map[key] = []
-        self.mouse_map[key].append(button)
+        if button.button_code not in self.mouse_map:
+            self.mouse_map[button.button_code] = []
+        self.mouse_map[button.button_code].append(button)
 
     def update_desktops(self) -> None:
         try:
@@ -626,7 +625,10 @@ class Qtile(CommandObject):
 
     def process_button_click(self, button_code, modmask, x, y, event) -> None:
         self.mouse_position = (x, y)
-        for m in self.mouse_map.get((button_code, modmask), []):
+        for m in self.mouse_map.get(button_code, []):
+            if not m.modmask == modmask:
+                continue
+
             if m.focus == "before":
                 self.core.focus_by_click(event)
 
@@ -658,15 +660,11 @@ class Qtile(CommandObject):
                 self.core.focus_by_click(event)
 
     def process_button_release(self, button_code, modmask):
-        for m in self.mouse_map.get((button_code, modmask), []):
-            if not m:
-                logger.info(
-                    "Ignoring unknown button release: %s" % button_code
-                )
-                continue
+        for m in self.mouse_map.get(button_code, []):
             if isinstance(m, Drag):
                 self._drag = None
                 self.core.ungrab_pointer()
+                return
 
     def process_button_motion(self, x, y):
         self.mouse_position = (x, y)