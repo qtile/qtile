a86d0dbd1deb183405b4b9244600fc9b706db6ba
@desdemona <47291539+desdemona2@users.noreply.github.com>
2021-08-06 15:51:05 +0530

    Update __init__.py
    added accessDots in widgets dictionary, a widget to show active status for mic and camera.

--- libqtile/widget/__init__.py
+++ libqtile/widget/__init__.py
@@ -27,6 +27,7 @@ widgets = {
     "AGroupBox": "groupbox",
     "Backlight": "backlight",
     "Battery": "battery",
+    "AccessDots": "accessDots",
     "BatteryIcon": "battery",
     "Bluetooth": "bluetooth",
     "CPU": "cpu",
