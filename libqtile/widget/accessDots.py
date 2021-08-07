f2998ed854c06f015c0650471d473eaa29cd804e
@desdemona <47291539+desdemona2@users.noreply.github.com>
2021-08-07 13:51:41 +0530

    Update accessDots.py

--- libqtile/widget/accessDots.py
+++ libqtile/widget/accessDots.py
@@ -1,6 +1,6 @@
 # A qtile widget written to show active indication for mic or camera
 # my github (https://github.com/desdemona2).
-# maybe this is not well formatted as i am still a learner so feel free to modify this file.
+# modification suggested by elParaguayo (https://github.com/elParaguayo)
 # This feature is also included in iOS 14 and Android 12 and inspired from an app AccessDots for Android.
 
 from libqtile.widget import base
@@ -19,26 +19,21 @@ class active(base.ThreadPoolText):
             "widget updates whenever it's done'.",
         ),
         # ("fmt", "{mic_str} {cam_str}", "Display format for output"),
+        ("camDev", "/dev/video0", "Path to camera device"),
+        ("micDev", "/dev/snd/pcmC0D0c", "Path to Microphone device"),
+        ("camAct", "📸", "Indication when camera active"),
+        ("micAct", "📢", "Indication when Microphone active"),
     ]
 
-    def __init__(
-        self, camDev="video0", sndDev="pcmC0D0c", camAct="📸", micAct="📢", **config
-    ):
+    def __init__(self, **config):
         super().__init__("", **config)
         self.add_defaults(active.defaults)
 
-        self.camDev = camDev
-        self.micDev = sndDev
-
-        self.camAct = camAct
-        self.micAct = micAct
-
     def poll(self):
 
-        mic = os.system(f"fuser /dev/snd/{self.micDev}")
-        camera = os.system(f"fuser /dev/{self.camDev}")
+        mic = os.system(f"fuser {self.micDev}")
+        camera = os.system(f"fuser {self.camDev}")
 
         mic_str = "" if mic == 256 else self.micAct
         cam_str = "" if camera == 256 else self.camAct
-
-        return f"{mic_str}{cam_str}"
+        return f"{mic_str} {cam_str}"
