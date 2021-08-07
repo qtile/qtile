ada7e91dd09d0793488554aa249718f2eb845be3
elParaguayo <elparaguayocode@gmail.com>
2021-08-07 14:24:48 +0100

    Add graph access to group from screen

--- test/test_sh.py
+++ test/test_sh.py
@@ -74,7 +74,7 @@ def test_ls(manager):
 
     assert sh.do_cd("layout") == "layout"
     assert sh.do_ls(None) == "group/   window/  screen/"
-    assert sh.do_ls("screen") == "screen/layout/  screen/window/  screen/bar/     screen/widget/"
+    assert sh.do_ls("screen") == "screen/layout/  screen/window/  screen/bar/     screen/widget/  screen/group/ "
 
 
 @sh_config
