47aeb730c610d702ae75b1e82a2813de407534b8
schragge <schragge@gmail.com>
2021-11-05 22:25:03 +0100

    wttr.py: Fix docstring with a wrong units value
    wttr uses "u" to indicate US standard units should be used. This PR updates the docs to reflect this.
    
    Closes #2983

--- libqtile/widget/wttr.py
+++ libqtile/widget/wttr.py
@@ -67,7 +67,7 @@ class Wttr(GenPollUrl):
         (
             'units', 'm',
             "``'m'`` - metric, ``'M'`` - show wind speed in m/s, "
-            "``'s'`` - imperial"
+            "``'u'`` - United States units"
         ),
         (
             'update_interval', 600,
