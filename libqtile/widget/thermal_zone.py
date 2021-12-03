397be73725e2f9fd8794bed4a12901f175c07af7
Ilyas Arinov <ilyas.arinov@aitudala.kz>
2021-12-03 19:58:12 +0300

    Using full thermal zone path as default zone value.

--- libqtile/widget/thermal_zone.py
+++ libqtile/widget/thermal_zone.py
@@ -6,7 +6,7 @@ class ThermalZone(base.ThreadPoolText):
     orientations = base.ORIENTATION_HORIZONTAL
     defaults = [
         ('update_interval', 2.0, 'Update interval for the thermal zone sensor'),
-        ('zone', '0', 'Default thermal zone'),
+        ('zone', '/sys/class/thermal/thermal_zone0/temp', 'Default thermal zone'),
     ]
 
     def __init__(self, **config):
@@ -14,12 +14,11 @@ class ThermalZone(base.ThreadPoolText):
         self.add_defaults(ThermalZone.defaults)
 
     def poll(self):
-        zone = '/sys/class/thermal/thermal_zone' + self.zone + '/temp'
-        if path.isfile(zone):
-            with open(zone) as f:
+        if path.isfile(self.zone):
+            with open(self.zone) as f:
                 value = str(round(int(f.read().rstrip()) / 1000)) + '°C'
             return value
         else:
-            logger.exception('{} does not exist'.format(zone))
+            logger.exception('{} does not exist'.format(self.zone))
             return 'err!'
 
