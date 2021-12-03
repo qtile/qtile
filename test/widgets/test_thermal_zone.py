a4dff4c11e6cbd6b5f73f3052e6107ac19368425
Ilyas Arinov <ilyas.arinov@aitudala.kz>
2021-12-03 20:33:21 +0300

    Unit test update

--- test/widgets/test_thermal_zone.py
+++ test/widgets/test_thermal_zone.py
@@ -1,14 +1,17 @@
-import os.path
+import os
 from libqtile import widget
 
 def test_thermal_zone_getting_value():
-    # If thermal zone zero exists - get value
-    zone_zero = '/sys/class/thermal/thermal_zone0/temp'
-    zone_exists = os.path.isfile(zone_zero)
-    
-    if zone_exists:
-        thermal_zone = widget.ThermalZone(zone='0')
-        output = thermal_zone.poll()
-        print(output)
-        assert output != ''
+    # Create temporary zone file
+    tmp = '/var/tmp/qtile/test/widgets/thermal_zone'
+    zone_file = tmp + '/sys/class/thermal/thermal_zone0/temp'
+    os.makedirs(os.path.dirname(zone_file), exist_ok=True)
+
+    with open(zone_file, "w") as f:
+        f.write("22000")
+
+    thermal_zone = widget.ThermalZone(zone=zone_file)
+    output = thermal_zone.poll()
+    print(output)
+    assert output == '22°C'
 
