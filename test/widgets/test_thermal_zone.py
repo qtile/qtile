daac1a50bb4ad2c741568fad1b3aabd1a6245912
Ilyas Arinov <ilyas.arinov@aitudala.kz>
2021-12-08 01:26:54 +0300

    Using FakeLayout class to set the self.layout.colour and pass the test.

--- test/widgets/test_thermal_zone.py
+++ test/widgets/test_thermal_zone.py
@@ -8,9 +8,13 @@ def test_thermal_zone_getting_value():
     zone_file = tmp + '/sys/class/thermal/thermal_zone0/temp'
     os.makedirs(os.path.dirname(zone_file), exist_ok=True)
 
+    class FakeLayout:
+        pass
+
     with open(zone_file, "w") as f:
         f.write("22000")
 
     thermal_zone = widget.ThermalZone(zone=zone_file)
+    thermal_zone.layout = FakeLayout()
     output = thermal_zone.poll()
     assert output == '22°C'
