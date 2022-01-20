feac6005c834a403c6a640511a9dbced23b5bfd8
elParaguayo <elParaguayo@users.noreply.github.com>
2022-01-20 09:50:58 +0000

    Merge pull request #3203 from elParaguayo/fix-volume-widget-theme-handling
    Change Volume widget's handling of theme_path

--- libqtile/widget/volume.py
+++ libqtile/widget/volume.py
@@ -83,9 +83,6 @@ class Volume(base._TextBox):
     def __init__(self, **config):
         base._TextBox.__init__(self, "0", width=bar.CALCULATED, **config)
         self.add_defaults(Volume.defaults)
-        if self.theme_path:
-            self.length_type = bar.STATIC
-            self.length = 0
         self.surfaces = {}
         self.volume = None
 
@@ -97,6 +94,12 @@ class Volume(base._TextBox):
                 "Button5": self.cmd_decrease_vol,
             }
         )
+
+    def _configure(self, qtile, parent_bar):
+        if self.theme_path:
+            self.length_type = bar.STATIC
+            self.length = 0
+        base._TextBox._configure(self, qtile, parent_bar)
 
     def timer_setup(self):
         self.timeout_add(self.update_interval, self.update)
