qtile/qtile

commit 4c98020e14be7b022a305854a18b7a2df4cdb2eb

Use built-ins for type annotations rather than typing
`from __future__ import annotations` is required to prevent errors
during the runtime of Python 3.7

libqtile/widget/systray.py
@@ -26,7 +26,8 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from typing import List
+
+from __future__ import annotations
 
 import xcffib
 from xcffib.xproto import ClientMessageData, ClientMessageEvent, EventMask, SetMode
@@ -127,7 +128,7 @@ class Systray(window._Window, base._Widget):
         self.tray_icons = []
         self.screen = 0
         self._name = config.get("name", "systray")
-        self._wm_class: List[str] | None = None
+        self._wm_class: list[str] | None = None
 
     def calculate_length(self):
         if self.bar.horizontal:
@@ -220,7 +221,7 @@ class Systray(window._Window, base._Widget):
                 self.tray_icons.sort(key=lambda icon: icon.name)
                 self.qtile.windows_map[wid] = icon
 
-            self.conn.conn.core.ChangeSaveSet(SetMode.Insert, wid)
+            self.conn.conn.core.ChangeSaveset(SetMode.Insert, wid)
             self.conn.conn.core.ReparentWindow(wid, parent.wid, 0, 0)
             self.conn.conn.flush()
 

libqtile/backend/base.py
@@ -13,7 +13,7 @@ from libqtile.command.base import CommandError, CommandObject
 from libqtile.log_utils import logger
 
 if typing.TYPE_CHECKING:
-    from typing import Any, Dict, List, Tuple
+    from typing import Any
 
     from libqtile import config
     from libqtile.command.base import ItemT
@@ -55,19 +55,19 @@ class Core(CommandObject, metaclass=ABCMeta):
     def remove_listener(self) -> None:
         """Setup a listener for the given qtile instance"""
 
-    def update_desktops(self, groups: List[_Group], index: int) -> None:
+    def update_desktops(self, groups: list[_Group], index: int) -> None:
         """Set the current desktops of the window manager"""
 
     @abstractmethod
-    def get_screen_info(self) -> List[Tuple[int, int, int, int]]:
+    def get_screen_info(self) -> list[tuple[int, int, int, int]]:
         """Get the screen information"""
 
     @abstractmethod
-    def grab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def grab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Configure the backend to grab the key event"""
 
     @abstractmethod
-    def ungrab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def ungrab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Release the given key event"""
 
     @abstractmethod
@@ -96,7 +96,7 @@ class Core(CommandObject, metaclass=ABCMeta):
     def warp_pointer(self, x: int, y: int) -> None:
         """Warp the pointer to the given coordinates relative."""
 
-    def update_client_list(self, windows_map: Dict[int, WindowType]) -> None:
+    def update_client_list(self, windows_map: dict[int, WindowType]) -> None:
         """Update the list of windows being managed"""
 
     @contextlib.contextmanager
@@ -114,14 +114,14 @@ class Core(CommandObject, metaclass=ABCMeta):
     def graceful_shutdown(self):
         """Try to close windows gracefully before exiting"""
 
-    def simulate_keypress(self, modifiers: List[str], key: str) -> None:
+    def simulate_keypress(self, modifiers: list[str], key: str) -> None:
         """Simulate a keypress with given modifiers"""
 
     def keysym_from_name(self, name: str) -> int:
         """Get the keysym for a key from its name"""
         raise NotImplementedError
 
-    def cmd_info(self) -> Dict:
+    def cmd_info(self) -> dict:
         """Get basic information about the running backend."""
         return {"backend": self.name, "display_name": self.display_name}
 
@@ -140,7 +140,7 @@ class _Window(CommandObject, metaclass=ABCMeta):
     def __init__(self):
         self.borderwidth: int = 0
         self.name: str = "<no name>"
-        self.reserved_space: Tuple[int, int, int, int] | None = None
+        self.reserved_space: tuple[int, int, int, int] | None = None
         # Window.cmd_static sets this in case it is hooked to client_new to stop the
         # Window object from being managed, now that a Static is being used instead
         self.defunct: bool = False
@@ -162,7 +162,7 @@ class _Window(CommandObject, metaclass=ABCMeta):
     def kill(self) -> None:
         """Kill the window"""
 
-    def get_wm_class(self) -> List | None:
+    def get_wm_class(self) -> list | None:
         """Return the class(es) of the window"""
         return None
 
@@ -214,7 +214,7 @@ class _Window(CommandObject, metaclass=ABCMeta):
         return None
 
     @abstractmethod
-    def info(self) -> Dict[str, Any]:
+    def info(self) -> dict[str, Any]:
         """
         Return information on this window.
 
@@ -231,7 +231,7 @@ class _Window(CommandObject, metaclass=ABCMeta):
         """
         return {}
 
-    def cmd_info(self) -> Dict:
+    def cmd_info(self) -> dict:
         """Return a dictionary of info."""
         return self.info()
 
@@ -331,11 +331,11 @@ class Window(_Window, metaclass=ABCMeta):
         return self.match(*args, **kwargs)
 
     @abstractmethod
-    def cmd_get_position(self) -> Tuple[int, int]:
+    def cmd_get_position(self) -> tuple[int, int]:
         """Get the (x, y) of the window"""
 
     @abstractmethod
-    def cmd_get_size(self) -> Tuple[int, int]:
+    def cmd_get_size(self) -> tuple[int, int]:
         """Get the (width, height) of the window"""
 
     @abstractmethod
@@ -561,7 +561,7 @@ class Static(_Window, metaclass=ABCMeta):
     def __repr__(self):
         return "Static(name=%r, wid=%s)" % (self.name, self.wid)
 
-    def info(self) -> Dict:
+    def info(self) -> dict:
         """Return a dictionary of info."""
         return dict(
             name=self.name,
@@ -585,8 +585,8 @@ class Drawer:
     """
 
     # We need to track extent of drawing to know when to redraw.
-    previous_rect: Tuple[int, int, int | None, int | None]
-    current_rect: Tuple[int, int, int | None, int | None]
+    previous_rect: tuple[int, int, int | None, int | None]
+    current_rect: tuple[int, int, int | None, int | None]
 
     def __init__(self, qtile: Qtile, win: Internal, width: int, height: int):
         self.qtile = qtile
@@ -598,7 +598,7 @@ class Drawer:
         self.ctx: cairocffi.Context
         self._reset_surface()
 
-        self.mirrors: Dict[Drawer, bool] = {}
+        self.mirrors: dict[Drawer, bool] = {}
 
         self.current_rect = (0, 0, 0, 0)
         self.previous_rect = (-1, -1, -1, -1)

libqtile/backend/wayland/core.py
@@ -71,7 +71,7 @@ from libqtile.backend.wayland.output import Output
 from libqtile.log_utils import logger
 
 if typing.TYPE_CHECKING:
-    from typing import Dict, List, Sequence, Set, Tuple
+    from typing import Sequence
 
     from wlroots.wlr_types import Output as wlrOutput
     from wlroots.wlr_types.data_device_manager import Drag
@@ -105,18 +105,18 @@ class Core(base.Core, wlrq.HasListeners):
         logger.info("Starting core with WAYLAND_DISPLAY=" + self.socket.decode())
 
         # These windows have not been mapped yet; they'll get managed when mapped
-        self.pending_windows: Set[window.WindowType] = set()
+        self.pending_windows: set[window.WindowType] = set()
 
         # mapped_windows contains just regular windows
-        self.mapped_windows: List[window.WindowType] = []  # Ascending in Z
+        self.mapped_windows: list[window.WindowType] = []  # Ascending in Z
         # stacked_windows also contains layer_shell windows from the current output
         self.stacked_windows: Sequence[window.WindowType] = []  # Ascending in Z
         self._current_output: Output | None = None
 
         # set up inputs
-        self.keyboards: List[keyboard.Keyboard] = []
-        self.grabbed_keys: List[Tuple[int, int]] = []
-        self.grabbed_buttons: List[Tuple[int, int]] = []
+        self.keyboards: list[keyboard.Keyboard] = []
+        self.grabbed_keys: list[tuple[int, int]] = []
+        self.grabbed_buttons: list[tuple[int, int]] = []
         DataDeviceManager(self.display)
         self.live_dnd: wlrq.Dnd | None = None
         DataControlManagerV1(self.display)
@@ -127,7 +127,7 @@ class Core(base.Core, wlrq.HasListeners):
         self.add_listener(self.backend.new_input_event, self._on_new_input)
 
         # set up outputs
-        self.outputs: List[Output] = []
+        self.outputs: list[Output] = []
         self.add_listener(self.backend.new_output_event, self._on_new_output)
         self.output_layout = OutputLayout()
         self.add_listener(self.output_layout.change_event, self._on_output_layout_change)
@@ -174,7 +174,7 @@ class Core(base.Core, wlrq.HasListeners):
             pointer_constraints_v1.new_constraint_event,
             self._on_new_pointer_constraint,
         )
-        self.pointer_constraints: Set[wlrq.PointerConstraint] = set()
+        self.pointer_constraints: set[wlrq.PointerConstraint] = set()
         self.active_pointer_constraint: wlrq.PointerConstraint | None = None
         self._relative_pointer_manager_v1 = RelativePointerManagerV1(self.display)
         self.foreign_toplevel_manager_v1 = ForeignToplevelManagerV1.create(self.display)
@@ -411,7 +411,7 @@ class Core(base.Core, wlrq.HasListeners):
         logger.debug("Signal: xwayland ready")
         assert self._xwayland is not None
         self._xwayland.set_seat(self.seat)
-        self.xwayland_atoms: Dict[int, str] = wlrq.get_xwayland_atoms(self._xwayland)
+        self.xwayland_atoms: dict[int, str] = wlrq.get_xwayland_atoms(self._xwayland)
 
     def _on_xwayland_new_surface(self, _listener, surface: xwayland.Surface):
         logger.debug("Signal: xwayland new_surface")
@@ -754,18 +754,18 @@ class Core(base.Core, wlrq.HasListeners):
         else:
             self.stacked_windows = self.mapped_windows
 
-    def get_screen_info(self) -> List[Tuple[int, int, int, int]]:
+    def get_screen_info(self) -> list[tuple[int, int, int, int]]:
         """Get the screen information"""
         return [screen.get_geometry() for screen in self.outputs if screen.wlr_output.enabled]
 
-    def grab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def grab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Configure the backend to grab the key event"""
         keysym = xkb.keysym_from_name(key.key, case_insensitive=True)
         mask_key = wlrq.translate_masks(key.modifiers)
         self.grabbed_keys.append((keysym, mask_key))
         return keysym, mask_key
 
-    def ungrab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def ungrab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Release the given key event"""
         keysym = xkb.keysym_from_name(key.key, case_insensitive=True)
         mask_key = wlrq.translate_masks(key.modifiers)
@@ -839,7 +839,7 @@ class Core(base.Core, wlrq.HasListeners):
         """Get the keysym for a key from its name"""
         return xkb.keysym_from_name(name, case_insensitive=True)
 
-    def simulate_keypress(self, modifiers: List[str], key: str) -> None:
+    def simulate_keypress(self, modifiers: list[str], key: str) -> None:
         """Simulates a keypress on the focused window."""
         keysym = xkb.keysym_from_name(key, case_insensitive=True)
         mods = wlrq.translate_masks(modifiers)

libqtile/backend/wayland/keyboard.py
@@ -30,8 +30,6 @@ from libqtile.backend.wayland.wlrq import HasListeners
 from libqtile.log_utils import logger
 
 if typing.TYPE_CHECKING:
-    from typing import Dict, Tuple
-
     from wlroots.wlr_types import InputDevice
     from wlroots.wlr_types.keyboard import KeyboardKeyEvent
 
@@ -55,7 +53,7 @@ class Keyboard(HasListeners):
 
         self.keyboard.set_repeat_info(25, 600)
         self.xkb_context = xkb.Context()
-        self._keymaps: Dict[Tuple[str | None, str | None, str | None], xkb.Keymap] = {}
+        self._keymaps: dict[tuple[str | None, str | None, str | None], xkb.Keymap] = {}
         self.set_keymap(None, None, None)
 
         self.add_listener(self.keyboard.modifiers_event, self._on_modifier)

libqtile/backend/wayland/output.py
@@ -40,8 +40,6 @@ from libqtile.backend.wayland.wlrq import HasListeners
 from libqtile.log_utils import logger
 
 if typing.TYPE_CHECKING:
-    from typing import List, Tuple
-
     from wlroots.wlr_types import Surface
 
     from libqtile.backend.wayland.core import Core
@@ -66,7 +64,7 @@ class Output(HasListeners):
         self.add_listener(self._damage.frame_event, self._on_frame)
 
         # The layers enum indexes into this list to get a list of surfaces
-        self.layers: List[List[Static]] = [[] for _ in range(len(LayerShellV1Layer))]
+        self.layers: list[list[Static]] = [[] for _ in range(len(LayerShellV1Layer))]
 
         # This is run during tests, when we want to fix the output's geometry
         if wlr_output.is_headless and "PYTEST_CURRENT_TEST" in os.environ:
@@ -167,7 +165,7 @@ class Output(HasListeners):
                 wlr_output.render_software_cursors(damage=damage)
                 renderer.end()
 
-    def _render_surface(self, surface: Surface, sx: int, sy: int, rdata: Tuple) -> None:
+    def _render_surface(self, surface: Surface, sx: int, sy: int, rdata: tuple) -> None:
         texture = surface.get_texture()
         if texture is None:
             return
@@ -243,7 +241,7 @@ class Output(HasListeners):
                 self.renderer.render_texture_with_matrix(texture, matrix, 1)
                 icon.surface.send_frame_done(now)
 
-    def get_geometry(self) -> Tuple[int, int, int, int]:
+    def get_geometry(self) -> tuple[int, int, int, int]:
         width, height = self.wlr_output.effective_resolution()
         return int(self.x), int(self.y), width, height
 
@@ -313,7 +311,7 @@ class Output(HasListeners):
                         elif state.anchor == LayerSurfaceV1Anchor.RIGHT:
                             space[1] = state.exclusive_zone
 
-                    to_reserve: Tuple[int, int, int, int] = tuple(space)  # type: ignore
+                    to_reserve: tuple[int, int, int, int] = tuple(space)  # type: ignore
                     if win.reserved_space != to_reserve:
                         # Don't reserve more space if it's already been reserved
                         assert self.core.qtile is not None

libqtile/backend/wayland/window.py
@@ -42,8 +42,6 @@ from libqtile.command.base import CommandError
 from libqtile.log_utils import logger
 
 if typing.TYPE_CHECKING:
-    from typing import Dict, List, Set, Tuple
-
     from wlroots.wlr_types.surface import SubSurface as WlrSubSurface
 
     from libqtile.backend.wayland.core import Core
@@ -78,14 +76,14 @@ class Window(base.Window, HasListeners):
         self.qtile = qtile
         self.surface = surface
         self._group: _Group | None = None
-        self.popups: List[XdgPopupWindow] = []
-        self.subsurfaces: List[SubSurface] = []
+        self.popups: list[XdgPopupWindow] = []
+        self.subsurfaces: list[SubSurface] = []
         self._mapped: bool = False
         self.x = 0
         self.y = 0
-        self.bordercolor: List[ffi.CData] = [_rgb((0, 0, 0, 1))]
+        self.bordercolor: list[ffi.CData] = [_rgb((0, 0, 0, 1))]
         self._opacity: float = 1.0
-        self._outputs: Set[Output] = set()
+        self._outputs: set[Output] = set()
 
         # These become non-zero when being mapping for the first time
         self._width: int = 0
@@ -333,7 +331,7 @@ class Window(base.Window, HasListeners):
         )
         return pid[0]
 
-    def get_wm_class(self) -> List | None:
+    def get_wm_class(self) -> list | None:
         if self._app_id:
             return [self._app_id]
         return None
@@ -579,7 +577,7 @@ class Window(base.Window, HasListeners):
                 self.group.mark_floating(self, True)
             hook.fire("float_change")
 
-    def info(self) -> Dict:
+    def info(self) -> dict:
         """Return a dictionary of info."""
         float_info = {
             "x": self.float_x,
@@ -671,10 +669,10 @@ class Window(base.Window, HasListeners):
     def cmd_place(self, x, y, width, height, borderwidth, bordercolor, above=False, margin=None):
         self.place(x, y, width, height, borderwidth, bordercolor, above, margin)
 
-    def cmd_get_position(self) -> Tuple[int, int]:
+    def cmd_get_position(self) -> tuple[int, int]:
         return self.x, self.y
 
-    def cmd_get_size(self) -> Tuple[int, int]:
+    def cmd_get_size(self) -> tuple[int, int]:
         return self.width, self.height
 
     def cmd_toggle_floating(self) -> None:
@@ -767,7 +765,7 @@ class Internal(base.Internal, Window):
         self._opacity: float = 1.0
         self._width: int = width
         self._height: int = height
-        self._outputs: Set[Output] = set()
+        self._outputs: set[Output] = set()
         self._find_outputs()
         self._reset_texture()
         self._group = None
@@ -857,7 +855,7 @@ class Internal(base.Internal, Window):
         self._find_outputs()
         self.damage()
 
-    def info(self) -> Dict:
+    def info(self) -> dict:
         """Return a dictionary of info."""
         return dict(
             x=self.x,
@@ -886,7 +884,7 @@ class Static(base.Static, Window):
         self.qtile = qtile
         self.surface = surface
         self.screen = qtile.current_screen
-        self.subsurfaces: List[SubSurface] = []
+        self.subsurfaces: list[SubSurface] = []
         self._wid = wid
         self._mapped: bool = False
         self.x = 0
@@ -894,9 +892,9 @@ class Static(base.Static, Window):
         self._width = 0
         self._height = 0
         self.borderwidth: int = 0
-        self.bordercolor: List[ffi.CData] = [_rgb((0, 0, 0, 1))]
+        self.bordercolor: list[ffi.CData] = [_rgb((0, 0, 0, 1))]
         self.opacity: float = 1.0
-        self._outputs: Set[Output] = set()
+        self._outputs: set[Output] = set()
         self._float_state = FloatStates.FLOATING
         self.is_layer = False
         self._app_id: str | None = None
@@ -1074,7 +1072,7 @@ class XdgPopupWindow(HasListeners):
         self.parent = parent
         self.xdg_popup = xdg_popup
         self.core: Core = parent.core
-        self.popups: List[XdgPopupWindow] = []
+        self.popups: list[XdgPopupWindow] = []
 
         # Keep on output
         if isinstance(parent, XdgPopupWindow):
@@ -1129,7 +1127,7 @@ class SubSurface(HasListeners):
 
     def __init__(self, parent: WindowType | SubSurface, subsurface: WlrSubSurface):
         self.parent = parent
-        self.subsurfaces: List[SubSurface] = []
+        self.subsurfaces: list[SubSurface] = []
 
         self.add_listener(subsurface.destroy_event, self._on_destroy)
         self.add_listener(subsurface.surface.commit_event, parent._on_commit)
@@ -1167,9 +1165,9 @@ class XWindow(Window):
         self._unmapping: bool = False  # Whether the client or Qtile unmapped this
         self.x = 0
         self.y = 0
-        self.bordercolor: List[ffi.CData] = [_rgb((0, 0, 0, 1))]
+        self.bordercolor: list[ffi.CData] = [_rgb((0, 0, 0, 1))]
         self._opacity: float = 1.0
-        self._outputs: Set[Output] = set()
+        self._outputs: set[Output] = set()
 
         self._app_id: str | None = self.surface.wm_class
         self.ftm_handle = core.foreign_toplevel_manager_v1.create_handle()

libqtile/backend/wayland/wlrq.py
@@ -39,7 +39,7 @@ from libqtile.log_utils import logger
 from libqtile.utils import QtileError
 
 if TYPE_CHECKING:
-    from typing import Callable, Dict, List, Set
+    from typing import Callable
 
     from pywayland.server import Signal
     from wlroots import xwayland
@@ -94,7 +94,7 @@ buttons = [
 DRM_FORMAT_ARGB8888 = 875713089
 
 
-def translate_masks(modifiers: List[str]) -> int:
+def translate_masks(modifiers: list[str]) -> int:
     """
     Translate a modifier mask specified as a list of strings into an or-ed
     bit representation.
@@ -261,7 +261,7 @@ class Dnd(HasListeners):
     def __init__(self, core: Core, wlr_drag: data_device_manager.Drag):
         self.core = core
         self.wlr_drag = wlr_drag
-        self._outputs: Set[Output] = set()
+        self._outputs: set[Output] = set()
 
         self.x: float = core.cursor.x
         self.y: float = core.cursor.y
@@ -308,7 +308,7 @@ class Dnd(HasListeners):
             output.damage()
 
 
-def get_xwayland_atoms(xwayland: xwayland.XWayland) -> Dict[int, str]:
+def get_xwayland_atoms(xwayland: xwayland.XWayland) -> dict[int, str]:
     """
     These can be used when matching on XWayland clients with wm_type.
     http://standards.freedesktop.org/wm-spec/latest/ar01s05.html#idm139870830002400

libqtile/backend/x11/core.py
@@ -41,7 +41,7 @@ from libqtile.log_utils import logger
 from libqtile.utils import QtileError
 
 if TYPE_CHECKING:
-    from typing import Callable, Dict, Iterator, List, Tuple
+    from typing import Callable, Iterator
 
     from libqtile.core.manager import Qtile
 
@@ -56,11 +56,11 @@ _IGNORED_EVENTS = {
 }
 
 
-def get_keys() -> List[str]:
+def get_keys() -> list[str]:
     return list(xcbq.keysyms.keys())
 
 
-def get_modifiers() -> List[str]:
+def get_modifiers() -> list[str]:
     return list(xcbq.ModMasks.keys())
 
 
@@ -169,7 +169,7 @@ class Core(base.Core):
         self.qtile = None
         self.conn.finalize()
 
-    def get_screen_info(self) -> List[Tuple[int, int, int, int]]:
+    def get_screen_info(self) -> list[tuple[int, int, int, int]]:
         info = [(s.x, s.y, s.width, s.height) for s in self.conn.pseudoscreens]
 
         if not info:
@@ -335,7 +335,7 @@ class Core(base.Core):
                 logger.exception("Got an exception in poll loop")
         self.flush()
 
-    def _get_target_chain(self, event_type: str, event) -> List[Callable]:
+    def _get_target_chain(self, event_type: str, event) -> list[Callable]:
         """Returns a chain of targets that can handle this event
 
         Finds functions named `handle_X`, either on the window object itself or
@@ -412,7 +412,7 @@ class Core(base.Core):
         """The name of the connected display"""
         return self._display_name
 
-    def update_client_list(self, windows_map: Dict[int, base.WindowType]) -> None:
+    def update_client_list(self, windows_map: dict[int, base.WindowType]) -> None:
         """Updates the client stack list
 
         This is needed for third party tasklists and drag and drop of tabs in
@@ -438,7 +438,7 @@ class Core(base.Core):
             viewport += [group.screen.x, group.screen.y] if group.screen else [0, 0]
         self._root.set_property("_NET_DESKTOP_VIEWPORT", viewport)
 
-    def lookup_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def lookup_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Find the keysym and the modifier mask for the given key"""
         try:
             keysym = xcbq.get_keysym(key.key)
@@ -448,7 +448,7 @@ class Core(base.Core):
 
         return keysym, modmask
 
-    def grab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def grab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Map the key to receive events on it"""
         keysym, modmask = self.lookup_key(key)
         codes = self.conn.keysym_to_keycode(keysym)
@@ -468,7 +468,7 @@ class Core(base.Core):
                 )
         return keysym, modmask & self._valid_mask
 
-    def ungrab_key(self, key: config.Key | config.KeyChord) -> Tuple[int, int]:
+    def ungrab_key(self, key: config.Key | config.KeyChord) -> tuple[int, int]:
         """Ungrab the key corresponding to the given keysym and modifier mask"""
         keysym, modmask = self.lookup_key(key)
         codes = self.conn.keysym_to_keycode(keysym)
@@ -837,7 +837,7 @@ class Core(base.Core):
                 break
             time.sleep(0.1)
 
-    def get_mouse_position(self) -> Tuple[int, int]:
+    def get_mouse_position(self) -> tuple[int, int]:
         """
         Get mouse coordinates.
         """

commit c7b2e8e34637c75845998ad20c95a0fc87218583

Fix typo in SysTray
4c98020e introduced a typo in Systray which results in icons
being displayed incorrectly.

This wasn't caught by tests because we don't really test the
Systray widget...

libqtile/widget/systray.py
@@ -221,7 +221,7 @@ class Systray(window._Window, base._Widget):
                 self.tray_icons.sort(key=lambda icon: icon.name)
                 self.qtile.windows_map[wid] = icon
 
-            self.conn.conn.core.ChangeSaveset(SetMode.Insert, wid)
+            self.conn.conn.core.ChangeSaveSet(SetMode.Insert, wid)
             self.conn.conn.core.ReparentWindow(wid, parent.wid, 0, 0)
             self.conn.conn.flush()
 