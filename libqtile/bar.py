312f21d05463492405abe114e942fec28f82a089
elParaguayo <elparaguayocode@gmail.com>
2022-02-16 14:35:14 +0000

    Fix reconfigure screens bug
    When reconfiguring screens, we checked to see if the screen size or
    position has changed. If they had, we reset the `_configured` flag
    on the bar to reconfigure it again. This had an unintended side
    effect where we had configured widgets in an "unconfigured" bar.
    In that scenario, the bar replaces the widgets with `Mirror`
    instances which causes a number of issues (including `Systray`
    raising a `ConfigError`).
    
    This PR fixes the issue by leaving the `_configured` flag unchanged
    but passes a `reconfigure` argument to the bar to force it to
    reconfigure its size and position when required.

--- libqtile/bar.py
+++ libqtile/bar.py
@@ -64,7 +64,7 @@ class Gap(CommandObject):
         self.height = None
         self.horizontal = None
 
-    def _configure(self, qtile, screen):
+    def _configure(self, qtile, screen, **kwargs):
         self.qtile = qtile
         self.screen = screen
         self.size = self.initial_size
@@ -194,9 +194,14 @@ class Bar(Gap, configurable.Configurable):
         self.future = None
         self._borders_drawn = False
 
-    def _configure(self, qtile, screen):
-        # We only want to adjust margin sizes once unless there's a new strut
-        if not self._configured or self._add_strut:
+    def _configure(self, qtile, screen, reconfigure=False):
+        """
+        Configure the bar. `reconfigure` is set to True when screen dimensions
+        change, forcing a recalculation of the bar's dimensions.
+        """
+        # We only want to adjust margin sizes once unless there's a new strut or we're
+        # reconfiguring the bar because the screen has changed
+        if not self._configured or self._add_strut or reconfigure:
             Gap._configure(self, qtile, screen)
             self._borders_drawn = False
 
