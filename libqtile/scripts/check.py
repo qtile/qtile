416e5863d55001072a5d83b5d2148c344e695b11
MithicSpirit <rpc01234@gmail.com>
2022-02-20 17:14:20 -0500

    Fix double space

--- libqtile/scripts/check.py
+++ libqtile/scripts/check.py
@@ -95,7 +95,7 @@ def type_check_config_args(config_file):
     if shutil.which("mypy") is None:
         print("mypy not found, can't type check config file" "install it and try again")
         return
-    if  sys.version_info.minor < 8:  # < 3.8
+    if sys.version_info.minor < 8:  # < 3.8
         print(
             "mypy check not supported for the current version of python, "
             + "please update python to at least 3.8 and try again"
