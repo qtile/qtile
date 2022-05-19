ae6f25b5050dab2f94621a16a052ee7421cfb790
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-05-19 06:54:56 +0530

    Changed to using psutil to get the load avgs.

--- libqtile/widget/load.py
+++ libqtile/widget/load.py
@@ -16,18 +16,21 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-import subprocess
 from itertools import cycle
+from psutil import getloadavg
 
 from libqtile.widget import base
 
 
 class Load(base.ThreadPoolText):
-    """A small widget to show the load averages of the system."""
+    """
+    A small widget to show the load averages of the system.
+    Depends on psutil.
+    """
 
     defaults = [
         ("update_interval", 1.0, "The update interval for the widget"),
-        ("format", "Load({time}):{load}", "The format in which to display the results."),
+        ("format", "Load({time}):{load:.2f}", "The format in which to display the results."),
     ]
     times = ["1m", "5m", "15m"]
 
@@ -43,9 +46,10 @@ class Load(base.ThreadPoolText):
 
     def poll(self):
         loads = {}
-        uptime = subprocess.check_output("uptime").decode("utf-8").strip()
-        loads["1m"], loads["5m"], loads["15m"] = eval(
-            uptime[uptime.index("load") + 13 :]
-        )  # Gets the load averages as a dictionary.
+        (
+            loads["1m"],
+            loads["5m"],
+            loads["15m"],
+        ) = getloadavg()  # Gets the load averages as a dictionary.
         load = loads[self.time]
         return self.format.format(time=self.time, load=load)
