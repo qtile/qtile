0ed64ee3c078e16280e9d724c76fdeaf314039f5
SweepingBishops <roshan.thakidiyel@gmail.com>
2022-05-30 22:19:22 +0530

    Add Load widget for displaying CPU load averages

---
+++ test/widgets/docs_screenshots/ss_load.py
@@ -0,0 +1,41 @@
+# Copyright (c) 2022 elParaguayo
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+import sys
+from importlib import reload
+
+import pytest
+
+from test.widgets.test_load import MockPsutil
+
+
+@pytest.fixture
+def widget(monkeypatch):
+    monkeypatch.setitem(sys.modules, "psutil", MockPsutil("psutil"))
+    from libqtile.widget import load
+
+    reload(load)
+    yield load.Load
+
+
+@pytest.mark.parametrize(
+    "screenshot_manager", [{}, {"format": "{time}: {load:.1f}"}], indirect=True
+)
+def ss_load(screenshot_manager):
+    screenshot_manager.take_screenshot()
