2407cf57193aceb4a7dc644042434e82df2b50a8
mcol <mcol@posteo.net>
2023-01-09 01:19:32 -0600

    WL: drop intermediate Drawer ImageSurface

--- libqtile/backend/wayland/drawer.py
+++ libqtile/backend/wayland/drawer.py
@@ -19,19 +19,11 @@ class Drawer(base.Drawer):
     A helper class for drawing and text layout.
 
     1. We stage drawing operations locally in memory using a cairo RecordingSurface.
-    2. Then apply these operations to our ImageSurface self._source.
-    3. Then copy the pixels onto the window's wlr_texture.
-    TODO: update this docstring with new logic
+    2. Then apply these operations to the windows's underlying ImageSurface.
     """
 
     def __init__(self, qtile: Qtile, win: Internal, width: int, height: int):
         base.Drawer.__init__(self, qtile, win, width, height)
-
-        self._source = cairocffi.ImageSurface(cairocffi.FORMAT_ARGB32, width, height)
-        with cairocffi.Context(self._source) as context:
-            # Initialise surface to empty
-            context.set_source_rgba(0, 0, 0, 0)
-            context.paint()
 
     def _draw(
         self,
@@ -63,29 +55,18 @@ class Drawer(base.Drawer):
         if height > self._win.height - offsety:
             height = self._win.height - offsety
 
-        # Paint RecordingSurface operations to ImageSurface
-        with cairocffi.Context(self._source) as context:
-            context.set_source_surface(self.surface)
-            context.paint()
-
-        # TODO: is this intermediate surface necessary?
-        # Paint drawn ImageSurface to our window's ImageSurface
+        # Paint recorded operations to our window's underlying ImageSurface
         with cairocffi.Context(self._win.surface) as context:
             context.set_operator(cairocffi.OPERATOR_SOURCE)
-            context.set_source_surface(self._source, offsetx, offsety)
+            context.set_source_surface(self.surface, offsetx, offsety)
             context.rectangle(offsetx, offsety, width, height)
             context.fill()
 
         damage = PixmanRegion32()
         damage.init_rect(offsetx, offsety, width, height)  # type: ignore
+        # TODO: do we really need to `set_buffer` here? would be good to just set damage
         self._win._scene_buffer.set_buffer_with_damage(self._win.wlr_buffer, damage)
         damage.fini()
-
-        # Clear intermediate surface
-        with cairocffi.Context(self._source) as context:
-            context.set_source_rgba(0, 0, 0, 0)
-            context.set_operator(cairocffi.OPERATOR_SOURCE)
-            context.paint()
 
     def clear(self, colour: ColorsType) -> None:
         # Draw background straight to ImageSurface
