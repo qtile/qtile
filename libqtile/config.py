635119e7b22b3ce502addd433917624bb4ceb132
Ameer Taweel <ameertaweel2002@gmail.com>
2023-02-07 16:49:16 +0300

    Add EzKeyChord: Def KeyChords in Emacs-like format

--- libqtile/config.py
+++ libqtile/config.py
@@ -283,6 +283,42 @@ class EzKey(EzConfig, Key):
         super().__init__(modkeys, key, *commands, desc=desc)
 
 
+class EzKeyChord(EzConfig, KeyChord):
+    """
+    Define a key chord using the Emacs-like format.
+
+    Parameters
+    ==========
+    keydef:
+        The Emacs-like key specification, e.g. ``"M-S-a"``.
+    submappings:
+        A list of :class:`Key` or :class:`KeyChord` declarations to bind in this chord.
+    mode:
+        Boolean. Setting to ``True`` will result in the chord persisting until
+        Escape is pressed. Setting to ``False`` (default) will exit the chord once
+        the sequence has ended.
+    name:
+        A string to name the chord. The name will be displayed in the Chord
+        widget.
+    desc:
+        A string to describe the chord. This attribute is not directly used by Qtile
+        but users may want to access this when creating scripts to show configured
+        keybindings.
+
+    """
+
+    def __init__(
+        self,
+        keydef: str,
+        submappings: list[Key | KeyChord],
+        mode: bool | str = False,
+        name: str = "",
+        desc: str = "",
+    ):
+        modkeys, key = self.parse(keydef)
+        super().__init__(modkeys, key, submappings, mode, name, desc)
+
+
 class EzClick(EzConfig, Click):
     """
     Bind commands to a clicking action using the Emacs-like format.
