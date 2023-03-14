22e208746e419ee696c1b90e2df1007a82921d09
mrtnvgr <martynovegorOF@yandex.ru>
2023-03-14 17:53:36 +0700

    add ability to set custom "Undefined" status key value to `Mpd2Widget`

--- libqtile/widget/mpd2widget.py
+++ libqtile/widget/mpd2widget.py
@@ -69,6 +69,8 @@ default_format = (
     "{play_status} {artist}/{title} " + "[{repeat}{random}{single}{consume}{updating_db}]"
 )
 
+default_undefined_status_value = "Undefined"
+
 
 def default_cmd():
     return None
@@ -116,6 +118,11 @@ class Mpd2(base.ThreadPoolText):
         (i.e. no song in queue)
 
         Default:: "MPD IDLE"
+
+    undefined_value:
+        text to display when status key is undefined
+
+        Default:: "Undefined"
 
     prepare_status:
         dict of functions to replace values in status with custom characters.
@@ -165,6 +172,11 @@ class Mpd2(base.ThreadPoolText):
         ("status_format", default_format, "format for displayed song info."),
         ("idle_format", default_idle_format, "format for status when mpd has no playlist."),
         ("idle_message", default_idle_message, "text to display when mpd is idle."),
+        (
+            "undefined_value",
+            default_undefined_status_value,
+            "text to display when status key is undefined.",
+        ),
         ("timeout", 30, "MPDClient timeout"),
         ("idletimeout", 5, "MPDClient idle command timeout"),
         ("no_connection", "No connection", "Text when mpd is disconnected"),
@@ -256,8 +268,7 @@ class Mpd2(base.ThreadPoolText):
 
     def formatter(self, status, current_song):
         """format song info."""
-        default = "Undefined"
-        song_info = defaultdict(lambda: default)
+        song_info = defaultdict(lambda: self.undefined_value)
         song_info["play_status"] = self.play_states[status["state"]]
 
         if status["state"] == "stop" and current_song == {}:
@@ -272,7 +283,7 @@ class Mpd2(base.ThreadPoolText):
         del song_info["time"]
 
         song_info.update(status)
-        if song_info["updating_db"] == default:
+        if song_info["updating_db"] == self.undefined_value:
             song_info["updating_db"] = "0"
         if not callable(self.prepare_status["repeat"]):
             for k in self.prepare_status:
@@ -290,11 +301,19 @@ class Mpd2(base.ThreadPoolText):
         # Remaining should default to '00:00' if either or both are missing.
         # These values are also used for coloring text by progress, if wanted.
         if "remaining" in self.status_format or self.color_progress:
-            total = float(song_info["fulltime"]) if song_info["fulltime"] != default else 0.0
-            elapsed = float(song_info["elapsed"]) if song_info["elapsed"] != default else 0.0
+            total = (
+                float(song_info["fulltime"])
+                if song_info["fulltime"] != self.undefined_value
+                else 0.0
+            )
+            elapsed = (
+                float(song_info["elapsed"])
+                if song_info["elapsed"] != self.undefined_value
+                else 0.0
+            )
             song_info["remaining"] = "{:.2f}".format(float(total - elapsed))
 
-        if "song" in self.status_format and song_info["song"] != default:
+        if "song" in self.status_format and song_info["song"] != self.undefined_value:
             song_info["currentsong"] = str(int(song_info["song"]) + 1)
 
         # mpd serializes tags containing commas as lists.
