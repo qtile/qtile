c25b6bf5f207e673616284ece36e390389baefba
elParaguayo <elparaguayocode@gmail.com>
2023-06-25 17:42:56 +0100

    [docs] Document qtile migrate
    This commit adds documentation to support the `qtile migrate` script.
    
    The documentation covers:
    - How to use the script
    - How to write migrations for future breaking changes

--- docs/qtile_docs/migrations.py
+++ docs/qtile_docs/migrations.py
@@ -1,5 +1,4 @@
-# Copyright (c) 2015 dmpayton
-# Copyright (c) 2021 elParaguayo
+# Copyright (c) 2023 elParaguayo
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -18,22 +17,34 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
-from qtile_docs.templates.command import qtile_commands_template  # noqa: F401
-from qtile_docs.templates.graph import qtile_graph_template  # noqa: F401
-from qtile_docs.templates.hook import qtile_hooks_template  # noqa: F401
-from qtile_docs.templates.migrations import (  # noqa: F401
-    qtile_migrations_full_template,
-    qtile_migrations_template,
-)
-from qtile_docs.templates.module import qtile_module_template  # noqa: F401
-from qtile_docs.templates.qtile_class import qtile_class_template  # noqa: F401
+from docutils.parsers.rst import Directive, directives
+from qtile_docs.base import SimpleDirectiveMixin
+from qtile_docs.templates import qtile_migrations_full_template, qtile_migrations_template
 
-__all__ = [
-    "qtile_commands_template",
-    "qtile_graph_template",
-    "qtile_hooks_template",
-    "qtile_module_template",
-    "qtile_class_template",
-    "qtile_migrations_template",
-    "qtile_migrations_full_template",
-]
+from libqtile.scripts.migrations import MIGRATIONS, load_migrations
+
+
+class QtileMigrations(SimpleDirectiveMixin, Directive):
+    """
+    A custom directive that is used to display details about the
+    migrations available to `qtile migrate`.
+    """
+
+    required_arguments = 0
+    option_spec = {
+        "summary": directives.flag,
+        "help": directives.flag,
+    }
+
+    def make_rst(self):
+        load_migrations()
+
+        context = {"migrations": [(m, len(m.ID)) for m in MIGRATIONS]}
+
+        if "summary" in self.options:
+            rst = qtile_migrations_template.render(**context)
+        elif "help" in self.options:
+            rst = qtile_migrations_full_template.render(**context)
+
+        for line in rst.splitlines():
+            yield line
