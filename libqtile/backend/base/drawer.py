qtile/qtile

commit c840df2775a6209345b0350e04c0161723bc36c4

Fix empty space bug in bar draw
Where widgets do not completely fill the bar (which happens if there is
no `SPACER`-type widget), the bar should be filled with the background
colour. However, this is not currently happening as the bar is only
cleared/filled when drawing borders. This is particularly noticeable on
Wayland.

This PR addresses this by ensuring the bar background is always painted
whenever `Bar.draw()` is called.

libqtile/bar.py
@@ -614,6 +614,10 @@ class Bar(Gap, configurable.Configurable, CommandObject):
         self._draw_queued = False
         self._resize(self._length, self.widgets)
 
+        # Fill the drawer with the background colour which will be shown if
+        # widgets don't completely fill the bar.
+        self.drawer.clear(self.background)
+
         # We draw the border before the widgets
         if any(self.border_width):
             # The border is drawn "outside" of the bar (i.e. not in the space that the
@@ -640,8 +644,6 @@ class Bar(Gap, configurable.Configurable, CommandObject):
                 ),
             ]
 
-            self.drawer.clear(self.background)
-
             for border_width, colour, opts in zip(
                 self.border_width, self.border_color, line_opts
             ):
@@ -657,7 +659,7 @@ class Bar(Gap, configurable.Configurable, CommandObject):
                 self.drawer.ctx.line_to(*line_to)
                 self.drawer.ctx.stroke()
 
-            self.drawer.draw(0, 0)
+        self.drawer.draw()
 
         for i in self.widgets:
             i.draw()

commit 7d6e3047742045430a56c14f12f0a1021a5ddb83

Fix number of bugs in the bar
c840df2 introduced a change to Bar.draw to ensure that a bar was
completely filled even when there were insufficient widgets to fill the
bar.

However, this introduced a bug:

1) When the bar is drawn, it clears the entire bar and then redraws
   every widget. However, the clearing is unnecessary as widgets also
   clear the surface before they are drawn. The bar's clearing therefore
   results in flickering of the bar as there may be a gap between
   clearing the bar and the widgets drawing themselves, particularly
   when there are a large number of widgets in the bar.

That bug revealed another:

2) Drawing the borders also cleared the entire bar so flickering would
   also occur when a user used borders in their config.

Which, in turn, revealed another:

3) Bar borders were not being rendered correctly for vertical bars
   (surprised this wasn't flagged earlier...)

This PR addresses all of these issues by:
- Removing the need to clear the bar's surface when it's drawn. Borders
  will now only clear the part of the bar covered by those borders.
- Similarly, filling any bar space no occupied by widgets will only draw
  in that space.

libqtile/backend/base/drawer.py
@@ -185,6 +185,8 @@ class Drawer:
         offsety: int = 0,
         width: int | None = None,
         height: int | None = None,
+        src_x: int = 0,
+        src_y: int = 0,
     ):
         """
         A wrapper for the draw operation.
@@ -205,9 +207,20 @@ class Drawer:
             the X portion of the canvas to draw at the starting point.
         height :
             the Y portion of the canvas to draw at the starting point.
+        src_x  :
+            the X position of the origin in the source surface
+        src_y  :
+            the Y position of the origin in the source surface
         """
         if self._enabled:
-            self._draw(offsetx, offsety, width, height)
+            self._draw(
+                offsetx=offsetx,
+                offsety=offsety,
+                width=width,
+                height=height,
+                src_x=src_x,
+                src_y=src_y,
+            )
             if self.has_mirrors:
                 self._create_last_surface()
                 ctx = cairocffi.Context(self.last_surface)
@@ -222,6 +235,8 @@ class Drawer:
         offsety: int = 0,
         width: int | None = None,
         height: int | None = None,
+        src_x: int = 0,
+        src_y: int = 0,
     ):
         """
         This draws our cached operations to the Internal window.
@@ -237,6 +252,10 @@ class Drawer:
             the X portion of the canvas to draw at the starting point.
         height :
             the Y portion of the canvas to draw at the starting point.
+        src_x  :
+            the X position of the origin in the source surface
+        src_y  :
+            the Y position of the origin in the source surface
         """
 
     def new_ctx(self):

libqtile/backend/wayland/drawer.py
@@ -49,6 +49,8 @@ class Drawer(drawer.Drawer):
         offsety: int = 0,
         width: int | None = None,
         height: int | None = None,
+        src_x: int = 0,
+        src_y: int = 0,
     ) -> None:
         if offsetx > self._win.width:
             return
@@ -76,7 +78,9 @@ class Drawer(drawer.Drawer):
         # Paint recorded operations to our window's underlying ImageSurface
         with cairocffi.Context(self._win.surface) as context:
             context.set_operator(cairocffi.OPERATOR_SOURCE)
-            context.set_source_surface(self.surface, offsetx, offsety)
+            # Adjust the source surface position by src_x and src_y e.g. if we want
+            # to render part of the surface in a different position
+            context.set_source_surface(self.surface, offsetx - src_x, offsety - src_y)
             context.rectangle(offsetx, offsety, width, height)
             context.fill()
 

libqtile/backend/x11/drawer.py
@@ -175,6 +175,8 @@ class Drawer(drawer.Drawer):
         offsety: int = 0,
         width: int | None = None,
         height: int | None = None,
+        src_x: int = 0,
+        src_y: int = 0,
     ):
         self.current_rect = (offsetx, offsety, width, height)
 
@@ -193,8 +195,8 @@ class Drawer(drawer.Drawer):
             self._pixmap,
             self._win.wid,
             self._gc,
-            0,
-            0,  # srcx, srcy
+            src_x,
+            src_y,  # srcx, srcy
             offsetx,
             offsety,  # dstx, dsty
             self.width if width is None else width,

libqtile/bar.py
@@ -264,9 +264,9 @@ class Bar(Gap, configurable.Configurable, CommandObject):
                     self._length = self.height
                     self._size += margin[1] + margin[3]
                     if screen.left is self:
-                        self.x += margin[3]
+                        self.x += margin[3] - self.border_width[3]
                     else:
-                        self.x -= margin[1]
+                        self.x -= margin[1] + self.border_width[1]
 
             self._reserved_space_updated = False
 
@@ -613,11 +613,6 @@ class Bar(Gap, configurable.Configurable, CommandObject):
     def _actual_draw(self) -> None:
         self._draw_queued = False
         self._resize(self._length, self.widgets)
-
-        # Fill the drawer with the background colour which will be shown if
-        # widgets don't completely fill the bar.
-        self.drawer.clear(self.background)
-
         # We draw the border before the widgets
         if any(self.border_width):
             # The border is drawn "outside" of the bar (i.e. not in the space that the
@@ -628,41 +623,77 @@ class Bar(Gap, configurable.Configurable, CommandObject):
             # line_opts is a list of tuples where each tuple represents the borders
             # in the order N, E, S, W. The border tuple contains two pairs of
             # co-ordinates for the start and end of the border.
-            line_opts = [
-                ((0, self.border_width[0] * 0.5), (width, self.border_width[0] * 0.5)),
+            rects = [
+                (0, 0, width, self.border_width[0]),
                 (
-                    (width - (self.border_width[1] * 0.5), self.border_width[0]),
-                    (width - (self.border_width[1] * 0.5), height - self.border_width[2]),
+                    width - (self.border_width[1]),
+                    self.border_width[0],
+                    self.border_width[1],
+                    height - self.border_width[0] - self.border_width[2],
                 ),
+                (0, height - self.border_width[2], width, self.border_width[2]),
                 (
-                    (0, height - self.border_width[2] + (self.border_width[2] * 0.5)),
-                    (width, height - self.border_width[2] + (self.border_width[2] * 0.5)),
-                ),
-                (
-                    (self.border_width[3] * 0.5, self.border_width[0]),
-                    (self.border_width[3] * 0.5, height - self.border_width[2]),
+                    0,
+                    self.border_width[0],
+                    self.border_width[3],
+                    height - self.border_width[0] - self.border_width[2],
                 ),
             ]
 
-            for border_width, colour, opts in zip(
-                self.border_width, self.border_color, line_opts
-            ):
+            for border_width, colour, rect in zip(self.border_width, self.border_color, rects):
                 if not border_width:
                     continue
 
-                move_to, line_to = opts
-
                 # Draw the border
+                self.drawer.clear_rect(*rect)
+                self.drawer.ctx.rectangle(*rect)
                 self.drawer.set_source_rgb(colour)  # type: ignore[arg-type]
-                self.drawer.ctx.set_line_width(border_width)
-                self.drawer.ctx.move_to(*move_to)
-                self.drawer.ctx.line_to(*line_to)
-                self.drawer.ctx.stroke()
+                self.drawer.ctx.fill()
+                src_x, src_y, width, height = rect
 
-        self.drawer.draw()
+                self.drawer.draw(
+                    offsetx=src_x,
+                    offsety=src_y,
+                    width=width,
+                    height=height,
+                    src_x=src_x,
+                    src_y=src_y,
+                )
 
         for i in self.widgets:
             i.draw()
+
+        # We need to check if there is any unoccupied space in the bar
+        # This can happen where there are no SPACER-type widgets to fill
+        # empty space.
+        # In that scenario, we fill the empty space with the bar background colour
+        # We do this, instead of just filling the bar completely at the start of this
+        # method to avoid flickering.
+
+        # Widgets are offset by the top/left border but this is not included in self._length
+        # so we adjust the end of the bar area for this offset
+        if self.horizontal:
+            bar_end = self._length + self.border_width[3]
+        else:
+            bar_end = self._length + self.border_width[0]
+
+        widget_end = i.offset + i.length
+
+        if widget_end < bar_end:
+            # Defines a rectangle for the area enclosed by the bar's borders and the end of the
+            # last widget.
+            if self.horizontal:
+                rect = (widget_end, self.border_width[0], bar_end - widget_end, self.height)
+            else:
+                rect = (self.border_width[3], widget_end, self.width, bar_end - widget_end)
+
+            # Clear that area (i.e. don't clear borders) and fill with background colour
+            self.drawer.clear_rect(*rect)
+            self.drawer.ctx.rectangle(*rect)
+            self.drawer.set_source_rgb(self.background)
+            self.drawer.ctx.fill()
+            x, y, w, h = rect
+            self.drawer.draw(offsetx=x, offsety=y, height=h, width=w, src_x=x, src_y=y)
 
     @expose_command()
     def info(self) -> dict[str, Any]: