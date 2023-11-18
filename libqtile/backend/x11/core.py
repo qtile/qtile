bfd93e22d04ba0fe5755ff0dfaafeb0c202cf345
elParaguayo <elparaguayocode@gmail.com>
2023-11-18 18:38:53 +0000

    [x11] Better stacking unmanaged windows
    Where qtile has unmanaged windows (e.g. override redirect) then these
    windows were ignored for stacking purposes which meant they could appear
    in the wrong place.
    
    This PR adds the ability for qtile to stack these windows correctly
    while still not fully managing the windows.
    
    Fixes #4466

--- libqtile/backend/x11/core.py
+++ libqtile/backend/x11/core.py
@@ -47,7 +47,7 @@ if TYPE_CHECKING:
     from libqtile.core.manager import Qtile
 
 _IGNORED_EVENTS = {
-    xcffib.xproto.CreateNotifyEvent,
+    # xcffib.xproto.CreateNotifyEvent,
     xcffib.xproto.FocusInEvent,
     xcffib.xproto.KeyReleaseEvent,
     # DWM handles this to help "broken focusing windows".
@@ -170,6 +170,8 @@ class Core(base.Core):
         self._last_motion_time = 0
 
         self.last_focused: base.Window | None = None
+
+        self.override_redirect_map: dict[int, window._Window] = {}
 
     @property
     def name(self):
@@ -324,7 +326,6 @@ class Core(base.Core):
                 event = self.conn.conn.poll_for_event()
                 if not event:
                     break
-
                 if event.__class__ in _IGNORED_EVENTS:
                     continue
 
@@ -738,6 +739,9 @@ class Core(base.Core):
     def handle_MapRequest(self, event) -> None:  # noqa: N802
         assert self.qtile is not None
 
+        if event.window in self.override_redirect_map:
+            del self.override_redirect_map[event.window]
+
         xwin = window.XWindow(self.conn, event.window)
         try:
             attrs = xwin.get_attributes()
@@ -773,8 +777,16 @@ class Core(base.Core):
             self.update_client_lists()
             win.change_layer()
 
+    def handle_CreateNotify(self, event) -> None:  # noqa: N802
+        xwin = window.XWindow(self.conn, event.window)
+        win = window._Window(xwin, self.qtile)
+        self.override_redirect_map[event.window] = win
+
     def handle_DestroyNotify(self, event) -> None:  # noqa: N802
         assert self.qtile is not None
+
+        if event.window in self.override_redirect_map:
+            del self.override_redirect_map[event.window]
 
         self.qtile.unmanage(event.window)
         self.update_client_lists()
