b52eb5bb53a2c3d4d7863a8985e6716543c39e9a
Joe Rabinoff <rabinoff@post.harvard.edu>
2023-12-14 13:55:06 -0500

    Load xcursor scale factors.

--- libqtile/backend/wayland/core.py
+++ libqtile/backend/wayland/core.py
@@ -956,6 +956,8 @@ class Core(base.Core, wlrq.HasListeners):
                 self.output_layout.add(wlr_output, state.x, state.y)
                 wlr_output.set_transform(state.transform)
                 wlr_output.set_scale(state.scale)
+                # Ensure we have cursors loaded for the new scale factor.
+                self.cursor_manager.load(state.scale)
             else:
                 if wlr_output.enabled:
                     wlr_output.enable(enable=False)
