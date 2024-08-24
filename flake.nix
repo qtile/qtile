c8bd0f5b23bbefa170468595b6361fac3a791eac
Gurjaka <esartia.gurika@gmail.com>
2024-08-25 02:41:32 +0400

    Flake Cleanup/Sigma

--- flake.nix
+++ flake.nix
@@ -7,26 +7,23 @@
 
   outputs = { self, nixpkgs }: let
     supportedSystems = [
-      "aarch64-darwin"
-      "x86_64-darwin"
+      "x86_64-linux"
+      "aarch64_linux"
     ];
 
     forAllSystems = function:
       nixpkgs.lib.genAttrs supportedSystems
-      (system: function nixpkgs.legacyPackages.${system});
-
+        (system: function nixpkgs.legacyPackages.${system});
   in {
-    formatter = forAllSystems (pkgs: pkgs.alejandra);
-
-    overlays = import ./nix/overlays.nix self;
+    overlays.default = import ./nix/overlays.nix self;
 
     packages = forAllSystems (pkgs: let
       pypkgs = pkgs.python3Packages;
       nixpkgs-qtile = pypkgs.qtile;
-
     in {
       default = self.packages.${pkgs.system}.qtile;
-      qtile = nixpkgs-qtile.overrideAttrs (_: {
+
+      qtile = nixpkgs-qtile.overrideAttrs (prev: {
         name = "${nixpkgs-qtile.pname}-${nixpkgs-qtile.version}";
         passthru.unwrapped = nixpkgs-qtile;
       });
@@ -42,19 +39,31 @@
           QTILE_INCLUDE_LIBPIXMAN = "${pkgs.pixman.outPath}/include";
           QTILE_INCLUDE_LIBDRM = "${pkgs.libdrm.dev.outPath}/include/libdrm";
         };
-        
+
         shellHook = ''
           export PYTHONPATH=$(readlink -f .):$PYTHONPATH
         '';
       };
 
-      common-python-deps = ps: with ps; [
-        python-dateutil
-      ] ++ [
-        # tests
-        coverage
-        pytest
-      ];
+      common-python-deps = ps: with ps;
+        [ python-dateutil ]
+        ++ [
+          # tests
+          coverage
+          pytest
+        ];
+
+      tests = {
+        wayland = pkgs.writeScriptBin "qtile-run-tests-wayland" ''
+          ./scripts/ffibuild -v
+          pytest -x --backend=wayland
+        '';
+
+        x11 = pkgs.writeScriptBin "qtile-run-tests-x11" ''
+          ./scripts/ffibuild -v
+          pytest -x --backend=x11
+        '';
+      };
 
       common-system-deps = with pkgs; [
         # Gdk namespaces
@@ -68,25 +77,13 @@
         wlroots_0_17
         # test/backend/wayland/test_window.py
         gtk-layer-shell
-      ] ++ [
-        # some targets scripts run
-        (pkgs.writeScriptBin "qtile-run-tests-wayland" ''
-          ./scripts/ffibuild -v
-          pytest -x --backend=wayland
-        '')
-
-        (pkgs.writeScriptBin "qtile-run-tests-x11" ''
-          ./scripts/ffibuild -v
-          pytest -x --backend=x11
-        '')
-      ];
-
+      ] ++ (builtins.attrValues tests);
     in {
       default = pkgs.mkShell {
         inherit (common-shell) env shellHook;
 
         packages = with pkgs; [
-          (python3.withPackagescommon-python-deps)
+          (python3.withPackages common-python-deps)
           pre-commit
         ] ++ common-system-deps;
       };
