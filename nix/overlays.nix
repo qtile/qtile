e195f1da06dd2c61fa1aa57a0dda283415bd4568
Gurjaka <esartia.gurika@gmail.com>
2024-08-25 02:44:58 +0400

    Flake Cleanup/Sigma + Overlays fix

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,18 +1,18 @@
-self: final: super: {
+self: final: prev: {
   pythonPackagesOverlays =
-    (super.pythonPackagesOverlays or [])
+    (prev.pythonPackagesOverlays or [])
     ++ [
       (_: pprev: {
-        pywlroots = (pprev.pywlroots.overrideAttrs(_: rec {
+        pywlroots = (pprev.pywlroots.overrideAttrs(_: {
           version = "0.17.0";
-          src = super.fetchFromGitHub {
+          src = prev.fetchFromGitHub {
             owner = "flacjacket";
             repo = "pywlroots";
             rev = "bf314bae5d7a9552225c44ab1e7bf7e4fafa869e";
             hash = "sha256-tvem8gmih9EfseZ3MgU1UW05xfttzKLcJEaymM5uNPI=";
           };
         })).override {
-          wlroots = super.wlroots_0_17;
+          wlroots = prev.wlroots_0_17;
         };
         qtile = (pprev.qtile.overrideAttrs (old: let
           flakever = self.shortRev or "dev";
@@ -23,14 +23,14 @@ self: final: super: {
           # for qtile migrate, not in nixpkgs yet
           propagatedBuildInputs = old.propagatedBuildInputs ++ [ pprev.libcst ];
         })).override {
-          wlroots = super.wlroots_0_17;
+          wlroots = prev.wlroots_0_17;
         };
       })
     ];
   python3 = let
-    self = super.python3.override {
+    self = prev.python3.override {
       inherit self;
-      packageOverrides = super.lib.composeManyExtensions final.pythonPackagesOverlays;
+      packageOverrides = prev.lib.composeManyExtensions final.pythonPackagesOverlays;
     };
   in
     self;
