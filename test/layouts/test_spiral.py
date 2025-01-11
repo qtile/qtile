1670aa8100889ab126e2994168769d1a5be136e7
rauaap <aapr@protonmail.com>
2025-01-11 18:09:46 +0200

    Spiral layout: shuffle methods call client lists respective shuffle methods instead of rotate methods

--- test/layouts/test_spiral.py
+++ test/layouts/test_spiral.py
@@ -202,6 +202,56 @@ def test_spiral_bottom_anticlockwise(manager):
     assert_dimensions(manager, 200, 150, 198, 148)
 
 
+@spiral_config
+def test_spiral_shuffle_up(manager):
+    manager.test_window("one")
+    manager.test_window("two")
+    manager.test_window("three")
+    manager.test_window("four")
+    assert manager.c.layout.info()["clients"] == ["one", "two", "three", "four"]
+    manager.c.layout.shuffle_up()
+    assert manager.c.layout.info()["clients"] == ["one", "two", "four", "three"]
+    manager.c.layout.shuffle_up()
+    assert manager.c.layout.info()["clients"] == ["one", "four", "two", "three"]
+
+
+@spiral_config
+def test_spiral_shuffle_down(manager):
+    manager.test_window("one")
+    manager.test_window("two")
+    manager.test_window("three")
+    manager.test_window("four")
+    # wrap focus back to first window
+    manager.c.layout.down()
+    assert manager.c.layout.info()["clients"] == ["one", "two", "three", "four"]
+    manager.c.layout.shuffle_down()
+    assert manager.c.layout.info()["clients"] == ["two", "one", "three", "four"]
+    manager.c.layout.shuffle_down()
+    assert manager.c.layout.info()["clients"] == ["two", "three", "one", "four"]
+
+
+@spiral_config
+def test_spiral_shuffle_no_wrap_down(manager):
+    # test that shuffling down doesn't wrap around
+    manager.test_window("one")
+    manager.test_window("two")
+    manager.test_window("three")
+    manager.c.layout.shuffle_down()
+    assert manager.c.layout.info()["clients"] == ["one", "two", "three"]
+
+
+@spiral_config
+def test_spiral_shuffle_no_wrap_up(manager):
+    # test that shuffling up doesn't wrap around
+    manager.test_window("one")
+    manager.test_window("two")
+    manager.test_window("three")
+    # wrap focus back to first window
+    manager.c.layout.down()
+    manager.c.layout.shuffle_up()
+    assert manager.c.layout.info()["clients"] == ["one", "two", "three"]
+
+
 @singleborder_disabled_config
 def test_singleborder_disable(manager):
     manager.test_window("one")
