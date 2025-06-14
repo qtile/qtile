2011a3c0a0d02754b53fd4649cf47c462a2ed47b
Jeroen Wijenbergh <qtile@jwijenbergh.com>
2025-06-14 18:21:06 +0200

    flake: sync with master

--- flake.nix
+++ flake.nix
@@ -5,58 +5,60 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-  }: let
-    supportedSystems = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
+  outputs =
+    {
+      self,
+      nixpkgs,
+    }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
 
-    forAllSystems = function:
-      nixpkgs.lib.genAttrs supportedSystems (
-        system: let
-          nixpkgs-settings = {
-            inherit system;
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          let
+            nixpkgs-settings = {
+              inherit system;
 
-            overlays = [(import ./nix/overlays.nix self)];
-          };
-        in
+              overlays = [ (import ./nix/overlays.nix self) ];
+            };
+          in
           function (import nixpkgs nixpkgs-settings)
-      );
-  in {
-    checks = forAllSystems (pkgs: pkgs.python3Packages.qtile.passthru.tests);
-
-    overlays.default = import ./nix/overlays.nix self;
-
-    packages = forAllSystems (
-      pkgs: let
-        qtile' = pkgs.python3Packages.qtile;
-      in {
-        default = self.packages.${pkgs.system}.qtile;
-
-        qtile = qtile'.overrideAttrs (
-          prev: let
-            remove-dbus-next = dep: dep.pname != pkgs.python3Packages.dbus-next.pname;
-
-            # seems like dependencies is a fancy wrapper on that one!
-            propagatedBuildInputs = with pkgs.python3Packages;
-              [dbus-fast] ++ (pkgs.lib.filter remove-dbus-next prev.propagatedBuildInputs);
-          in {
-            name = "${qtile'.pname}-${qtile'.version}";
-            inherit propagatedBuildInputs;
-            passthru.unwrapped = qtile';
-          }
         );
-      }
-    );
+    in
+    {
+      formatter = forAllSystems (pkgs: pkgs.nixfmt-rfc-style);
 
-    devShells = forAllSystems (
-      pkgs: let
-        common-python-deps = ps:
-          with ps;
-            [python-dateutil]
+      checks = forAllSystems (pkgs: pkgs.python3Packages.qtile.passthru.tests);
+
+      overlays.default = import ./nix/overlays.nix self;
+
+      packages = forAllSystems (
+        pkgs:
+        let
+          qtile' = pkgs.python3Packages.qtile;
+        in
+        {
+          default = self.packages.${pkgs.system}.qtile;
+
+          qtile = qtile'.overrideAttrs (prev: {
+            name = "${qtile'.pname}-${qtile'.version}";
+            passthru.unwrapped = qtile';
+          });
+        }
+      );
+
+      devShells = forAllSystems (
+        pkgs:
+        let
+          common-python-deps =
+            ps:
+            with ps;
+            [ python-dateutil ]
             ++ [
               # docs building
               numpydoc
@@ -68,80 +70,81 @@
               isort
             ];
 
-        tests = {
-          wayland = pkgs.writeScriptBin "qtile-run-tests-wayland" ''
-            ./scripts/ffibuild -v
-            pytest -x --backend=wayland
-          '';
+          tests = {
+            wayland = pkgs.writeScriptBin "qtile-run-tests-wayland" ''
+              ./scripts/ffibuild -v
+              pytest -x --backend=wayland
+            '';
 
-          x11 = pkgs.writeScriptBin "qtile-run-tests-x11" ''
-            ./scripts/ffibuild -v
-            pytest -x --backend=x11
-          '';
-        };
-
-        common-system-deps = with pkgs;
-          [
-            # Gdk namespaces
-            wrapGAppsHook
-            gobject-introspection
-
-            # docs graphs
-            graphviz
-
-            # x11 deps
-            xorg.xorgserver
-            xorg.libX11
-
-            # formatting python
-            ruff
-
-            # wayland backend
-            wayland-scanner
-            wayland-protocols
-            # generating compile_commands.json
-            bear
-            # clang-format for formatting
-            clang-tools
-
-            wlroots_0_19
-            # test/backend/wayland/test_window.py
-            gtk-layer-shell
-            imagemagick
-          ]
-          ++ (builtins.attrValues tests);
-      in {
-        default = pkgs.mkShell {
-          env = {
-            QTILE_CAIRO_PATH = "${pkgs.cairo.dev}/include/cairo";
-            QTILE_PIXMAN_PATH = "${pkgs.pixman}/include/pixman-1";
-            QTILE_LIBDRM_PATH = "${pkgs.libdrm.dev}/include/libdrm";
-            QTILE_WLROOTS_PATH = "${pkgs.wlroots_0_19}/include/wlroots-0.19";
-
-            LD_LIBRARY_PATH = with pkgs;
-              lib.makeLibraryPath [
-                glib
-                pango
-                xcb-util-cursor
-                pixman
-                libdrm.dev
-              ];
+            x11 = pkgs.writeScriptBin "qtile-run-tests-x11" ''
+              ./scripts/ffibuild -v
+              pytest -x --backend=x11
+            '';
           };
 
-          shellHook = ''
-            export PYTHONPATH=$(readlink -f .):$PYTHONPATH
-          '';
-
-          inputsFrom = [self.packages.${pkgs.system}.qtile];
-
-          packages = with pkgs;
+          common-system-deps =
+            with pkgs;
             [
-              (python3.withPackages common-python-deps)
-              pre-commit
+              # Gdk namespaces
+              wrapGAppsHook
+              gobject-introspection
+
+              # docs graphs
+              graphviz
+
+              # x11 deps
+              xorg.xorgserver
+              xorg.libX11
+
+              # wayland backend
+              wayland-scanner
+              wayland-protocols
+
+              # generating compile_commands.json
+              bear
+
+              # clang-format for formatting
+              clang-tools
+
+              wlroots_0_19
+              # test/backend/wayland/test_window.py
+              gtk-layer-shell
+              imagemagick
             ]
-            ++ common-system-deps;
-        };
-      }
-    );
-  };
+            ++ (builtins.attrValues tests);
+        in
+        {
+          default = pkgs.mkShell {
+            env = {
+              QTILE_PIXMAN_PATH = "${pkgs.pixman}/include/pixman-1";
+              QTILE_LIBDRM_PATH = "${pkgs.libdrm.dev}/include/libdrm";
+
+              LD_LIBRARY_PATH =
+                with pkgs;
+                lib.makeLibraryPath [
+                  glib
+                  pango
+                  xcb-util-cursor
+                  pixman
+                  libdrm.dev
+                ];
+            };
+
+            shellHook = ''
+              export PYTHONPATH=$(readlink -f .):$PYTHONPATH
+            '';
+
+            inputsFrom = [ self.packages.${pkgs.system}.qtile ];
+
+            packages =
+              with pkgs;
+              [
+                (python3.withPackages common-python-deps)
+                pre-commit
+              ]
+              ++ common-system-deps;
+          };
+        }
+      );
+    };
 }
