604e6975349ec4467242a464adbf9bd0d1bd3fd1
richcarni <richcarni@gmail.com>
2025-07-09 20:34:26 +1000

    Fix geometry for outputs during tests
    Call a qw_server function to set test_env flag
    
    Count as outputs are added and set custom mode:
    - Output 1: 800 x 600
    - Output 2: 640 x 480

--- libqtile/backend/wayland/qw/output.c
+++ libqtile/backend/wayland/qw/output.c
@@ -52,9 +52,18 @@ void qw_server_output_new(struct qw_server *server, struct wlr_output *wlr_outpu
     wlr_output_state_init(&state);
     wlr_output_state_set_enabled(&state, true);
 
-    struct wlr_output_mode *mode = wlr_output_preferred_mode(wlr_output);
-    if (mode) {
-        wlr_output_state_set_mode(&state, mode);
+    if (server->test_env) {
+        if (server->output_count == 0) {
+            wlr_output_state_set_custom_mode(&state, 800, 600, 0);
+        }
+        else {
+            wlr_output_state_set_custom_mode(&state, 640, 480, 0);
+        }
+    } else {
+        struct wlr_output_mode *mode = wlr_output_preferred_mode(wlr_output);
+        if (mode) {
+            wlr_output_state_set_mode(&state, mode);
+        }
     }
 
     wlr_output_commit_state(wlr_output, &state);
@@ -74,7 +83,11 @@ void qw_server_output_new(struct qw_server *server, struct wlr_output *wlr_outpu
     output->destroy.notify = qw_output_handle_destroy;
     wl_signal_add(&wlr_output->events.destroy, &output->destroy);
 
-    wl_list_insert(&server->outputs, &output->link);
+    // Insert output at end of list
+    wl_list_insert(server->outputs.prev, &output->link);
+    if (server->test_env) {
+        server->output_count++;
+    }
 
     // Add the output to the output layout automatically and to the scene layout
     struct wlr_output_layout_output *l_output =
