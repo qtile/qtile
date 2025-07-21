93d29711cba2ecefbacac5a44f5c32279e15201f
Tycho Andersen <tycho@tycho.pizza>
2025-07-20 20:50:02 -0600

    drop DefaultEventLoopPolicy
    python3.14 depreciates event loop policies entirely, in favor of just
    manually specifying the event loop as an argument to asyncio.run().
    
    we were really using the event loop policy as a hack for not having a
    global variable, so let's switch to a global variable instead, and use it
    in the one place we need it.
    
    we have to drop two verisons of python because they do not have the
    loop_factory= argument to asyncio:
    
        raise AssertionError(f"Error launching qtile, traceback:\n{error}")
        AssertionError: Error launching qtile, traceback:
        Traceback (most recent call last):
          File "/home/runner/work/qtile/qtile/test/helpers.py", line 207, in run_qtile
            ).loop()
          File "/home/runner/work/qtile/qtile/libqtile/core/manager.py", line 216, in loop
            asyncio.run(self.async_loop(), loop_factory=lambda: libqtile.event_loop)
        TypeError: run() got an unexpected keyword argument 'loop_factory'
    
    but maybe that's ok: 3.10 is outside of our last-three-versions policy, and
    3.11 will be outside it in a few months when 3.14 is released.
    
    There is another option here: since the motivation for this was really
    #2620, we could "just" switch the battery to running in-loop, which would
    mean that it is not run via run_in_executor() i.e. in a separate thread.
    That means that get_running_loop() would return the right loop, and we
    wouldn't need this hack at all.
    
    (It also means that the battery could potentially block the UI while
    reading from e.g. procfs/sysfs files, but that seems unlikely. Perhaps it
    also suggests another widget type: something with an `async def poll()`,
    which could run in the main loop and avoid both of these problems.)
    
    The only other widget which uses send_notification() is pomodoro, which
    doesn't read any files at all and just uses gettimeofday(), which is a VDSO
    i.e. memory access, that should also not block, so if we convert the
    battery widget, we should do this one as well.
    
    Finally, we suggest use of send_notification() in the hooks documentation,
    but all hooks are fired from the main thread, so the get_running_loop()
    call should work fine there.
    
    That does mean we'd have to be careful to always send_notification() from
    the main thread going forward, but it doesn't seem like that hard of a
    challenge.
    
    Fixes #5346
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- libqtile/core/manager.py
+++ libqtile/core/manager.py
@@ -47,7 +47,7 @@ from libqtile.command.interface import IPCCommandServer, QtileCommandInterface
 from libqtile.config import Click, Drag, Key, KeyChord, Match, Mouse, Rule, Screen, ScreenRect
 from libqtile.config import ScratchPad as ScratchPadConfig
 from libqtile.core.lifecycle import lifecycle
-from libqtile.core.loop import LoopContext, QtileEventLoopPolicy
+from libqtile.core.loop import LoopContext
 from libqtile.core.state import QtileState
 from libqtile.dgroups import DGroups
 from libqtile.extension.base import _Extension
@@ -113,6 +113,7 @@ class Qtile(CommandObject):
         self.screens: list[Screen] = []
 
         libqtile.init(self)
+        libqtile.event_loop = asyncio.new_event_loop()
 
         self._stopped_event: asyncio.Event | None = None
 
@@ -212,7 +213,7 @@ class Qtile(CommandObject):
         return socket_path
 
     def loop(self) -> None:
-        asyncio.run(self.async_loop())
+        asyncio.run(self.async_loop(), loop_factory=lambda: libqtile.event_loop)
 
     async def async_loop(self) -> None:
         """Run the event loop
@@ -220,8 +221,6 @@ class Qtile(CommandObject):
         Finalizes the Qtile instance on exit.
         """
         self._eventloop = asyncio.get_running_loop()
-        # Set the event loop policy to facilitate access to main event loop
-        asyncio.set_event_loop_policy(QtileEventLoopPolicy(self))
         self._stopped_event = asyncio.Event()
         self.core.qtile = self
         self.load_config(initial=True)
