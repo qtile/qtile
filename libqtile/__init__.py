f54375392a8fd92e5415c579f20ca9088eab6506
Tycho Andersen <tycho@tycho.pizza>
2025-08-07 06:47:38 -0600

    add build_config for specifying library paths
    The goal here is to give distros (Nix) a way to hard-code library paths
    that we use without having to patch the paths manually.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- libqtile/__init__.py
+++ libqtile/__init__.py
@@ -19,6 +19,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 import asyncio
+import ctypes.util
+import enum
 
 
 class _UndefinedCore:
@@ -36,3 +38,33 @@ event_loop: asyncio.AbstractEventLoop
 def init(q):
     global qtile
     qtile = q
+
+
+class DynamicLibraries(enum.Enum):
+    GOBJECT = "gobject-2.0"
+    PANGO = "pango-1.0"
+    PANGOCAIRO = "pangocairo-1.0"
+    XCBCURSOR = "xcb-cursor"
+
+
+def find_library(key: DynamicLibraries) -> str | None:
+    # Some distros want to hard-code libpaths, BSDs generally want to resolve
+    # things via ldconfig, and glibc has /etc/ld.so.cache. These last two are
+    # handled by ctypes.util.find_library(), but the first one wants to be
+    # specified at build time. So if it's been specified, let's use it.
+    # Otherwise, just use ctypes.
+    #
+    # Finally, this module may not always be present, since during development
+    # on a source tree, no install has actually taken place, so let's make its
+    # presence optional.
+    try:
+        from . import build_config  # type: ignore
+
+        attr = key.name + "_PATH"
+        value = getattr(build_config, attr, None)
+        if value is not None:
+            return value
+    except ImportError:
+        pass
+
+    return ctypes.util.find_library(key.value)
