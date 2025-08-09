bd12d77a9ffb42664f509ee8a5eb799eff2238fc
elParaguayo <elparaguayocode@gmail.com>
2025-08-09 09:01:56 +0100

    [wayc] build via pyproject.toml
    setup.py has been removed from main repo so, instead, we can incorporate
    the ffi build into a custom builder and put it behind a config setting.

--- builder.py
+++ builder.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023, elParaguayo. All rights reserved.
+# Copyright (c) 2025 elParaguayo
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -17,12 +17,14 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+import distutils.errors
+import subprocess
 import sys
 
 from setuptools import build_meta as _orig
 from setuptools.build_meta import *  # noqa: F401,F403
 
-WAYLAND_DEPENDENCIES = ["pywlroots>=0.17.0,<0.18.0"]
+WAYLAND_FFI_BUILD = "./libqtile/backend/wayland/cffi/build.py"
 
 
 def wants_wayland(config_settings):
@@ -34,32 +36,14 @@ def wants_wayland(config_settings):
     return False
 
 
-def get_requires_for_build_wheel(config_settings=None):
-    """Inject pywlroots into build dependencies if wayland requested."""
-    reqs = _orig.get_requires_for_build_wheel(config_settings)
-    if wants_wayland(config_settings):
-        reqs += WAYLAND_DEPENDENCIES
-
-    return reqs
-
-
 def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
-    """Stop building if wayland requested but pywlroots is not installed."""
-    if config_settings is None:
-        config_settings = {}
-
+    """If wayland backend is requested, build it!"""
     if wants_wayland(config_settings):
         try:
-            import wlroots  # noqa: F401
-        except ImportError:
-            sys.exit("Wayland backend requested but pywlroots is not installed.")
+            from libqtile.backend.wayland.cffi.build import ffi
 
-    # Write library paths to file, if they are specified at build time via
-    # --config-settings=PANGO_PATH=...
-    with open("libqtile/_build_config.py", "w") as f:
-        f.write("# This file is generated at build time by builder.py\n")
-        for lib in ["PANGO", "PANGOCAIRO", "GOBJECT", "XCBCURSOR"]:
-            p = lib + "_PATH"
-            f.write(f"{p} = {config_settings.get(p)!r}\n")
+            ffi.compile(verbose=True)
+        except (distutils.errors.DistutilsError, subprocess.CalledProcessError) as e:
+            sys.exit(f"Wayland backend requested but backend could not be built: {e}")
 
     return _orig.build_wheel(wheel_directory, config_settings, metadata_directory)
