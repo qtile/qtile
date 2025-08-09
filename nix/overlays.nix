ab7193e45d9564a71ad76b43b50d39db5bbd8406
Sigmanificient <edhyjox@gmail.com>
2025-08-09 02:49:56 +0200

    Flatten the overlay file to make it more readable
    Quite a lot of refactor, to make this file cleaner
    
    Co-authored-by: Gurjaka <esartia.gurika@gmail.com>

--- nix/overlays.nix
+++ nix/overlays.nix
@@ -1,55 +1,58 @@
 self: final: prev:
 let
-  inherit (prev) lib pkgs;
+  inherit (prev) pkgs lib;
 
   build-config = import ./build-config.nix pkgs;
+
+  local-version =
+    with builtins;
+    let
+      flakever = self.shortRev or "dev";
+
+      symver = lib.pipe (readFile ../CHANGELOG) [
+        (split "\n")
+        (filter (x: !isList x && lib.strings.hasPrefix "Qtile" x))
+        (release: elemAt release 1) # the 0th element is the template
+        (match "Qtile ([0-9.]+), released ([0-9-]+):")
+        head
+      ];
+    in
+    "${symver}+${flakever}.flake";
+
+  qtile-override-func =
+    qtile-prev:
+    {
+      name = "${qtile-prev.pname}-${qtile-prev.version}";
+
+      version = local-version;
+
+      src = ./..; # use the source of the git repo
+    }
+    // {
+      env = build-config.resolved-env-vars;
+
+      pypaBuildFlags = build-config.resolved-config-settings;
+
+      postPatch = ""; # removes nixpkgs postPatch
+    };
 in
 {
   pythonPackagesOverlays = (prev.pythonPackagesOverlays or [ ]) ++ [
-    (_: pprev: {
-      qtile =
-        (pprev.qtile.overrideAttrs (
-          old:
-          let
-            flakever = self.shortRev or "dev";
-
-            releases = (
-              builtins.filter (x: !builtins.isList x && prev.lib.strings.hasPrefix "Qtile" x) (
-                builtins.split "\n" (builtins.readFile ../CHANGELOG)
-              )
-            );
-
-            # the 0th element is the template
-            current-release-title = builtins.elemAt releases 1;
-
-            symver = builtins.head (
-              builtins.match "Qtile ([0-9.]+), released ([0-9-]+):" current-release-title
-            );
-          in
-          {
-            version = "${symver}+${flakever}.flake";
-            # use the source of the git repo
-            src = ./..;
-            disabled = false;
-
-            env = build-config.resolved-env-vars;
-
-            pypaBuildFlags = build-config.resolved-config-settings;
-
-            # removes nixpkgs postPatch
-            postPatch = "";
-          }
-        )).override
-          { wlroots = prev.wlroots_0_17; };
-    })
+    (
+      _: py-prev:
+      let
+        qtile = py-prev.qtile.overrideAttrs qtile-override-func;
+      in
+      {
+        qtile = qtile.override { wlroots = pkgs.wlroots_0_17; };
+      }
+    )
   ];
-  python3 =
-    let
-      self = prev.python3.override {
-        inherit self;
-        packageOverrides = prev.lib.composeManyExtensions final.pythonPackagesOverlays;
-      };
-    in
-    self;
+
+  python3 = prev.python3.override {
+    self = final.python3;
+    packageOverrides = lib.composeManyExtensions final.pythonPackagesOverlays;
+  };
+
   python3Packages = final.python3.pkgs;
 }
