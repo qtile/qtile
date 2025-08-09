271c71709c83e84af38537e285e0f612d0bd1149
Sigmanificient <edhyjox@gmail.com>
2025-08-09 02:04:10 +0200

    Provide a build config to resolve the lib/headers in nix builder
    This implementation follow the discussion of #5420, to make an
    abstraction that generate the config flags and environment variable
    dynamically.
    The goal is to have an easier to use interface for non-nix user to edit.
    
    Co-authored-by: Gurjaka <esartia.gurika@gmail.com>

---
+++ nix/build-config.nix
@@ -0,0 +1,66 @@
+pkgs:
+let
+  # library to be provided using the `--config-setting`
+  runtime-libs = [
+    {
+      varname = "GOBJECT";
+      pkg = pkgs.glib;
+      libname = "libgobject-2.0.so";
+    }
+    {
+      varname = "PANGO";
+      pkg = pkgs.pango;
+      libname = "libpango-1.0.so";
+    }
+    {
+      varname = "PANGOCAIRO";
+      pkg = pkgs.pango;
+      libname = "libpangocairo-1.0.so";
+    }
+    {
+      varname = "XCBCURSOR";
+      pkg = pkgs.xorg.xcbutilcursor;
+      libname = "libxcb-cursor.so";
+    }
+  ];
+
+  # header files provided through environment variables
+  headers-location = [
+    {
+      varname = "PIXMAN";
+      pkg = pkgs.pixman;
+      header-dir = "pixman-1";
+    }
+    {
+      varname = "LIBDRM";
+      pkg = pkgs.libdrm;
+      header-dir = "libdrm";
+    }
+  ];
+
+  # Internal implementation
+  inherit (pkgs) lib;
+in
+{
+  resolved-config-settings = map (
+    {
+      varname,
+      pkg,
+      libname,
+    }:
+    "--config-setting=${varname}_PATH=${lib.getLib pkg}/lib/${libname}"
+  ) runtime-libs;
+
+  resolved-env-vars = lib.foldr (a: b: a // b) { } (
+    map (
+      {
+        varname,
+        pkg,
+        header-dir,
+      }:
+      {
+        "QTILE_${varname}_PATH" = "${lib.getDev pkg}/include/${header-dir}";
+      }
+    ) headers-location
+  );
+}
