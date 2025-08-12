4da07e3d8590b8125b090a7dd0a4d75979426624
elParaguayo <elparaguayocode@gmail.com>
2025-08-12 06:22:29 +0100

    [wayc] builder update

--- builder.py
+++ builder.py
@@ -59,4 +59,3 @@ def build_wheel(wheel_directory, config_settings=None, metadata_directory=None):
             f.write(f"{p} = {config_settings.get(p)!r}\n")
 
     return _orig.build_wheel(wheel_directory, config_settings, metadata_directory)
-
