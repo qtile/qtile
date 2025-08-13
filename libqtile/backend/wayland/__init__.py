07e49bfe012cd574806c1d4ea9840e37dfaa0195
Sigmanificient <edhyjox@gmail.com>
2025-08-13 14:32:08 +0200

    Setup the python extension for wayland backend

--- libqtile/backend/wayland/__init__.py
+++ libqtile/backend/wayland/__init__.py
@@ -24,4 +24,11 @@ try:
 except ModuleNotFoundError:
     print("InputConfig couldn't be imported from libqtile.backend.wayland")
     print("If this happened during setup.py installation, ignore this message.")
-    print("Otherwise, make sure to run ./scripts/ffibuild.")
+
+
+try:
+    import wayland_backend
+
+    wayland_backend.hello()
+except ModuleNotFoundError:
+    print("Wayland Backend (python ext) is not present")
