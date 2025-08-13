555830f5b72d7fdeb339ed88e6d62544e8717f7b
Sigmanificient <edhyjox@gmail.com>
2025-08-13 19:59:26 +0200

    Move the logger to the wayland backend extension

--- libqtile/backend/wayland/core.py
+++ libqtile/backend/wayland/core.py
@@ -116,10 +116,10 @@ def translate_masks(modifiers: list[str]) -> int:
 qw_logger = logging.getLogger("qw")
 
 
-@ffi.def_extern()
-def log_cb(importance: int, formatted_str) -> None:
+def log_cb(importance: int, formatted_str: str) -> None:
     """Callback that logs the string at the given level"""
-    log_str = ffi.string(formatted_str).decode()
+    log_str = formatted_str
+
     if importance == wayland_backend.WLR_ERROR:
         qw_logger.error(log_str)
     elif importance == wayland_backend.WLR_INFO:
@@ -176,12 +176,12 @@ def on_screen_reserve_space_cb(output, userdata):
 
 def get_wlr_log_level():
     if logger.level <= logging.DEBUG:
-        return lib.WLR_DEBUG
+        return wayland_backend.WLR_DEBUG
     elif logger.level <= logging.INFO:
-        return lib.WLR_INFO
+        return wayland_backend.WLR_INFO
     elif logger.level <= logging.ERROR:
-        return lib.WLR_ERROR
-    return lib.WLR_SILENT
+        return wayland_backend.WLR_ERROR
+    return wayland_backend.WLR_SILENT
 
 
 class Core(base.Core):
@@ -195,7 +195,8 @@ class Core(base.Core):
 
         """Setup the Wayland core backend"""
         log_utils.init_log(logger.level, log_path=log_utils.get_default_log(), logger=qw_logger)
-        lib.qw_log_init(get_wlr_log_level(), lib.log_cb)
+
+        wayland_backend.set_log_callback(get_wlr_log_level(), log_cb);
         self.qw = lib.qw_server_create()
         if not self.qw:
             sys.exit(1)
