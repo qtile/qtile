2762ff006a774480acd338e1c23d3fe1b188cd18
elParaguayo <elparaguayocode@gmail.com>
2025-08-15 09:10:58 +0100

    Fix wayland builder
    We removed setup.py but our build script (`builder.py`) did not actually
    trigger the wayland backend build, instead it just ensured the correct
    dependencies were injected into the build requirement.
    
    This PR updates `builder.py` to trigger the build. qtile will still
    build even if the wayland backend build fails unless the user passes
    `--config-settings backend=wayland` in which case an exception will be
    raised if the backend cannot be built.

--- libqtile/backend/wayland/cffi/build.py
+++ libqtile/backend/wayland/cffi/build.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 
 import wlroots.ffi_build as wlr
 from cffi import FFI
@@ -30,5 +31,21 @@ ffi.include(wlr.ffi_builder)
 ffi.cdef(libinput.CDEF)
 ffi.cdef(cairo_buffer.CDEF)
 
+
+def ffi_compile(verbose: bool = False) -> None:
+    # The ffi source of "libqtile.backend.wayland._ffi" means that we'll compile the library file
+    # at libqtile/backend/wayland/_ffi.so.
+    # This is built at the path specified in tmpdir which is "." by default. So, if we're in a
+    # virtual environment, libqtile might be at .venv/lib/python3.13/site-packages/ but if we run
+    # the builder script, we'll get a new libqtile folder in th current directory that only has that
+    # tree shown above and the libtile library won't find the `_ffi` library.
+    # We therefore set the tmpdir to be the path to the folder containing libqtile so the library
+    # is always created in the correct folder.
+    # The compile command is nested in a function to ensure that the tmpdir value is not overwritten.
+    ffi.compile(
+        tmpdir=Path(__file__).parent.parent.parent.parent.parent.as_posix(), verbose=verbose
+    )
+
+
 if __name__ == "__main__":
-    ffi.compile()
+    ffi_compile()
