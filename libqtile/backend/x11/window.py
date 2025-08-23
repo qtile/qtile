9b6b952191f9ad17c3567def6026b54363697422
elParaguayo <elparaguayocode@gmail.com>
2025-08-23 10:12:45 +0100

    force bring_to_front call to top

--- libqtile/backend/x11/window.py
+++ libqtile/backend/x11/window.py
@@ -1276,6 +1276,7 @@ class _Window:
     def bring_to_front(self):
         if self.get_wm_type() != "desktop":
             self.change_layer(layer=LayerGroup.BRING_TO_FRONT)
+            self.move_to_top()
 
     def is_visible(self):
         return not self.hidden
