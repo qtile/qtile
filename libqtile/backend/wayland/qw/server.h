d99be15062a286a7d4b30e5b7cc66af769519283
richcarni <richcarni@gmail.com>
2025-08-28 13:38:29 +1000

    Configure input devices on device added
    Also configure devices on startup

--- libqtile/backend/wayland/qw/server.h
+++ libqtile/backend/wayland/qw/server.h
@@ -83,6 +83,9 @@ struct qw_input_device;
 // Iterate input devices callback
 typedef void (*input_device_cb_t)(struct qw_input_device *input_device, const char* name, int type, int vendor, int product);
 
+// Callback for when an input device is added
+typedef void (*on_input_device_added_cb_t)(void *userdata);
+
 enum {
     LAYER_BACKGROUND,   // background, layer shell
     LAYER_BOTTOM,       // bottom, layer shell
@@ -120,6 +123,7 @@ struct qw_server {
     on_screen_change_cb_t on_screen_change_cb;
     on_screen_reserve_space_cb_t on_screen_reserve_space_cb;
     view_urgent_cb_t view_urgent_cb;
+    on_input_device_added_cb_t on_input_device_added_cb;
     void *view_urgent_cb_data;
     void *cb_data;
 
