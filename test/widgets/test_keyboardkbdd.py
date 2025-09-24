6a6442f3d16c34c68e0eceac029dd86beab04d1f
Tycho Andersen <tycho@tycho.pizza>
2025-09-24 14:35:15 -0600

    *: drop licenses from individual files
    ...in favor of the LICENSE we already ship!
    
    I had originally added these because debian's `licensecheck` complains
    about them, but they are noisy, get out of date quickly, and debian's
    policy no longer requires a license per-file. So, let's drop them all.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- test/widgets/test_keyboardkbdd.py
+++ test/widgets/test_keyboardkbdd.py
@@ -1,30 +1,3 @@
-# Copyright (c) 2021 elParaguayo
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-# Widget specific tests
-
-# The test_widget_init_config will cover the scenario where `kbdd` is not
-# running and will also run the asyncio _config_async method.
-
-# This test file covers the remaining widget code
-
 from importlib import reload
 
 import pytest
