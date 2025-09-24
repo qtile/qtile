0e02ec96c64805c625539b60e3350daaf1821485
Tycho Andersen <tycho@tycho.pizza>
2025-09-24 14:35:15 -0600

    *: drop licenses from individual files
    ...in favor of the LICENSE we already ship!
    
    I had originally added these because debian's `licensecheck` complains
    about them, but they are noisy, get out of date quickly, and debian's
    policy no longer requires a license per-file. So, let's drop them all.
    
    Signed-off-by: Tycho Andersen <tycho@tycho.pizza>

--- test/core/test_exitcode.py
+++ test/core/test_exitcode.py
@@ -1,29 +1,3 @@
-# Copyright (c) 2024 Thomas Krug
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
-
-# in these testcases there are two qtile instances
-# one started by the helpers.TestManager,
-# which is used to evaluate code coverage
-# and a second instance started by run_qtile,
-# which is used to test the actual exit code behavior
 def test_exitcode_default(manager):
     manager.c.shutdown()
     manager.proc.join()
