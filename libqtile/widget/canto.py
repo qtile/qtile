a25d09ed1d2fd18e8486e68669c85159bbf404be
paradise <keonfarrugia@gmail.com>
2025-12-29 12:00:48 +0100

    Patch:Canto widget now uses correct binary and has a test(fix #5728)

--- libqtile/widget/canto.py
+++ libqtile/widget/canto.py
@@ -2,9 +2,13 @@ from subprocess import call
 
 from libqtile.widget import base
 
+from libqtile.widget.generic_poll_text import GenPollCommand
 
-class Canto(base.BackgroundPoll):
-    """Display RSS feeds updates using the canto console reader
+from libqtile.log_utils import logger
+
+
+class Canto(GenPollCommand):
+    """Display RSS feeds updates using the canto remote
 
     Widget requirements: canto_
 
@@ -12,31 +16,46 @@ class Canto(base.BackgroundPoll):
     """
 
     defaults = [
-        ("fetch", False, "Whether to fetch new items on update"),
-        ("feeds", [], "List of feeds to display, empty for all"),
-        ("one_format", "{name}: {number}", "One feed display format"),
-        ("all_format", "{number}", "All feeds display format"),
+        ("tags", [], "List of tags to display, empty for all"),
+        ("one_format", "{name}: {number}", "One tag display format"),
+        ("all_format", "{number}", "All tags display format"),
     ]
 
     def __init__(self, **config):
-        base.BackgroundPoll.__init__(self, "", **config)
+        config["cmd"] = ["canto-remote", "status", "--tags"]
+        GenPollCommand.__init__(self, **config)
         self.add_defaults(Canto.defaults)
 
-    def poll(self):
-        if not self.feeds:
-            arg = "-a"
-            if self.fetch:
-                arg += "u"
-            output = self.all_format.format(number=self.call_process(["canto", arg])[:-1])
-            return output
+    def get_info(self, output):
+        output = output.splitlines()
+        if not self.tags:
+            total_items = 0
+            for line in output:
+                if "maintag:" in line:
+                    current_tag_items = line[line.index(":", line.index(":") + 1) + 2 :]
+                    total_items += int(current_tag_items)
+            return total_items
         else:
-            if self.fetch:
-                call(["canto", "-u"])
-            return "".join(
-                [
-                    self.one_format.format(
-                        name=feed, number=self.call_process(["canto", "-n", feed])[:-1]
-                    )
-                    for feed in self.feeds
-                ]
-            )
+            all_tags_output = {}
+            for line in output:
+                second_colon_index = line.index(":", line.index(":") + 1)
+                current_tag_name = line[:second_colon_index].strip()
+                if current_tag_name in self.tags:
+                    current_tag_items = line[second_colon_index + 2 :].strip()
+                    all_tags_output.update({current_tag_name: current_tag_items})
+            return all_tags_output
+
+    def parse(self, output):
+        output = self.get_info(output)
+        if not self.tags:
+            display = self.all_format.format(number=output)
+            return display
+        else:
+            if not output:
+                logger.debug("Canto remote found no tags")
+                return ""
+            display = []
+            for key in output:
+                current_tag_output = self.one_format.format(name=key, number=output[key])
+                display.append(current_tag_output)
+            return "".join(display)
