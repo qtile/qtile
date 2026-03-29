qtile/qtile

commit 4c37efd25d1a184c55f934d8bbf5b4c66d55bb4f

Allow lazy.spawn to take os.PathLike
Function will also call expanduser on Path objects.

Closes #5831

libqtile/core/manager.py
@@ -49,7 +49,8 @@ from libqtile.utils import (
 from libqtile.widget.base import _Widget
 
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Callable, Sequence
+    from os import PathLike
     from typing import Any, Literal
 
     from libqtile.command.base import ItemT
@@ -1381,7 +1382,7 @@ class Qtile(CommandObject):
     @expose_command()
     def spawn(
         self,
-        cmd: list[str] | str,
+        cmd: list[str] | str | Sequence[str | PathLike],
         shell: bool = False,
         env: dict[str, str] = dict(),
         group: str | None = None,
@@ -1410,10 +1411,16 @@ class Qtile(CommandObject):
 
             spawn("screenshot | xclip", shell=True)
         """
-        if isinstance(cmd, str):
-            args = shlex.split(cmd)
+
+        def expand(arg: str | PathLike) -> str:
+            if isinstance(arg, Path):
+                arg = arg.expanduser()
+            return os.fspath(arg)
+
+        if isinstance(cmd, str | os.PathLike):
+            args = shlex.split(expand(cmd))
         else:
-            args = list(cmd)
+            args = [expand(c) for c in cmd]
             cmd = shlex.join(args)
 
         to_lookup = args[0]

commit cf3c80080d11f9964ca35766a0dbbb1671c3d123

Fix `qtile cmd-obj -f spawn` crash
4c37efd introduced additional types that could be passed to
`qtile.spawn`. This caused IPC to crash as it was unable to lift string
arguments to this new type.

This PR adds better handling of argument types.

Fixes #5893

libqtile/command/interface.py
@@ -356,7 +356,10 @@ def lift_args(cmd, args, kwargs):
     converted_args = []
     converted_kwargs = dict()
 
-    params = typing.get_type_hints(cmd, globalns=globals())
+    # We use the globals from the command itself to ensure that all types are
+    # available. This means that structural types need to imported at run time
+    # and not just for typing.
+    params = typing.get_type_hints(cmd, globalns=cmd.__globals__)
 
     non_return_annotated_args = filter(lambda k: k != "return", params.keys())
     for param, arg in zip(non_return_annotated_args, args):

libqtile/core/manager.py
@@ -13,7 +13,9 @@ import socket
 import tempfile
 import time
 from collections import defaultdict
+from collections.abc import Sequence
 from logging.handlers import RotatingFileHandler
+from os import PathLike
 from pathlib import Path
 from typing import TYPE_CHECKING
 
@@ -58,8 +60,7 @@ from libqtile.utils import (
 from libqtile.widget.base import _Widget
 
 if TYPE_CHECKING:
-    from collections.abc import Callable, Sequence
-    from os import PathLike
+    from collections.abc import Callable
     from typing import Any, Literal
 
     from libqtile.command.base import ItemT